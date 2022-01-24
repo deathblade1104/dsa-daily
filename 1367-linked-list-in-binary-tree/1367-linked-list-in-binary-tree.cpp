1c56b12ceb9638d87b1c3c0682861d90b27d6d33
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-01-25 03:01:12 +0530

    Time: 837 ms (5.02%), Space: 83.8 MB (5.15%) - LeetHub

--- 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.cpp
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.cpp
@@ -21,14 +21,42 @@
  */
 class Solution {
 public:
-    bool findSubPath(ListNode* head, TreeNode* root) {
-        if (!head) return true;
-        if (!root) return false;
-        return head->val == root->val && (findSubPath(head->next, root->left) || findSubPath(head->next, root->right));
+    bool get_ans(ListNode* curr, ListNode*head,TreeNode *root, 
+                 map<pair<ListNode*,TreeNode*>,bool>&mp)
+    {
+        if(curr==NULL) return true;
+        
+        if(root==NULL) return false;
+        
+        pair<ListNode*,TreeNode*>p(curr,root);
+        
+        if(mp.find(p)!=mp.end())
+            return mp[p];
+            
+        bool op1=false,op2=false;
+        
+        
+        if(head->val==root->val)
+            op1= get_ans(head->next,head,root->left,mp) or 
+            get_ans(head->next,head,root->right,mp);
+        
+        else
+            op1 = get_ans(head,head,root->left,mp) or 
+            get_ans(head,head,root->right,mp);
+        
+        if(curr->val==root->val)
+            op2 = get_ans(curr->next,head,root->left,mp) 
+            or get_ans(curr->next,head,root->right,mp);
+        
+        mp[p]= op1 or op2;
+        
+        return mp[p];
     }
-    
     bool isSubPath(ListNode* head, TreeNode* root) {
-        if (!root) return false;
-        return findSubPath(head, root) || isSubPath(head, root->left) || isSubPath(head, root->right);
+        map<pair<ListNode*,TreeNode*>,bool>mp;
+        
+        return get_ans(head,head,root,mp);
+        
+        
     }
 };
