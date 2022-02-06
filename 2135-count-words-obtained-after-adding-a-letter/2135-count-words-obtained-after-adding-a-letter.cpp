46a598fe56068251da2f177541c7274e4bd03c56
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-02-06 18:36:03 +0530

    Time: 456 ms (78.81%), Space: 107.8 MB (56.81%) - LeetHub

---
+++ 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int wordCount(vector<string>& startWords, vector<string>& targetWords) {
+        
+        unordered_set<string>mp;        
+        for(int i=0;i<startWords.size();i++)
+        {
+            sort(startWords[i].begin(),startWords[i].end());
+            mp.insert(startWords[i]);
+        }
+        
+        for(int i=0;i<targetWords.size();i++)
+            sort(targetWords[i].begin(),targetWords[i].end());
+        
+        int ans=0;
+        
+        for(int i=0;i<targetWords.size();i++)
+        {
+           string s=targetWords[i];
+            
+            for(int j=0;j<s.size();j++)
+            {
+                string temp= s.substr(0,j) + s.substr(j+1);
+                
+                if(mp.find(temp)!=mp.end())
+                {   
+                    // cout<<s<<" "<<temp<<endl;
+                    ans++;
+                    break;
+                }
+            }
+        }
+        
+        return ans;
+        
+    }
+};
