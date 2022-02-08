e6bb0b493823bb3b91a12a255d9476d82fa11c6d
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-02-08 21:06:16 +0530

    Time: 3 ms (53.62%), Space: 6 MB (25.37%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int n) {
+        
+        if(n<10)
+            return n;
+        
+        int x=0;
+        while(n>0)
+        {
+            x+=n%10;
+            n/=10;
+        }
+        
+        return addDigits(x);
+    }
+};
