98406be0c218ee9f0f20a4395c04c328a8e62258
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-02-13 00:42:02 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -6,28 +6,45 @@ using namespace std;
 class Solution {
   public:
     // Function to find the number of islands.
-    void dfs(vector<vector<char>>&grid, int r, int c, int &n , int &m)
-    {
-        if(r<0 or r>=n or c<0 or c>=m or grid[r][c]=='0')
-        return;
+    // void dfs(vector<vector<char>>&grid, int r, int c, int &n , int &m)
+    // {
+    //     if(r<0 or r>=n or c<0 or c>=m or grid[r][c]=='0')
+    //     return;
         
-        grid[r][c]='0';
+    //     grid[r][c]='0';
         
-        dfs(grid,r+1,c,n,m);
-        dfs(grid,r-1,c,n,m);
-        dfs(grid,r,c+1,n,m);
-        dfs(grid,r,c-1,n,m);
-        dfs(grid,r+1,c+1,n,m);
-        dfs(grid,r+1,c-1,n,m);
-        dfs(grid,r-1,c+1,n,m);
-        dfs(grid,r-1,c-1,n,m);
-        return;
+    //     dfs(grid,r+1,c,n,m);
+    //     dfs(grid,r-1,c,n,m);
+    //     dfs(grid,r,c+1,n,m);
+    //     dfs(grid,r,c-1,n,m);
+    //     dfs(grid,r+1,c+1,n,m);
+    //     dfs(grid,r+1,c-1,n,m);
+    //     dfs(grid,r-1,c+1,n,m);
+    //     dfs(grid,r-1,c-1,n,m);
+    //     return;
         
-    }
+    // }
     int numIslands(vector<vector<char>>& grid) {
         // Code here
-        int count=0;
-        int n=grid.size(),m=grid[0].size();
+        // int count=0;
+        // int n=grid.size(),m=grid[0].size();
+        
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<m;j++)
+        //     {
+        //         if(grid[i][j]=='1')
+        //         {
+        //             count++;
+        //             dfs(grid,i,j,n,m);
+        //         }
+        //     }
+        // }
+        
+        // return count;
+        
+        
+        int count=0, n=grid.size(),m=grid[0].size();
         
         for(int i=0;i<n;i++)
         {
@@ -36,7 +53,30 @@ class Solution {
                 if(grid[i][j]=='1')
                 {
                     count++;
-                    dfs(grid,i,j,n,m);
+                    queue<pair<int,int>>q;
+                    q.push({i,j});
+                    
+                    while(q.size()>0)
+                    {
+                        pair<int,int>p=q.front();
+                        q.pop();
+                        
+                        int r=p.first,c=p.second;
+                        
+                        if(r<0 or r>=n or c<0 or c>=m or grid[r][c]=='0')
+                        continue;
+                        
+                        grid[r][c]='0';
+                        
+                        q.push({r+1,c});
+                        q.push({r-1,c});
+                        q.push({r,c+1});
+                        q.push({r,c-1});
+                        q.push({r+1,c+1});
+                        q.push({r+1,c-1});
+                        q.push({r-1,c+1});
+                        q.push({r-1,c-1});
+                    }
                 }
             }
         }
