00c99474da4f5945a9399d799c9258f52ad75ef7
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-02-25 02:12:04 +0530

    Time: 31 ms (53.29%), Space: 14.1 MB (46.37%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,48 +1,104 @@
 class Solution {
 private :
-    bool DFS(int curr, vector<vector<int>>&adj, vector<bool>&vis, vector<bool>&anc, vector<int>&ans)
-    {
-        if(vis[curr])
-        {
-            if(anc[curr])
-                return true;
+//     bool DFS(int curr, vector<vector<int>>&adj, vector<bool>&vis, vector<bool>&anc, vector<int>&ans)
+//     {
+//         if(vis[curr])
+//         {
+//             if(anc[curr])
+//                 return true;
             
-            return false;
-        }
+//             return false;
+//         }
         
-        vis[curr]=true;
-        anc[curr]=true;
+//         vis[curr]=true;
+//         anc[curr]=true;
                 
-        for(int x : adj[curr])
-        {
-            if(DFS(x,adj,vis,anc,ans))
-                return true;
-        }
+//         for(int x : adj[curr])
+//         {
+//             if(DFS(x,adj,vis,anc,ans))
+//                 return true;
+//         }
         
-        ans.push_back(curr);
-        anc[curr]=false;
-        return false;
-    }
+//         ans.push_back(curr);
+//         anc[curr]=false;
+//         return false;
+//     }
     
-public:
+//     vector<int>DFS_driver(int V, vector<vector<int>>&edges)
+//     {
+//          vector<int>ans;
+//         vector<vector<int>>adj(V);
+        
+//         for(auto i : edges)
+//             adj[i[1]].push_back(i[0]);
+        
+//         vector<bool>vis(V,false),anc(V,false);
+        
+//         for(int i=0;i<V;i++)
+//         {
+//             if(!vis[i] and DFS(i,adj,vis,anc,ans))
+//                 return {};
+//         }
+        
+//         reverse(ans.begin(),ans.end());        
+//         return ans;
+//     }
     
-    vector<int> findOrder(int V, vector<vector<int>>&edges) {
     
+    vector<int> BFS_Kahn(int V, vector<vector<int>>&edges)
+    {
+        
         vector<int>ans;
+        vector<int>indegree(V,0);
         vector<vector<int>>adj(V);
         
         for(auto i : edges)
             adj[i[1]].push_back(i[0]);
         
-        vector<bool>vis(V,false),anc(V,false);
         
         for(int i=0;i<V;i++)
         {
-            if(!vis[i] and DFS(i,adj,vis,anc,ans))
-                return {};
+            for(int x : adj[i])
+                indegree[x]++;
         }
         
-        reverse(ans.begin(),ans.end());        
-        return ans;
+        queue<int>q;
+        for(int i=0;i<V;i++)
+            if(indegree[i]==0)
+                q.push(i);
+        
+        
+        int cnt=q.size();
+        
+        while(q.size()>0)
+        {
+            int curr=q.front();
+            q.pop();
+            
+            
+            ans.push_back(curr);
+            
+            
+            for(int x : adj[curr])
+            {
+                indegree[x]--;
+                if(indegree[x]==0)
+                {
+                    q.push(x);
+                    cnt++;
+                }
+            }
+        }
+        
+        
+        if(cnt == V)
+            return ans;
+        
+        else return {};
+    }
+public:
+    
+    vector<int> findOrder(int V, vector<vector<int>>&edges) {        
+       return BFS_Kahn(V,edges);
     }
 };
