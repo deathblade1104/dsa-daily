a5e533becd2b1c5eb60594fda6a7d3ac1c2301ad
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-02-25 02:17:28 +0530

    Time: 44 ms (26.79%), Space: 14.1 MB (46.37%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,48 +1,48 @@
 class Solution {
 private :
-//     bool DFS(int curr, vector<vector<int>>&adj, vector<bool>&vis, vector<bool>&anc, vector<int>&ans)
-//     {
-//         if(vis[curr])
-//         {
-//             if(anc[curr])
-//                 return true;
+    bool DFS(int curr, vector<vector<int>>&adj, vector<bool>&vis, vector<bool>&anc, vector<int>&ans)
+    {
+        if(vis[curr])
+        {
+            if(anc[curr])
+                return true;
             
-//             return false;
-//         }
+            return false;
+        }
         
-//         vis[curr]=true;
-//         anc[curr]=true;
+        vis[curr]=true;
+        anc[curr]=true;
                 
-//         for(int x : adj[curr])
-//         {
-//             if(DFS(x,adj,vis,anc,ans))
-//                 return true;
-//         }
+        for(int x : adj[curr])
+        {
+            if(DFS(x,adj,vis,anc,ans))
+                return true;
+        }
         
-//         ans.push_back(curr);
-//         anc[curr]=false;
-//         return false;
-//     }
+        ans.push_back(curr);
+        anc[curr]=false;
+        return false;
+    }
     
-//     vector<int>DFS_driver(int V, vector<vector<int>>&edges)
-//     {
-//          vector<int>ans;
-//         vector<vector<int>>adj(V);
+    vector<int>DFS_driver(int V, vector<vector<int>>&edges)
+    {
+         vector<int>ans;
+        vector<vector<int>>adj(V);
         
-//         for(auto i : edges)
-//             adj[i[1]].push_back(i[0]);
+        for(auto i : edges)
+            adj[i[1]].push_back(i[0]);
         
-//         vector<bool>vis(V,false),anc(V,false);
+        vector<bool>vis(V,false),anc(V,false);
         
-//         for(int i=0;i<V;i++)
-//         {
-//             if(!vis[i] and DFS(i,adj,vis,anc,ans))
-//                 return {};
-//         }
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i] and DFS(i,adj,vis,anc,ans))
+                return {};
+        }
         
-//         reverse(ans.begin(),ans.end());        
-//         return ans;
-//     }
+        reverse(ans.begin(),ans.end());        
+        return ans;
+    }
     
     
     vector<int> BFS_Kahn(int V, vector<vector<int>>&edges)
