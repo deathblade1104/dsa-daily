1cdd380a5a378dcd85e756e0a77f60b96e515f8a
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-02-25 17:32:52 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (73.32%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -1,9 +1,8 @@
 class Solution {
 private:
-    pair<long long,int>get_num(string &s, int i)
+    long long get_num(string &s, int &i)
     {
-        long long ans=0;
-        
+        long long ans=0;        
         
         while( i<s.size() and isdigit(s[i]) )
         {
@@ -12,8 +11,7 @@ private:
             i++;
         }
         
-        pair<long long,int>p(ans,i);
-        return p;
+        return ans;
     }
     
 public:    
@@ -23,20 +21,20 @@ public:
         
         while(i<v1.size() or j<v2.size())
         {
-            pair<long long,int>p1,p2;
-            p1=get_num(v1,i);
-            p2=get_num(v2,j);
+            long long n1=0,n2=0;
+            n1=get_num(v1,i);
+            n2=get_num(v2,j);
             
-            if(p1.first>p2.first)
+            if(n1>n2)
                 return 1;
             
-            else if (p1.first<p2.first)
+            else if (n1<n2)
                 return -1;
             
             else
             {
-                i=p1.second+1;
-                j=p2.second+1;
+                i++;
+                j++;
             }
         }
         
