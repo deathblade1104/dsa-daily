37d20b134249ef9d2f437b1b917edb1790aef6a8
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-02-27 18:12:22 +0530

    Time: 4 ms (97.43%), Space: 17.4 MB (48.00%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -21,10 +21,8 @@ public:
         long long ans=1;        
         while(q.size()>0)
         {
-            int sz=q.size();
-            long long mn=q.front().second;
-            long long l=INT_MAX,r=INT_MIN;
-            for(int i=0;i<sz;i++)
+            long long sz=q.size(), mn=q.front().second,l=INT_MAX,r=INT_MIN;
+            for(long long i=0;i<sz;i++)
             {
                 pair<TreeNode*,long long>p=q.front(); q.pop();
                 TreeNode *curr=p.first;
@@ -37,10 +35,10 @@ public:
                 }
                 
                 if(curr->left)
-                    q.push({curr->left, (hd*2*(1LL))+1});
+                    q.push({curr->left, (hd*2*(1LL))});
                 
                 if(curr->right)
-                    q.push({curr->right, (hd*2*(1LL))+2});
+                    q.push({curr->right, (hd*2*(1LL))+1});
             }
             
             ans=max(ans,r-l+1);
