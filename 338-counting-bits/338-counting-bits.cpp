93009e37a2a479b0d50a9fedee8e4e48f207f99a
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-01 22:56:50 +0530

    Time: 7 ms (62.32%), Space: 7.6 MB (98.10%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     // DP Approach 
-    
-    int set_bits(int n,vector<int>&ans)
-    {
-        if(ans[n]!=-1)
-            return ans[n];
-        
-        if(n%2==0)
-            return set_bits(n/2,ans);
-        
-        else return 1 + set_bits(n/2,ans);
-        
-    }
-    vector<int> countBits(int n) {
-        vector<int>ans(n+1,-1);
+     vector<int> countBits(int n) {
+        vector<int>ans(n+1);
         ans[0]=0;
         
         for(int i=1;i<=n;i++)
-            ans[i]=set_bits(i,ans);
+        {
+            if(i%2==0)
+            ans[i]=ans[i/2];
         
+            else ans[i]=ans[i/2]+1;
+        }
         return ans;
     }
 };
