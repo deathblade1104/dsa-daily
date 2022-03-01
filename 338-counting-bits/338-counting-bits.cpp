311de74db1c807b17462e15f1dc450c18fa767a8
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-01 22:51:26 +0530

    Time: 4 ms (88.16%), Space: 7.9 MB (75.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    int set_bits(int n)
+    int set_bits(int n,vector<int>&ans)
     {
-        int count = 0;
+        if(ans[n]!=-1)
+            return ans[n];
         
-        for(int i=0;i<32;i++)
-        {
-            if(n & (1<<i))
-                count++;
-        }
-        return count;
+        if(n%2==0)
+            return set_bits(n/2,ans);
+        
+        else return 1 + set_bits(n/2,ans);
+        
     }
     vector<int> countBits(int n) {
-        vector<int>ans(n+1);
+        vector<int>ans(n+1,-1);
+        ans[0]=0;
         
-        for(int i=0;i<=n;i++)
-            ans[i]=set_bits(i);
+        for(int i=1;i<=n;i++)
+            ans[i]=set_bits(i,ans);
         
         return ans;
     }
