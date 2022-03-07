fa03e431900eae7f43ef7fb50075c00ec8978de7
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-08 00:44:59 +0530

    Added solution - LeetHub

--- 4.                         Minimum Spanning Tree using Kruskal - GFG/4.-minimum-spanning-tree-using-kruskal.cpp
+++ 4.                         Minimum Spanning Tree using Kruskal - GFG/4.-minimum-spanning-tree-using-kruskal.cpp
@@ -20,70 +20,99 @@ using namespace std;
 */
 
 //Function to find the minimum spanning tree value using Kruskal.
-int
-findRoot (int a, int parent[])
+struct Node{
+    int src,dest;
+    long long int wt;
+    
+    Node(int a, int b, long long int w)
+    {
+        src=a;
+        dest=b;
+        wt=w;
+    }
+};
+
+struct mycomp{
+    bool operator()(const Node&a,const Node&b)
+    {
+        return a.wt<b.wt;
+    }
+};
+
+vector<Node>make_edges(vector<pair<int, long long int>> adj[],int n)
 {
-  if (a == parent[a])
-    return a;
-  parent[a] = findRoot (parent[a], parent);
-  return parent[a];
+    vector<Node>edges;
+    
+    for(int i=1;i<=n;i++)
+    {
+        for(auto x : adj[i])
+        {
+            Node n1(i,x.first,x.second);
+            edges.push_back(n1);
+        }
+    }
+    
+    sort(edges.begin(),edges.end(),mycomp());
+    return edges;
 }
 
-void
-union_Nodes (int a, int b, int parent[], int rank[])
+int find_par(int v, vector<int>&par)
 {
-  int x = findRoot (a, parent);
-  int y = findRoot (b, parent);
-  if (x == y)
+    if(par[v]==v)
+    return v;
+    par[v] = find_par(par[v],par);
+    return par[v];
+}
+void DSU_union(int s,int d, vector<int>&par, vector<int>&rank)
+{
+    int p1=find_par(s,par);
+    int p2=find_par(d,par);
+    
+    if(p1 == p2)
     return;
-  else if (rank[x] < rank[y])
-    parent[x] = y;
-  else if (rank[y] < rank[x])
-    parent[y] = x;
-  else
+
+    if(rank[p1] < rank[p2])
+    par[p1] = p2;
+    
+    else if (rank[p2] < rank[p1])
+    par[p2] = p1;
+    
+    else
     {
-      parent[x] = y;
-      rank[y]++;
+        par[p1] = p2;
+        rank[p2]++;
     }
 }
 
-bool
-same (int a, int b, int parent[], int rank[])
+long long int kruskalDSU(vector<pair<int, long long int>> adj[], int n, int m) 
 {
-  return findRoot (a, parent) == findRoot (b, parent);
-}
-
-long long int kruskalDSU (vector < pair < int, long long int >>adj[], int n, int m)
-{
-  pair < long long int, pair < int, int >>edges[2 * m]; //convert into an edge list where the first parameter in the pair is the edge weight to facilitate sorting
-  int parent[n + 1], rank[n + 1];
-  for (int i = 0; i <= n; i++)
+    vector<Node>edges=make_edges(adj,n);
+    vector<int>par(n+1,0),rank(n+1,0);
+    
+    for(int i=0;i<=n;i++)
+    par[i]=i;
+    
+    int count=0;
+    long long ans=0;
+    for(auto it : edges)
     {
-      parent[i] = i;
-      rank[i] = 1;
+        int s=it.src,d=it.dest;
+        long long w=it.wt;
+        
+        if(find_par(s,par)==find_par(d,par))
+        continue;
+        
+        else
+        {
+            ans+=w;
+            count++;
+            DSU_union(s,d,par,rank);
+        }
+        if(count>=n)
+        break;
     }
-  int index = 0;
-  for (int i = 0; i <= n; i++)
-    {
-      for (int j = 0; j < adj[i].size (); j++)
-	{
-	  edges[index++] =
-	    make_pair (adj[i][j].second, make_pair (i, adj[i][j].first));
-	}
-    }
-  sort (edges, edges + index);
-  long long int mst = 0;
-  for (int i = 0; i < 2 * m; i++)
-    {
-      int a = edges[i].second.first;
-      int b = edges[i].second.second;
-      if (!same (a, b, parent, rank))
-	{
-	  union_Nodes (a, b, parent, rank);
-	  mst += edges[i].first;
-	}
-    }
-  return mst;
+    
+    return ans;
 }
 
 // { Driver Code Starts.
