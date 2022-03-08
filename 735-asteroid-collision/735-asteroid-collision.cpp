6c919cc9dff99c967d8d7085f7003020049f8592
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-09 01:47:52 +0530

    Time: 19 ms (56.63%), Space: 17.5 MB (85.16%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.cpp
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -6,25 +6,19 @@ public:
         for(int i: arr)
         {
             int ele=i;
-            
-            while(ele<0 and ans.size()>0 and ans.back()>0)
-            {
-                int top= ans.back();
-                ans.pop_back();
-                
-                if(top==abs(ele))
-                {
-                    ele=0;
-                    break;
-                }
-                
-                else if(top>abs(ele))
-                    ele=top;               
-                
-            }
-            
-            if(ele!=0)
+            if(ele>0)
                 ans.push_back(ele);
+            
+            else
+            {
+                while(ans.size() and ans.back()>0 and ans.back()<abs(ele))
+                    ans.pop_back();
+                if(ans.size()==0 or ans.back()<0)
+                    ans.push_back(ele);
+                else if(ans.back()==abs(ele))
+                    ans.pop_back();                   
+                    
+            }
         }
         
         return ans;
