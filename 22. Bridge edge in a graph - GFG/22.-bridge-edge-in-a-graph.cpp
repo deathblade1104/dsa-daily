43c6dbca0a5171cbeb6a047f4d18382eb543dd16
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-09 05:47:59 +0530

    Added solution - LeetHub

---
+++ 22. Bridge edge in a graph - GFG/22.-bridge-edge-in-a-graph.cpp
@@ -0,0 +1,70 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+    private:
+    
+    void DFS(int curr, int par, vector<bool>&vis , vector<int>adj[] , int &c, int &d)
+    {
+        if(vis[curr])
+        return;
+        
+        vis[curr]=true;
+        
+        for(int neigh : adj[curr])
+        {
+            if(neigh==par or  vis[neigh] or (curr==c and neigh==d))
+            continue;
+            
+            DFS(neigh,curr,vis,adj,c,d);
+        }
+        
+        return;
+    }
+  
+	public:
+    //Function to find if the given edge is a bridge in graph.
+    int isBridge(int V, vector<int> adj[], int c, int d) 
+    {
+        vector<bool>vis(V,false);
+        
+        DFS(c,-1,vis,adj,c,d);
+        
+        return (!vis[d]);
+        
+    }
+};
+
+// { Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back (v);
+            adj[v].push_back (u);
+        }
+        
+        int c,d;
+        cin>>c>>d;
+        
+        Solution obj;
+    	cout << obj.isBridge(V, adj, c, d) << "\n";
+    }
+
+    return 0;
+}
+
+  // } Driver Code Ends
