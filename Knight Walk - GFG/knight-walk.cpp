570f463881a741ea36eb0917b470cd3f3d1127f1
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-12 11:08:38 +0530

    Added solution - LeetHub

--- Knight Walk - GFG/knight-walk.cpp
+++ Knight Walk - GFG/knight-walk.cpp
@@ -9,38 +9,48 @@ using namespace std;
 
 class Solution {
 public:
-	int minStepToReachTarget(vector<int>&KnightPos, vector<int>&TargetPos, int N){
+	int minStepToReachTarget(vector<int>&KnightPos, vector<int>&TargetPos, int n){
 	    // Code here
 
-	    TargetPos[0]-=1,TargetPos[1]-=1;
+	    KnightPos[0]-=1;
+	    KnightPos[1]-=1;
+	    TargetPos[0]-=1;
+	    TargetPos[1]-=1;
+	    
 	    int ans=0;
-	    vector<vector<bool>>vis(N,vector<bool>(N,false));
-	    vector<vector<int>>dim={{-2,-1},{-2,+1},{+2,-1},{+2,+1},{-1,-2},{-1,+2},{+1,-2},{+1,+2}};
+	    vector<vector<bool>>vis(n,vector<bool>(n,false));
+	    
+	    
 	    queue<pair<int,int>>q;
-	    q.push({KnightPos[0]-1,KnightPos[1]-1});
-	    vis[KnightPos[0]-1][KnightPos[1]-1]=true;
+	    q.push({KnightPos[0],KnightPos[1]});
 	    
 	    while(q.size())
 	    {
 	        int sz=q.size();
 	        for(int i=0;i<sz;i++)
 	        {
-	            auto p=q.front();q.pop();
+	            auto p=q.front();
+	            q.pop();
+	            
 	            int r=p.first,c=p.second;
 	            
-	            for(int j=0;j<8;j++)
-	            {
-	                int x=dim[j][0] + r, y=dim[j][1]+c;
-	                
-	                if(x<0 or y<0 or x>=N or y>=N or vis[x][y]==true)
-	                continue;
-	                
-	                if(x==TargetPos[0] and y==TargetPos[1])
-	                return ans+1;
-	                
-	                q.push({x,y});
-	                vis[x][y]=true;
-	            }
+	            if(r<0 or r>=n or c<0 or c>=n or vis[r][c]==true)
+	            continue;
+	            
+	            if(r==TargetPos[0] and c==TargetPos[1])
+	            return ans;
+	            
+	            vis[r][c]=true;
+	            
+	            q.push({r-2,c-1});
+	            q.push({r-2,c+1});
+	            q.push({r+2,c-1});
+	            q.push({r+2,c+1});
+	            
+	            q.push({r-1,c-2});
+	            q.push({r-1,c+2});
+	            q.push({r+1,c-2});
+	            q.push({r+1,c+2});
 	        }
 	        ans++;
 	    }
