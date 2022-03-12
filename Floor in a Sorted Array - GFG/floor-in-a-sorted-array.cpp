dafc50ca3e3a844818263ff07dcebbdbd00daef6
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-12 07:13:50 +0530

    Added solution - LeetHub

---
+++ Floor in a Sorted Array - GFG/floor-in-a-sorted-array.cpp
@@ -0,0 +1,74 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+  private:
+  long long bs(vector<long long>v, long long n,long long x)
+  {
+      long long ans=INT_MIN,l=0,h=n-1;
+      
+      while(l<=h)
+      {
+          long long mid = l + ((h-l)/2);
+          
+          if(v[mid]>x)
+          h=mid-1;
+          
+          else if(v[mid]<=x)
+          {
+              ans=max(ans,mid);
+              l=mid+1;
+          }
+      }
+      return ans;
+  }
+  
+  public:
+    // Function to find floor of x
+    // n: size of vector
+    // x: element whose floor is to find
+    long long findFloor(vector<long long> v, long long n, long long x){
+        
+        // Your code here
+        if(v[0]>x)
+        return -1;
+        
+        return bs(v,n,x);
+        
+        
+        
+        
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+	
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    long long n;
+	    cin >> n;
+	    long long x;
+	    cin >> x;
+	    
+	    vector<long long> v;
+	    
+	    for(long long i = 0;i<n;i++){
+	        long long temp;
+	        cin >> temp;
+	        v.push_back(temp);
+	    }
+	    Solution obj;
+	    cout << obj.findFloor(v, n, x) << endl;
+	   
+	}
+	
+	return 0;
+}  // } Driver Code Ends
