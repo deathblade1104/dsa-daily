307f616bb39ff98bc7903ec38fa4dd945421138d
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-14 23:14:36 +0530

    Time: 746 ms (8.51%), Space: 231 MB (5.03%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,22 +1,9 @@
 class Solution {
-    vector<int>dim={1,0,-1,0,1};
-    void DFS(int r, int c, int&n ,int&m, vector<vector<int>>&grid1, vector<vector<int>>&grid2, bool&ans)
-    {
-        if(r<0 or c<0 or r>=n or c>=m or grid2[r][c]==0)
-            return;
-        
-        if((grid1[r][c] & grid2[r][c] )== 0)
-            ans=false;
-        
-        grid2[r][c]=0;
-        
-        for(int i=1;i<=4;i++)
-            DFS(r+dim[i-1],c+dim[i],n,m,grid1,grid2,ans);
-        
-    }
 public:
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        int dim[5]={1,0,-1,0,1};
         int n=grid2.size(),m=grid2[0].size(),count=0;
+        
         
         for(int i=0;i<n;i++)
         {
@@ -25,7 +12,26 @@ public:
                 if(grid2[i][j])
                 {
                     bool ans=true;
-                    DFS(i,j,n,m,grid1,grid2,ans);
+                    
+                    queue<pair<int,int>>q;
+                    q.push({i,j});
+                    
+                    while(q.size()>0)
+                    {
+                        auto p = q.front();q.pop();
+                        int r =p.first,c=p.second;
+                        
+                        if(r<0 or c<0 or r>=n or c>=m or grid2[r][c]==0)
+                            continue;
+                        
+                        if(grid1[r][c]==0 and grid2[r][c]==1)
+                            ans=false;
+                        
+                        grid2[r][c]=0;
+                        
+                        for(int k=1;k<=4;k++)
+                            q.push({r+dim[k-1],c+dim[k]});
+                    }
                     
                     if(ans)
                         count++;
