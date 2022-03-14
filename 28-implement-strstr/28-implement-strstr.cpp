d3cad57efdb639322adf0d21aacc067a08ce0fa7
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-14 23:59:19 +0530

    Time: 116 ms (46.58%), Space: 7.4 MB (34.16%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,7 +1,31 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        return haystack.find(needle);
+        
+        if(haystack==needle)
+            return 0;
+        
+        int sz=needle.size();
+        string temp="";
+        
+        for(int i=0;i<sz;i++)
+            temp+=haystack[i];
+        
+        
+        if(temp==needle)
+            return 0;
+        
+        for(int i=sz;i<haystack.size();i++)
+        {
+            temp.erase(temp.begin() + 0);
+            temp+=haystack[i];
+            
+            if(temp==needle)
+                return i-sz+1;
+        }
+        
+        return -1;
+        
         
     }
 };
