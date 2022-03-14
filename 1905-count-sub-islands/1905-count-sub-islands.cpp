fcc9a5bcdef8ad7b8fb4f958cc4baf6229b8074d
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-14 23:30:51 +0530

    Time: 400 ms (60.29%), Space: 219.6 MB (7.13%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,5 +1,37 @@
 class Solution {
+   
 public:
+    bool BFS(int i,int j, int &n ,int &m ,vector<vector<int>>&grid1,vector<vector<int>>&grid2,int dim[])
+    {
+            bool ans=true;
+
+            queue<pair<int,int>>q;
+            q.push({i,j});
+
+            while(q.size()>0)
+            {
+                auto p = q.front();q.pop();
+                int r =p.first,c=p.second;
+
+                if(r<0 or c<0 or r>=n or c>=m or grid2[r][c]==0)
+                    continue;                        
+
+                if(grid1[r][c]==0)
+                    ans=false;
+
+                grid2[r][c]=0;
+
+                for(int k=1;k<=4;k++)
+                {
+                    int x = r + dim[k-1], y = c + dim[k];
+
+                    if(x>=0 and x<n and y>=0 and y<m and grid2[x][y]==1 )
+                        q.push({x,y});
+                }
+            }
+        return ans;
+    }
+    
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         int dim[5]={1,0,-1,0,1};
         int n=grid2.size(),m=grid2[0].size(),count=0;        
@@ -8,40 +40,12 @@ public:
         {
             for(int j=0;j<m;j++)
             {
-                if(grid2[i][j])
-                {
-                    bool ans=true;
-                    
-                    queue<pair<int,int>>q;
-                    q.push({i,j});
-                    
-                    while(q.size()>0)
-                    {
-                        auto p = q.front();q.pop();
-                        int r =p.first,c=p.second;
-                        
-                        if(r<0 or c<0 or r>=n or c>=m or grid2[r][c]==0)
-                            continue;                        
-                        
-                        if(grid1[r][c]==0)
-                            ans=false;
-                        
-                        grid2[r][c]=0;
-                        
-                        for(int k=1;k<=4;k++)
-                        {
-                            int x = r + dim[k-1], y = c + dim[k];
-                            
-                            if(x>=0 and x<n and y>=0 and y<m and grid2[x][y]==1 )
-                                q.push({x,y});
-                        }
-                    }
-                    
-                    if(ans)
-                        count++;
-                }
+                if(grid2[i][j]==1 and BFS(i,j,n,m,grid1,grid2,dim)==true)
+                    count++;
+                
             }
         }
-        return count;
+    
+    return count;
     }
 };
