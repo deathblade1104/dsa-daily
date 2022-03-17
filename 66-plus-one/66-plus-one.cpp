5cf3f788049d3e4c359221d4a117b6b8b34ba65f
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-17 10:47:00 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (13.28%) - LeetHub

---
+++ 66-plus-one/66-plus-one.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        
+        reverse(digits.begin(),digits.end());        
+        int c=1;
+        for(int i=0;i<digits.size();i++)
+        {
+            int x = digits[i] + c;
+            if(x>=10)
+            {
+                digits[i]=x%10;
+                c=1;
+            }
+            else
+            {
+                digits[i]=x;
+                c=0;
+            }
+        }
+        
+        if(c==1)
+            digits.push_back(1);
+        
+        reverse(digits.begin(),digits.end());
+        return digits;
+        
+    }
+};
