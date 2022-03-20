48bc682d8132687a8b01c1fa4feade1eecabd4de
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-20 06:03:51 +0530

    Time: 336 ms (73.33%), Space: 112.8 MB (77.34%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    void DFS(int curr,int prevTime, int &ans,vector<int>adj[],vector<int>&inform)
+    {
+        int currTime = prevTime + inform[curr];
+        inform[curr]=-1;
+        
+        ans=max(ans,currTime);
+        
+        for(int x : adj[curr])
+        {            
+            if(inform[x]!=-1)
+                DFS(x,currTime,ans,adj,inform);
+        }
+        return;
+        
+    }
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& inform) {
+        
+        vector<int>adj[n];
+        for(int i=0;i<n;i++)
+        {
+            if(manager[i]!=-1)
+                adj[manager[i]].push_back(i);
+        }
+        
+        int ans=0;
+        DFS(headID,0,ans,adj,inform);
+        return ans;
+    }
+};
