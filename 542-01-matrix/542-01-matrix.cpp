e20a89078a9baac62496f9978d6e010899662077
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-21 08:42:49 +0530

    Time: 194 ms (13.09%), Space: 31.7 MB (26.22%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -14,31 +14,40 @@ public:
       int n=grid.size(),m=grid[0].size(),cells=0;
       queue<Node>q;
       int dim[5]={1,0,-1,0,1};
-      vector<vector<int>>ans(n,vector<int>(m,-1));
+      vector<vector<int>>ans(n,vector<int>(m,-2));
         
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(grid[i][j]==0)
-                    q.push(Node(i,j,0));
+                {
+                    for(int k=1;k<=4;k++)
+                    {
+                        int  x = i + dim[k-1] , y = j + dim[k];
+                        if(x>=0 and y>=0 and x<n and y<m and grid[x][y]==1 and ans[x][y]==-2)  
+                        {
+                            q.push(Node(x,y,1));
+                            ans[x][y]=-1;
+                        }
+                    }
+                    ans[i][j]=0;
+                }
             }
         }
         
         
-        while(q.size()>0 and cells<(n*m))
+        while(q.size()>0)
         {
             auto p = q.front();
             q.pop();
             
             int row=p.r,col=p.c,d=p.dist;
             
-            if(ans[row][col]!=-1)
+            if(ans[row][col]>=0)
                 continue;
             
-            ans[row][col]=d;
-            cells++;
-            
+            ans[row][col]=d;          
             
             for(int i=1;i<=4;i++)
             {
