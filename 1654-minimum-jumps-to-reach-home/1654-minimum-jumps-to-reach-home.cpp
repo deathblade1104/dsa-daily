1b4c93cf08eddbc6d7b54189229b4936133f1b85
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-22 15:13:38 +0530

    Time: 28 ms (91.40%), Space: 17.6 MB (86.80%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -1,53 +1,36 @@
 class Solution {
 public:
-    int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
+    int minimumJumps(vector<int>& f, int a, int b, int x) {
         
-        if(a==1999 and b==2000 and x==2000)
-            return 3998;
-        
-        bool vis[6005][2]={false};
-        
-        for(int i : forbidden)
-            vis[i][0]=1,vis[i][1]=1;
-        
-        
-        vis[0][0]=1,vis[0][1]=1;
-        
-        queue<pair<int,int>>q;
-        q.push({0,0});
-        
-        int steps=0;
-        while(q.size()>0)
-        {
-            int sz=q.size();
-            for(int i=0;i<sz;i++)
-            {
-                int curr=q.front().first;
-                int flag=q.front().second;
-                q.pop();
-                
-                
-                if(curr==x)
-                    return steps;
-                
-                int forward= curr + a;
-                if(forward <=5000 and vis[forward][0]==false)
-                {
-                    q.push({forward,0});
-                    vis[forward][0]=true;
-                }
-                
-                int backward=curr-b;
-                if(backward>=0 and flag==0 and vis[backward][1]==false)
-                {
-                    q.push({backward,1});
-                    vis[backward][1]=true;
-                }
-            }
-            steps++;
-        }
-        
-        return -1;
+        vector < int  > v(10000 , 0);
+         for(int i = 0; i< f.size(); i++){
+             v[f[i]] =1;
+             }
+           queue <  pair < int  , int  >> q;
+            q.push({0, 1});
+         int ans = 0;
+          while(q.size() > 0){
+               int s =  q.size();
+               while(s--){
+                     auto node =  q.front();
+                    q.pop();
+                    int index  =   node.first;
+                     int prev =  node.second;
+                      if(index ==  x) return ans;
+                     if(v[index] == 1) continue;
+                       v[index] =1;
+                    if(prev == 1 && (index-b)   >=  0){
+                        q.push({(index-b) , 0}); 
+                     }
+                   if((index+a) <= 6000){
+                           q.push({(index+a) , 1});
+                      }
+                    
+                      
+               }
+                ans++;
+          }
+         return -1;
     }
     
 };
