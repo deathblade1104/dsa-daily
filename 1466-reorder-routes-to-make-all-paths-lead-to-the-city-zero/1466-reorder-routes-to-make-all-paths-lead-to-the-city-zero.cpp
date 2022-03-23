92e0934cc346c061cb84232a78249ce380056a02
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-23 11:50:43 +0530

    Time: 496 ms (66.29%), Space: 132.3 MB (27.99%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,8 +1,26 @@
 class Solution {
+private :
+    void DFS(int curr, int par, int &ans,vector<bool>&vis, set<pair<int,int>>&edges, vector<int>adj[])
+    {
+        for(auto neigh : adj[curr])
+        {
+            if(!vis[neigh])
+            {
+                pair<int,int>p=make_pair(neigh,curr);
+                if(edges.count(p)==0)
+                    ans++;
+                
+                vis[neigh]=true;
+                DFS(neigh,curr, ans,vis,edges,adj);
+            }
+        }
+        
+        return;
+    }
 public:
     int minReorder(int n, vector<vector<int>>& connections) {
         vector<int>adj[n];
-        set<pair<int,int>>edges;
+        set<pair<int,int>>edges;       
         vector<bool>vis(n,false);
         
         for(int i =0;i<connections.size();i++)
@@ -11,38 +29,15 @@ public:
             int u=p[0],v=p[1];
             
             adj[u].push_back(v);
-            edges.insert({u,v});
-            
+            edges.insert(make_pair(u,v));        
             
             adj[v].push_back(u);
         }
         
-        queue<pair<int,int>>q;
-        q.push({0,-1});
         vis[0]=true;
         
         int ans=0;
-        
-        while(q.size()>0)
-        {
-            auto [c,p]= q.front();
-            q.pop();
-                        
-            for(auto neigh : adj[c])
-            {
-                if(!vis[neigh])
-                {
-                    pair<int,int>p=make_pair(neigh,c);
-                    
-                    if(edges.count(p)==0)
-                        ans++;
-                    
-                    q.push({neigh,c});
-                    vis[neigh]=true;
-                }
-            }                      
-            
-        }
+        DFS(0,-1,ans,vis,edges,adj);
         
         return ans;
         
