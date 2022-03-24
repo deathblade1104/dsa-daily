018a4fd6410882fac3cd769ee314641efcb8f39d
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-24 22:21:10 +0530

    Time: 325 ms (35.61%), Space: 17 MB (41.99%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dp[10001];
+    int get_ans(int curr,int &sz,vector<int>&nums)
+    {
+        if(curr>=sz-1)
+            return 0;
+        
+        if(dp[curr]!=-1)
+            return dp[curr];
+        
+        int ans=(INT_MAX/2);
+        
+        for(int i=nums[curr];i>=1;i--)
+        {
+            int temp = 1 + get_ans(curr+i,sz,nums);
+            ans=min(ans,temp);
+        }
+        
+        return dp[curr]=ans;
+    }
+    int jump(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        int sz= nums.size();
+        
+        return get_ans(0,sz,nums);
+    }
+};
