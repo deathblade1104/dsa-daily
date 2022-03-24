2abd6d71fd68c7d0465d15d98d8bbaceaf17035c
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-25 01:11:57 +0530

    Time: 545 ms (57.86%), Space: 112.8 MB (60.02%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,4 +1,22 @@
 class Solution {
+private:
+    void DFS(int curr, int &ans, vector<bool>&vis,vector<pair<int,bool>>adj[])
+    {
+        
+        for(auto neigh : adj[curr])
+        {
+            if(!vis[neigh.first])
+            {
+                if(neigh.second==true)  
+                    ans++;
+
+                vis[neigh.first]=true;
+                DFS(neigh.first,ans,vis,adj);
+            }
+         }
+            
+        return;
+    }
 
 public:
     int minReorder(int n, vector<vector<int>>& connections) {
@@ -16,29 +34,7 @@ public:
         
         vis[0]=true;
         int ans=0;
-        
-        queue<int>q;
-        q.push(0);
-        
-        while(q.size()>0)
-        {
-            
-            auto curr= q.front();
-            q.pop();
-            
-             for(auto neigh : adj[curr])
-             {
-                if(!vis[neigh.first])
-                {
-                    if(neigh.second==true)  
-                        ans++;
-
-                    vis[neigh.first]=true;
-                    q.push(neigh.first);
-                }
-             }
-        }
-        
+        DFS(0,ans,vis,adj);
         return ans;
         
     }
