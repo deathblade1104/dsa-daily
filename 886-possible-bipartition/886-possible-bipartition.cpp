a277109d954336ce9deee7b83fbe3b13b9d7be54
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-25 09:47:46 +0530

    Time: 373 ms (22.75%), Space: 71.1 MB (39.87%) - LeetHub

---
+++ 886-possible-bipartition/886-possible-bipartition.cpp
@@ -0,0 +1,43 @@
+class Solution {
+private :
+    bool DFS(int curr, int color, vector<int>&vis, vector<int>adj[])
+    {
+        vis[curr]=color;
+        
+        bool ans= true;
+        
+        for(int neigh : adj[curr])
+        {
+            if(vis[neigh]!=-1 and vis[neigh]==color)
+                return false;
+            
+            else if(vis[neigh]==-1)
+                ans = ans & DFS(neigh,!color,vis,adj);
+            
+           if(ans==false)
+               return false;
+                
+        }
+        
+        return true;
+    }
+    
+public:
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        vector<int>vis(n+1,-1);
+        vector<int>adj[1+n];
+        
+        for(auto edge : dislikes)
+        {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+        
+            
+        for(int i=1;i<=n;i++)
+            if((vis[i]==-1) and (DFS(i,1,vis,adj)==false))   
+                    return false;
+        
+        return true;
+    }
+};
