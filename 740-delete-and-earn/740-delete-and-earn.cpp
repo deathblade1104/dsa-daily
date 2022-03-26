a199df63ba2917497eeb7dcc95f618a0939d760e
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-26 19:41:14 +0530

    Time: 21 ms (21.22%), Space: 12.6 MB (31.41%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,34 +1,39 @@
 class Solution {
 private:
-    int get_ans(int curr, int &sz, int freq[], vector<int>&dp)
+    int count[100001];
+    int dp[100001];
+    
+    int get_ans(int curr,int sz)
     {
         if(curr>sz)
-        return 0;
+            return 0;
         
         if(dp[curr]!=-1)
-        return dp[curr];
+            return dp[curr];
         
-        int consider=freq[curr]*curr + get_ans(curr+2,sz,freq,dp);        
-        int dont_consider= get_ans(curr+1,sz,freq,dp);
+        int consider= (count[curr]*curr) + get_ans(curr+2,sz);
+        int dont = get_ans(curr+1,sz);
         
-        dp[curr]=max(consider,dont_consider);
         
-        return dp[curr];
+        return dp[curr] = max(consider,dont);       
     }
-    
 public:
     int deleteAndEarn(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        memset(count,0,sizeof(count));
+    
+        int start=INT_MAX,end=INT_MIN;
         
-        int m=-1;
-        int freq[10001]={0};
-                
         for(int i=0;i<nums.size();i++)
         {
-            freq[nums[i]]++;
-            m=max(m,nums[i]);
-        }        
-        vector<int>dp(m+1,-1);
-        return get_ans(0,m,freq,dp);
+            start=min(start,nums[i]);
+            end= max(end,nums[i]);
+            
+            count[nums[i]]++;
+        }
+        
+        return get_ans(start,end);
+        
         
         
     }
