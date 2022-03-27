b3a7f82c5e6ed666d7102bbdedc12875891e126d
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-27 19:41:03 +0530

    Time: 1768 ms (5.02%), Space: 386.9 MB (5.04%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int dp[300][5001];
+    int getans(vector<int>arr,int curr,int amt,int n)
+    {
+        if(amt ==0)
+            return 1;
+        
+        if(curr>=n)
+            return 0;
+        
+        if(dp[curr][amt]!=-1)
+            return dp[curr][amt];
+        
+        int same=0;
+        
+        if(arr[curr]<=amt)
+            same=getans(arr,curr,amt-arr[curr],n);
+        
+        int not_same=getans(arr,curr+1,amt,n);
+        
+        dp[curr][amt]=same + not_same;
+        
+        return dp[curr][amt];       
+        
+    }
+    
+    int change(int amount, vector<int>& coins) {
+        
+        int n=coins.size();
+        
+        for(int i=0;i<n;i++)
+            for(int j=0;j<=amount;j++)
+                dp[i][j]=-1;
+        
+        
+        int ans=getans(coins,0,amount,n);
+    
+        return ans;
+        
+    }
+};
