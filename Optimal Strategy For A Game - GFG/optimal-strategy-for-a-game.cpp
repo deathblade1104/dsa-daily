80f003d3b327d22b06c739f29dae86bdbab927f5
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-29 15:35:53 +0530

    Added solution - LeetHub

--- Optimal Strategy For A Game - GFG/optimal-strategy-for-a-game.cpp
+++ Optimal Strategy For A Game - GFG/optimal-strategy-for-a-game.cpp
@@ -9,13 +9,13 @@ class Solution{
     public:
     long long get_ans(int i,int j, int arr[],vector<vector<long long>>&dp)
     {
-        if(i>j)
-        return dp[i][j]=0;
+        // if(i>j)
+        // return dp[i][j]=0;
         
         // else if(i==j)
         // return dp[i][j]= arr[i];
         
-        else if((i+1)==j)
+        if((i+1)==j)
         return dp[i][j]=max(arr[i],arr[j]);
         
         if(dp[i][j]!=-1)
