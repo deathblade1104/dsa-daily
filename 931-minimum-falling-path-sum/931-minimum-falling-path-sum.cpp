c7ac6cdc5737ebbec3226031be8bdd5415e0eb17
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-30 19:52:21 +0530

    Time: 16 ms (71.74%), Space: 10.3 MB (21.92%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int dp[101][101];
-    int get_ans(int r, int c, int &n,int &m, vector<vector<int>>& matrix,vector<vector<int>>&dp)
+
+    int get_ans(int r, int c, int &n,vector<vector<int>>& matrix,vector<vector<int>>& dp)
     {
-        if(r>=n or c<0 or c>=m) return INT_MAX/2;
+        if(r>=n or c<0 or c>=n) return INT_MAX/2;
         
-        if(r==n-1 and (c>=0 and c<=m-1))
+        if(r==n-1 and (c>=0 and c<=n-1))
             return dp[r][c] = matrix[r][c];
         
         if(dp[r][c]!=INT_MAX/2)
@@ -14,29 +14,19 @@ public:
         int op1,op2,op3;
         op1=op2=op3=matrix[r][c];
         
-        op1+= get_ans(r+1,c-1,n,m,matrix,dp);
-        op2+= get_ans(r+1,c,n,m,matrix,dp);
-        op3+= get_ans(r+1,c+1,n,m,matrix,dp);
+        op1+= get_ans(r+1,c-1,n,matrix,dp);
+        op2+= get_ans(r+1,c,n,matrix,dp);
+        op3+= get_ans(r+1,c+1,n,matrix,dp);
         
         return dp[r][c]=min({op1,op2,op3});
     }
     
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n=matrix.size();
+        int n=matrix.size(),ans=INT_MAX;
         vector<vector<int>>dp(n,vector<int>(n,INT_MAX/2));
-        
-        int ans=INT_MAX;        
+            
         for(int i=0;i<n;i++)
-            ans=min(ans,get_ans(0,i,n,n,matrix,dp));
-        
-       for(int i=0;i<n;i++)
-       {
-           for(int j=0;j<n;j++)
-               cout<<dp[i][j]<<" ";
-           
-           cout<<endl;
-       }
-    
+            ans=min(ans,get_ans(0,i,n,matrix,dp));    
         return ans;
         
     }
