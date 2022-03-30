03381cd2cf94bb53da384168e8fba937ed998c40
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-03-30 13:22:59 +0530

    Time: 87 ms (58.41%), Space: 10.3 MB (77.66%) - LeetHub

---
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int dp[10001][101];
+    int get_ans(int floor, int eggs)
+    {
+        if(floor==0 and eggs>0) return dp[floor][eggs]=0;
+        else if (floor==1 and eggs>0) return dp[floor][eggs]=1;
+        else if(eggs==0) return dp[floor][eggs]=INT_MAX/2;    
+        else if(eggs==1 and floor>0) return dp[floor][eggs]=floor;
+        
+        if(dp[floor][eggs]!=-1) return dp[floor][eggs];
+        
+        int res =INT_MAX,low=0,high=floor;
+        
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            
+            int left = get_ans(mid-1,eggs-1);
+            int right = get_ans(floor-mid,eggs);
+            
+            int temp =  1 + max(left,right);
+            
+            if(left<right)
+                low=mid+1;
+            
+            else
+                high=mid-1;
+            
+            res=min(res,temp);
+        }
+        
+        return dp[floor][eggs]=res;           
+    }
+    
+    int superEggDrop(int eggs, int floors) {
+        memset(dp,-1,10001*101*sizeof(int));
+        
+        return get_ans(floors,eggs);      
+       
+    }
+};
