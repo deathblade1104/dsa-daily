1726fb67e67af3b80e894e23cb2b60fd3fb2a7b6
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-01 20:02:09 +0530

    Time: 24 ms (77.34%), Space: 6 MB (88.01%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dp[31][1001];
+    int mod = 1000000007;
+    int get_ans(int dice, int target,int &k)
+    {
+        if(dice==0)
+        {
+            if(target==0) return 1;
+            else return 0;
+        }
+        
+        if(dp[dice][target]!=-1)
+            return dp[dice][target];
+        
+        int ans=0;        
+        for(int i=1;i<=min(k,target);i++)
+        ans=(ans%mod + get_ans(dice-1,target-i,k)%mod)%mod;        
+        
+        return dp[dice][target]=ans;
+        
+    }
+    int numRollsToTarget(int n, int k, int target) {
+        memset(dp,-1,31*1001*sizeof(int));
+        
+        return get_ans(n,target,k);
+    }
+};
