e63cefad90225e8b291e28dd8bd0b899e5777fc1
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-04 19:01:39 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (48.27%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -2,27 +2,25 @@ class Solution {
 public:
     int get_sum(int n)
     {
-        return (n * (n+1))/2;
+        int ans = ((n+1)*n)/2;
+        return ans;
     }
-    
     int numberOfArithmeticSlices(vector<int>& nums) {
-        if(nums.size()<3)
-            return 0;
-        int ans=0,count=0;
-        
+        int ans=0,count = 0;
         for(int i=2;i<nums.size();i++)
         {
-            if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2])
+            if((nums[i]-nums[i-1])== (nums[i-1] - nums[i-2]))
                 count++;
             
             else
             {
+                
                 ans+=get_sum(count);
-                count=0;
+                count=0;               
             }
         }
-        
         ans+=get_sum(count);
         return ans;
+        
     }
 };
