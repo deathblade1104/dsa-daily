e313e7a9a2ec92b644027e2d1fff8088c687192b
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-06 14:55:32 +0530

    Time: 42 ms (38.33%), Space: 13.9 MB (46.63%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     int dp[5001][301];
-    int get_ans(int amt,int curr, vector<int>&coins)
+    int get_ans(int amount , int curr, vector<int>&coins)
     {
-        if(amt<0 or curr>=coins.size())
+        if(curr>=coins.size())
             return 0;
         
-        if(amt==0)
-            return dp[0][curr]=1;
-       
-        if(dp[amt][curr]!=-1)
-            return dp[amt][curr];
+        if(amount == 0)
+            return 1;         
+            
+        if(dp[amount][curr]!=-1)
+            return dp[amount][curr];
         
-        int op1=0,op2=0;
+        int op1 = 0;
         
-        if(coins[curr]<=amt)
-            op1 = get_ans(amt-coins[curr],curr,coins);
+        if(coins[curr]<=amount)
+            op1 = get_ans(amount-coins[curr],curr,coins);
         
-        op2= get_ans(amt,curr+1,coins);
-        
-        return dp[amt][curr] = op1 + op2;
+        int op2= get_ans(amount,curr+1,coins);
+        return dp[amount][curr] = op1 + op2;
     }
     
     int change(int amount, vector<int>& coins) {
+        sort(coins.begin(),coins.end());
         memset(dp,-1,5001*301*sizeof(int));
         
-        sort(coins.begin(),coins.end());
         return get_ans(amount,0,coins);
     }
 };
