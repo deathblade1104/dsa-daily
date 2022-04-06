d61a9d95f12e10d6eeeda3755f2a2de542b0ca20
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-06 14:35:36 +0530

    Time: 68 ms (87.78%), Space: 10.7 MB (78.46%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,47 +1,40 @@
 class Solution {
 public:
     int dp[10001];
-    int coinChange(vector<int>& coins, int amt) {
-        memset(dp,10000,10001*sizeof(int));        
-        int n=coins.size();
-        sort(coins.begin(),coins.end());
-        
-        if(amt == 0 )
+    int get_ans(int curr,vector<int>&coins)
+    {
+        if(curr==0)
             return 0;
         
-        if(coins[0]>amt)
-            return -1;
+        if(dp[curr]!=-1)
+            return dp[curr];
         
-        for(int i=0;i<n;i++)
+        int ans=INT_MAX/2;
+        for(int i=0;i<coins.size();i++)
         {
-            if(coins[i]<=amt)
-                dp[coins[i]]=1;
+            int temp=INT_MAX/2;
+            if(coins[i]<=curr)
+               temp = 1 + get_ans(curr-coins[i],coins);
             
-            else break;
+            
+            ans=min(ans,temp);
+            
+            if(coins[i]>curr)
+                break;
         }
         
-        dp[0]=0;       
-        for(int i=1;i<=amt;i++)
-        {
-            if(dp[i]!=10001)
-            {
-                int temp =10001;
-                for(int j=0; j<n;j++)
-                {
-                    if(coins[j]<=i)
-                    temp = min(temp,dp[i-coins[j]]);
-                    
-                    else
-                    break;
-                }
-                
-                dp[i] = temp + 1;
-            }
-        }
+        return dp[curr]=ans;
+    }
+    
+    int coinChange(vector<int>& coins, int amount) {
+        sort(coins.begin(),coins.end());
+        memset(dp,-1, sizeof(dp));
         
-        if(dp[amt]>=10000)
+        int ans = get_ans(amount,coins);
+        
+        if(ans>=INT_MAX/2)
             return -1;
         
-        return dp[amt];
+        return ans;
     }
 };
