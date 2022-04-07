fa58f99ec998e2e82d3b75b0fe86e1b9be601ac6
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-08 03:23:15 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (75.10%) - LeetHub

---
+++ 343-integer-break/343-integer-break.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dp[59];
+    int get_ans(int n)
+    {       
+        if(n<=1)
+            return 1;
+        
+        if(dp[n]!=-1)
+            return dp[n];
+        
+        int ans=1,temp=1;
+        for(int i=2;i<n;i++)
+        {
+            temp= get_ans(n-i) * get_ans(i);
+            ans=max(temp,ans);            
+        }
+        
+        return dp[n]=max(ans,n);
+    }
+    int integerBreak(int n) {
+        memset(dp,-1,sizeof(dp));
+        
+        if(n<=3)
+            return n-1;
+        return get_ans(n); 
+    }
+};
