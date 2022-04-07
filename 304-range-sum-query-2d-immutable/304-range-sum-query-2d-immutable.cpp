2e9a29c8af7a632821aff5cb813c5e17c8b5c23d
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-08 01:06:53 +0530

    Time: 422 ms (84.79%), Space: 149.3 MB (20.21%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -1,28 +1,24 @@
 class NumMatrix {
-    private:
-    vector<vector<int>>ps;
-
-    public:
+public:
+    int ps[201][201];
     NumMatrix(vector<vector<int>>& matrix) {
-        int n=matrix.size(),m=matrix[0].size();
-        vector<vector<int>>p(n+1,vector<int>(m+1,0));
+        memset(ps,0,201*201*sizeof(int));
+        
+        int n=matrix.size(), m=matrix[0].size();
         
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
-                p[i][j] = matrix[i-1][j-1] + p[i-1][j] + p[i][j-1] - p[i-1][j-1];
+                ps[i][j] = ps[i-1][j] + ps[i][j-1] - ps[i-1][j-1] + matrix[i-1][j-1];
         }
-        ps=p;
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        row2+=1;
-        row1+=1;
-        col1+=1;
-        col2+=1;
         
         
-        return ps[row2][col2] +ps[row1-1][col1-1] - ps[row2][col1-1] - ps[row1-1][col2];
+        row1++;row2++;col2++;col1++;
+        
+        return ps[row2][col2] - ps[row2][col1-1] - ps[row1-1][col2] + ps[row1-1][col1-1];
     }
 };
 
