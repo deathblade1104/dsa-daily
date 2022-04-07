43088560e70db411e9f62eb0e3aa4c6567c34a4d
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-08 01:11:58 +0530

    Time: 13 ms (58.38%), Space: 10.6 MB (57.01%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -5,7 +5,6 @@ public:
         if(s.size()==0 or st.count(s)==1) return mp[s]=true;
         if(mp.count(s)==1) return mp[s];
         
-        
         string temp="";
         int i=0;
 
@@ -17,18 +16,18 @@ public:
                 string str = s.substr(i);
                 if(get_ans(str,st,mp))
                     return mp[s]=mp[temp]=true;
+                
+                else
+                    mp[str]=false;
             }               
         }
 
-        return mp[s]=false;
-        
-            
+        return mp[s]=false;       
     }
     
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string>st(wordDict.begin(),wordDict.end());
         unordered_map<string,bool>mp;
-        
         return get_ans(s,st,mp);
     }
 };
