3dacf17c9a71fdf327448b2ade9dbb7e4e031018
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-08 01:36:49 +0530

    Time: 115 ms (76.62%), Space: 6.1 MB (85.98%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,48 +1,29 @@
 class Solution {
 public:
-    int dp[101][10001];	
-	int getans(vector<int>&v,int curr,int sz,int n)
-	{
-	    if(n==0)
-	    return 0;
-	    
-	    if(curr>=sz)
-	    return 10001;
-	    
-	   if(dp[curr][n]!=-1)
-	   return dp[curr][n];
-	    
-	    int same=10001;
-	    
-	    if(v[curr]<=n)
-	    same = 1 + getans(v,curr,sz,n-v[curr]);
-	    
-	    int not_same = getans(v,curr+1,sz,n);
-	    
-	    dp[curr][n]= min(same,not_same);
-	    
-	    return dp[curr][n];
-	}
+    int dp[10001];
+    int get_ans(int curr)
+    {
+        if(curr<=3)
+            return dp[curr]=curr;
+        
+        if(dp[curr]!=-1)
+            return dp[curr];
+        
+        int ans = curr;
+        for(int i=2;i*i<=curr;i++)
+        {
+            int temp = 1 + get_ans(curr-(i*i));
+            ans=min(ans,temp);
+            
+        }
+        
+        return dp[curr]=ans;
+    }
+    
     
     int numSquares(int n)
     {
-       int lim;
-	    lim=floor(sqrt(n));
-	    
-	    vector<int>v;
-	    
-	    for(int i=1;i<=lim;i++)
-	    v.push_back(i*i);
-	    
-	    int sz=v.size();
-	    
-	    for(int i=0;i<sz;i++)
-	        for(int j=0;j<=n;j++)
-	            dp[i][j]=-1;
-	  
-	    
-	    int ans= getans(v,0,sz,n);
-	    
-	    return ans;
+        memset(dp,-1,sizeof(dp));        
+        return get_ans(n);
     }
 };
