fa40c63891678fa0668d4a0e7f3b66b719b03cef
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-09 20:21:16 +0530

    Time: 542 ms (20.31%), Space: 80.9 MB (16.39%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -3,35 +3,27 @@ public:
     struct mycomp{
         bool operator()(const vector<int>&a, const vector<int>&b)
         {
-            return a[0]<b[0];
+            return ((a[0]*a[0]) + (a[1]*a[1])) < ((b[0]*b[0]) + (b[1]*b[1]));
         }
     };
-    
-    int get_distance(int&r, int&c)
-    {
-        int x = (r*r) + (c*c);
-        return x;
-    }
-    
+        
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         
         priority_queue<vector<int>, vector<vector<int>> , mycomp>pq;
         
         for(auto i : points)
         {
-            int dist= get_distance(i[0],i[1]);
-            pq.push({dist,i[0],i[1]});
+            pq.push({i[0],i[1]});
             
             if(pq.size()>k)
                 pq.pop();
         }
         
         vector<vector<int>>ans;
-        
         while(pq.size()>0)
         {
             auto p = pq.top(); pq.pop();
-            ans.push_back({p[1],p[2]});
+            ans.push_back({p[0],p[1]});
         }
         
         return ans;
