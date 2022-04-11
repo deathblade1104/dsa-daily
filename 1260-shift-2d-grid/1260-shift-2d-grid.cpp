aa3344273625e94465ea06978ab47610919a1407
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-12 03:35:17 +0530

    Time: 45 ms (48.42%), Space: 13.8 MB (97.42%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    pair<int,int> get_dimension(int ele , int&n, int&m)
+    {
+        int r=ele/m, c=ele%m;
+        
+        if(m==1)
+            r=ele;
+        
+        if(n==1)
+            c=ele;
+        
+        pair<int,int>p=make_pair(r,c);
+        return p;
+        
+    }
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        
+        int r= grid.size(), c= grid[0].size();
+        
+        if(k>=(r*c))
+        k = k%(r*c);
+        
+        if(k==0)return grid;      
+        vector<vector<int>>ans(r,vector<int>(c));
+        
+        int e_org=0,e=0;
+        
+        while(e_org < ((r*c)-k))
+        {            
+            auto p1 = get_dimension(e_org, r,c);
+            auto p2 = get_dimension(e_org+k,r,c);
+            
+            ans[p2.first][p2.second] = grid[p1.first][p1.second];           
+            e_org++;
+        }
+        
+        while(e_org<(r*c))
+        {
+            auto p1 = get_dimension(e_org, r,c);
+            auto p2 = get_dimension(e,r,c);
+            
+            ans[p2.first][p2.second] = grid[p1.first][p1.second];
+            e++;
+            e_org++;
+        }
+        
+        return ans;
+        
+    }
+};
