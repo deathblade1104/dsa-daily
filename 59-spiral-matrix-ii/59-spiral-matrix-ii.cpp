736aeab602ebafb0ab4bb92e1d49037055e70f22
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-13 07:11:11 +0530

    Time: 4 ms (19.27%), Space: 6.5 MB (85.87%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    void solve(int sz, int r1, int c1, int r2, int c2,int &val, vector<vector<int>>&ans)
+    void solve(int r1, int c1, int r2, int c2,int &val, vector<vector<int>>&ans)
     {
-        if(sz==0)
+        if(r1>r2 or c1>c2)
             return;
         
-        if(sz==1)
+        if(r1==r2 and c1==c2)
         {
             ans[r1][c1]=val;
             return;
@@ -15,8 +15,7 @@ public:
             ans[r1][i]=val++;
         
         for(int i=r1+1;i<=r2;i++)
-            ans[i][c2]=val++;
-        
+            ans[i][c2]=val++;        
         
         for(int i=c2-1;i>=c1;i--)
             ans[r2][i]=val++;
@@ -24,7 +23,7 @@ public:
         for(int i=r2-1;i>r1;i--)
             ans[i][c1]=val++;
         
-        solve(sz-2,r1+1,c1+1,r2-1,c2-1,val,ans);
+        solve(r1+1,c1+1,r2-1,c2-1,val,ans);
         return;      
         
         
@@ -35,7 +34,7 @@ public:
         
         vector<vector<int>>ans(n,vector<int>(n,-1));
         int val=1;
-        solve(n,0,0,n-1,n-1,val,ans);
+        solve(0,0,n-1,n-1,val,ans);
         
         return ans;
         
