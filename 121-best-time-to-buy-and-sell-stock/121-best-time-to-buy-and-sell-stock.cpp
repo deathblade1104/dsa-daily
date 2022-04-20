15331a7105576014dfd57a1729e41116c5a3dc4b
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-20 07:15:19 +0530

    Time: 143 ms (75.12%), Space: 97.4 MB (6.34%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
-    int dp[100001][2];
-    
-    int get_ans(int curr,int can_buy, int tc, int n , vector<int>&prices)
-    {
-        if(curr>=n or tc <=0)
-            return 0;
-        
-        if(dp[curr][can_buy]!=-1)
-            return dp[curr][can_buy];
-        
-        int idle = get_ans(curr+1,can_buy,tc,n,prices),buy=0,sell=0;
-        
-        if(can_buy)
-            buy =  get_ans(curr+1,0,tc,n,prices) - prices[curr];
-        
-        else
-            sell = prices[curr] + get_ans(curr+1,1,tc-1,n,prices);
-        
-        return dp[curr][can_buy] = max({idle,buy,sell});
-        
-    }
     int maxProfit(vector<int>& prices) {
-        memset(dp,-1,100001*2*sizeof(int));
-        int n=prices.size();
-        
-        return get_ans(0,1,1,n,prices);
+        vector<int>dp(prices.size());
+        dp[prices.size()-1]=INT_MIN/2;
         
         
+        for(int i=prices.size()-2;i>=0;i--)
+        dp[i]= max(prices[i+1], dp[i+1]);
         
+        
+        int ans=0;
+        
+        for(int i = 0;i<dp.size();i++)
+        ans=max(ans, dp[i]-prices[i]);
+        
+        return ans;
     }
 };
