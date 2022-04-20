a897ec60f8403cea9e342486408ba7c396fd0ee8
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-20 13:45:40 +0530

    Time: 48 ms (45.60%), Space: 58 MB (47.16%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -11,36 +11,32 @@
  */
 class Solution {
 public:
-    void get_tree(TreeNode *root, vector<int>&v)
+    TreeNode *prev=NULL,*first=NULL,*second=NULL;
+    
+    void get_ans(TreeNode *root)
     {
         if(!root)
             return;
         
-        get_tree(root->left,v);
-        v.push_back(root->val);
-        get_tree(root->right,v);
+        get_ans(root->left);
+        if(prev!=NULL and root->val<prev->val)
+        {
+            if(!first)
+                first=prev;
+
+            second = root;
+        }        
+        prev= root;
+        get_ans(root->right);
+        
         return;
     }
-    void buildBST(TreeNode *root, int&i,vector<int>&v)
-    {
-        if(!root or i>=v.size())
-            return;
-        
-        buildBST(root->left,i,v);
-        
-        if(i<v.size())
-            root->val=v[i++];
-        
-        buildBST(root->right,i,v);
-        return;
-    }
+    
     void recoverTree(TreeNode* root) {
+      
+        get_ans(root);
+        swap(first->val,second->val);
         
-        vector<int>v;
-        get_tree(root,v);
-        sort(v.begin(),v.end());
-        int i=0;
-        buildBST(root,i,v);
         return;
     }
 };
