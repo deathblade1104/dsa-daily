8b500007bb91cb03bc56bbf68f42cdcbea793dc7
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-21 05:55:31 +0530

    Time: 4 ms (86.77%), Space: 7.6 MB (36.44%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string get_string(string&s)
+    {
+        string ans="";
+        
+        for(char ch : s)
+        {
+            if(isalpha(ch))
+            {
+                if((int)ch <97)
+                {
+                    int x = (int)ch + 32;
+                    ans+=(char)x;
+                }
+                
+                else
+                ans+=ch;
+            }
+            
+            else if(isdigit(ch))
+                ans+=ch;
+        }
+        
+        return ans;
+    }
+    bool isPalindrome(string s) {
+        
+        string str= get_string(s);
+        int i=0,j=str.size()-1;
+        
+        cout<<str<<endl;
+        while(i<=j)
+        {
+            if(str[i]!=str[j])
+                return false;
+            
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+};
