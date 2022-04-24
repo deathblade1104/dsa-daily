74b288eaa208b743531d45ff8c2ae24c289375f5
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-24 13:25:55 +0530

    Time: 12 ms (66.67%), Space: 11.6 MB (16.67%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
@@ -3,19 +3,19 @@ public:
     vector<int> intersection(vector<vector<int>>& nums) {
         int count[1001];
         memset(count,0,sizeof(count));
+        vector<int>ans;
         
         for(auto v : nums)
         {
             for(auto i : v)
+            {
                 count[i]++;
+                if(count[i]==nums.size())
+                    ans.push_back(i);
+            }
         }
-        
-        vector<int>ans;
-        
-        for(int i=1;i<=1000;i++)
-            if(count[i]==nums.size())
-                ans.push_back(i);
-        
+            
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
