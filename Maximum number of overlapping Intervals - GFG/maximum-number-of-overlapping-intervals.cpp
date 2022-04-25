96d32cc7d67f64d4ee5a1772baffa06ae9ffeaa2
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-25 15:21:31 +0530

    Added solution - LeetHub

---
+++ Maximum number of overlapping Intervals - GFG/maximum-number-of-overlapping-intervals.cpp
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+/* Driver program to test above function */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+class Solution{
+    public:
+    int overlap(vector<pair<int,int>>intervals, int n){
+        //Write your code here
+        
+
+        int count[10000];
+    	memset(count,0,sizeof(count));
+    	
+    	for(auto p : intervals)
+    	{
+    	    count[p.first]++;
+    	    count[p.second+1]--;
+    	}
+    	
+
+    	int ans=count[0];
+    	
+    	for(int i=1;i<10000;i++)
+    	{
+    	    count[i]+=count[i-1];
+    	    ans=max(ans,count[i]);
+    	}
+    	return ans;
+    }
+};
+
+
+
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin >> n;
+	    vector<pair<int,int>>intervals;
+	    for(int i = 0; i < n; i++){
+	        int a, b;
+	        cin >> a >> b;
+	        intervals.push_back({a, b});
+	    }
+	    Solution ob;
+	    int ans = ob.overlap(intervals, n);
+	    cout << ans <<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
