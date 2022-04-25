b56447c4ab0735210c4983591d4d181ff2c6d437
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-25 10:27:19 +0530

    Time: 8 ms (39.68%), Space: 13.6 MB (58.23%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void get_ans(string temp, int sz, int op , int cl , vector<string>&ans)
+    {
+        if(temp.size()==sz and op==cl)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        
+        if(op<sz/2)
+        get_ans(temp+"(",sz,op+1,cl,ans);
+        
+        if(op>cl)
+        get_ans(temp+")",sz,op,cl+1,ans);
+        
+        return;
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        int sz=n*2,op=0,cl=0;
+        
+        get_ans("",sz,op,cl,ans);
+        
+        return ans;
+        
+    }
+};
