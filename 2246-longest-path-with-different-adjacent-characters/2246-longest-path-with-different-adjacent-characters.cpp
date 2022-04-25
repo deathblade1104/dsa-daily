c97701393377e0d061199219b15b19557c624ae2
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-25 23:15:37 +0530

    Time: 612 ms (48.85%), Space: 172.6 MB (84.71%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -2,31 +2,51 @@ class Solution {
 public:
     int DFS(int i, vector<int>adj[], int &ans, string &s)
     {
-        int m1,m2;
-        m1=m2=0;
+        if(adj[i].size()==0)
+            return 1;
         
-        for(int neigh : adj[i])
+        int curr=1,m1=-1,m2=-1;
+            
+        for(int n : adj[i])
         {
-            int curr = DFS(neigh,adj,ans,s);
-            if(s[i] == s[neigh])
-                continue;
-            if (curr > m2) m2 = curr;
-            if (m2 > m1) swap(m1, m2);
+            int x = DFS(n,adj,ans,s);
+            
+            if(s[n] != s[i])
+            {
+                curr = max(curr,x+1);
+                 
+                if(m1==-1)
+                    m1=x;
+                
+                else if(m1!=-1 and m2==-1)
+                    m2=x;
+                
+                if(m1!=-1 and m2!=-1)
+                {
+                    ans = max(ans, (m1+m2+1));
+                    m1=max(m1,m2);
+                    m2=-1;
+                }
+            }
+            
+            ans = max({ans,curr,x});
         }
         
-        ans= max(ans, m2+m1+1);
-        return m1+1;
+        return curr;
         
     }
     
     int longestPath(vector<int>& parent, string s) {
-        int n=s.size();
+        int n=s.size();        
+        if(n==1)
+            return n;
+        
         vector<int>adj[n];
         
         for(int i=1;i<n;i++)
         adj[parent[i]].push_back(i);
-        int ans=0;
         
+        int ans=0;        
         int x= DFS(0,adj,ans,s);
         return ans;
         
