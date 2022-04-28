d718af19dcf113a8b4f138fc255422f3d70fac61
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-28 11:55:43 +0530

    Time: 809 ms (14.10%), Space: 47.6 MB (31.27%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -16,7 +16,7 @@ public:
         priority_queue<vector<int>,vector<vector<int>>,mycomp>pq;
         
         pq.push({0,0,0});
-//         effort[0][0]=0;
+        effort[0][0]=0;
         
         while(pq.size()>0)
         {
@@ -24,14 +24,9 @@ public:
             pq.pop();
             
             int r=v[0],c=v[1],curr=v[2];
-                         
-            if(curr>=effort[r][c])
-            continue;
-                         
+            
             if(r==n-1 and c==m-1)
-                return curr;
-                         
-            effort[r][c]=curr;
+            return curr;         
                          
             for(int i=1;i<=4;i++)
             {
@@ -40,7 +35,12 @@ public:
                 if(x>=0 and y>=0 and x<n and y<m)
                 {
                     int temp = abs(heights[x][y] - heights[r][c]);
-                    pq.push({x,y,max(temp,curr)});
+                    
+                    if(temp<effort[x][y])
+                    {
+                        effort[x][y] = temp;
+                        pq.push({x,y,max(temp,curr)});
+                    }
                 }
             }
           
