3b62425904be67883d3b364194c64535b838fef9
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-04-29 12:04:35 +0530

    Time: 341 ms (67.73%), Space: 131.9 MB (50.33%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        
-        if(k==1)
-            return nums;
-        
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {        
         vector<int>ans;
         deque<int>dq;
         
