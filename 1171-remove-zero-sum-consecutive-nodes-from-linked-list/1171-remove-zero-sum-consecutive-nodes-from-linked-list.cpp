7accf4de07df07ca78de1eecc6bf19274f913bb1
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-06 03:23:12 +0530

    Time: 9 ms (89.52%), Space: 11.4 MB (68.00%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -12,10 +12,9 @@ class Solution {
 public:
     void remove_nodes(unordered_map<int,ListNode*>&mp, ListNode *start, ListNode*end, int &ps)
     {
-        int t = ps;        
-        ListNode *curr;        
+        int t = ps;           
         
-       for(curr= start->next; curr!=end and curr!=NULL; curr= curr->next)
+       for(ListNode *curr= start->next; curr!=end and curr!=NULL; curr= curr->next)
        {
            t+=curr->val;
            mp.erase(t);
@@ -33,19 +32,14 @@ public:
         ListNode *dummy = new ListNode(-1);
         dummy->next=head;
         mp[0]=dummy;
-        
-        // int i =0;
-        
+          
         ListNode *curr = head;
         while(curr!=NULL)
         { 
             ps+=curr->val;
-            // i++;
-            
-            // cout<<i<<"__"<<curr->val<<"-->"<<ps<<endl;
+           
             if(mp.count(ps)==1)
             {               
-                // cout<<i<<"__"<<curr->val<<"-->"<<ps<<endl;
                 ListNode *temp = curr;
                 remove_nodes(mp, mp[ps], temp, ps);               
                 curr=temp->next;                
