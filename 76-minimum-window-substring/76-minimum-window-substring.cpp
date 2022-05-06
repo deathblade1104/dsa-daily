46668e02992bd3e721f1b617fa7c9ae8c0fbf1f3
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-06 21:37:55 +0530

    Time: 501 ms (5.03%), Space: 368.5 MB (5.01%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
-    string minWindow(string s1, string s2) {
+    string minWindow(string s, string p) {
         
-        if(s1.size()<s2.size())
+        if(s.size()<p.size())
             return "";
-        
+                
         int mp1[128],mp2[128];
         memset(mp1,0,sizeof(mp1));
         memset(mp2,0,sizeof(mp2));
         
-        for(char ch : s2)
-            mp2[(int)ch]++;        
-        
+        for(char ch: p)
+	    mp1[ch]++;
+
+	    int necessary_chars=0, start=0;
         string ans="";
-        int release=0,matchcount=0;
         
-        for(int acquire=0;acquire<s1.size();acquire++)
+
+        for(int end =0;end<s.size();end++)
         {
-            int ac_idx=s1[acquire];
-            mp1[ac_idx]++;
-            
-            if(mp2[ac_idx]>0 and mp2[ac_idx]>=mp1[ac_idx])
-                matchcount++;
-            
-            while(matchcount==s2.size() and release<=acquire)
-            {             
-                string temp = s1.substr(release,acquire-release+1);
+            mp2[s[end]]++;
+
+            if(mp1[s[end]]>0 and mp2[s[end]]<=mp1[s[end]])
+            necessary_chars++;
+
+            while(necessary_chars==p.size() and start<=end)
+            {               
+                string temp = s.substr(start, (end - start + 1));
                 
                 if(ans.size()==0 or temp.size()<ans.size())
                     ans=temp;
-                   
-                int rel_idx = s1[release];               
-                
-                if(mp2[rel_idx]>0 and mp1[rel_idx]-1<mp2[rel_idx])
-                    matchcount--;
-                
-                mp1[rel_idx]--;
-                
-                release++;
+
+                if(mp1[s[start]]>0 and mp2[s[start]]-1 <mp1[s[start]])
+                necessary_chars --;
+
+                mp2[s[start]]--;
+
+                start++;	
             }
         }
-        
-        return ans;
+	
+    return ans;
+
     }
 };
