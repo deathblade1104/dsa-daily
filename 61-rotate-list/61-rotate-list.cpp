17b98a7d7d27920e704cce9287e518c805e5bbe5
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-06 19:04:01 +0530

    Time: 11 ms (50.24%), Space: 11.7 MB (89.43%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -9,51 +9,53 @@
  * };
  */
 class Solution {
-private:
-    ListNode *kthfromend(ListNode *head, int k)
-    {
-        ListNode *curr1=head;
-        for(int i=0;i<k;i++)
-            curr1=curr1->next;
-            
-        ListNode *curr2=head,*prev=NULL,*p=NULL;
-        while(curr1!=NULL)
-        {
-            prev=curr2;
-            curr2=curr2->next;
-            p=curr1;
-            curr1=curr1->next;
-        }
-        
-        prev->next=NULL;        
-        p->next=head;
-        
-        return curr2;
-    }
 public:
-    ListNode* rotateRight(ListNode* head, int k) {
+    
+    int find_length(ListNode *head)
+    {
+        if(!head)
+            return 0;
         
-        if(head==NULL or head->next==NULL or k==0)
-            return head;
-        
-        int n=0;
         ListNode *curr=head;
-        
+        int ans=0;
         while(curr!=NULL)
         {
-            n++;
             curr=curr->next;
+            ans++;
         }
         
-        if(k>=n and k%n==0)
+        
+        return ans;
+    }
+    ListNode* rotateRight(ListNode* head, int k) {
+        
+        int n = find_length(head);
+        
+        if(n!=0)
+            k=k%n;
+        
+        if(k==0 or n==0)
             return head;
         
-        else if(k>n and k%n!=0)
-            return kthfromend(head,k%n);
+        ListNode *last=head;
         
-        else return kthfromend(head,k);     
-    
-
+        for(int i=0;i<k-1;i++)
+            last = last->next;
+        
+        ListNode *kth=head,*prev=NULL;
+        
+        while(last->next != NULL)
+        {
+            prev = kth;
+            kth = kth->next;
+            last = last ->next;
+        }
+        
+        prev->next=NULL;
+        last->next= head;
+        
+        return kth;
+             
         
         
     }
