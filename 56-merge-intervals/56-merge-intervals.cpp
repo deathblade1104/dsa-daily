c75e3c0cb77d82e3e2d0029c53fb0d3123e97673
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-11 01:30:49 +0530

    Time: 61 ms (31.36%), Space: 20 MB (27.71%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    struct  mycomp
+    {
+        bool operator()(const vector<int>&a, const vector<int>&b)
+        {
+            if(a[0]!=b[0])
+                return a[0]<b[0];
+            
+            return a[1]<b[1];
+            
+        }
+    };
+    
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),mycomp());
+        
+        vector<vector<int>>ans;
+        vector<int>prev = intervals[0];
+        
+        for(int i=1;i<intervals.size();i++)
+        {
+            vector<int>curr = intervals[i];
+            if(prev[1]>=curr[0])
+            {
+                prev[1]=max(curr[1],prev[1]);
+                prev[0]=min(curr[0],prev[0]);
+            }
+            
+            else
+            {
+                ans.push_back(prev);
+                prev=curr;
+            }
+        }
+        
+        ans.push_back(prev);
+        
+        return ans;
+    }
+};
