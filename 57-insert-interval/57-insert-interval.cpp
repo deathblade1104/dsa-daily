36fd70701e8abcf9714808f65ee5802673c9625e
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-11 03:17:23 +0530

    Time: 22 ms (52.58%), Space: 19.6 MB (5.60%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,32 +1,41 @@
 class Solution {
 public:
-        struct  mycomp
-    {
-        bool operator()(const vector<int>&a, const vector<int>&b)
-        {
-            if(a[0]!=b[0])
-                return a[0]<b[0];
-            
-            return a[1]<b[1];
-            
-        }
-    };
-    
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         
-        intervals.push_back(newInterval);
-        sort(intervals.begin(),intervals.end(),mycomp());
+        vector<vector<int>>temp;
+        bool flag = false;
         
+        int i=0;
+        
+        while(i<intervals.size())
+        {
+            auto p = intervals[i];
+            
+            if(p[0]<=newInterval[0] or flag)
+            {
+                temp.push_back(p);
+                i++;               
+            }
+            
+            else
+            {
+                flag=true;
+                temp.push_back(newInterval);
+            }
+        }
+        
+        if(!flag)
+            temp.push_back(newInterval);
+        
+        intervals=temp;
         vector<vector<int>>ans;
         vector<int>prev = intervals[0];
         
         for(int i=1;i<intervals.size();i++)
         {
             vector<int>curr = intervals[i];
-            
             if(prev[1]>=curr[0])
-                prev[1]=max(curr[1],prev[1]);
-            
+              prev[1]=max(curr[1],prev[1]);
             
             else
             {
