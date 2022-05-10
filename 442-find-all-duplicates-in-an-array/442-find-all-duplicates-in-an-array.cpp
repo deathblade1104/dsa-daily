fa504d07753b1f85e15192676f7380444bccc21c
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-11 03:52:38 +0530

    Time: 83 ms (53.26%), Space: 33.5 MB (93.84%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -5,18 +5,13 @@ public:
         
         for(int i=0;i<nums.size();i++)
         {
-            int x = abs(nums[i]);
-            x--;
+            int x = abs(nums[i]) - 1;
             
-            if(x<nums.size())
-            {
-                if(nums[x]<0)
-                    ans.push_back(x+1);
-                
-                nums[x] = -1 * abs(nums[x]);
-            }
-        }
-        
+            if(nums[x]<0)
+                ans.push_back(x+1);
+
+            nums[x] = -1 * abs(nums[x]);
+        }        
         return ans;
     }
 };
