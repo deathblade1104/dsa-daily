84a6a8463f95bfbea93d1abbf7176b5f0804ce2a
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-15 00:54:41 +0530

    Time: 7 ms (43.54%), Space: 11.4 MB (27.04%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    void helper(int curr, int&sz, vector<int>&nums, vector<int>&temp, vector<vector<int>>&ans,bool consider)
+    void helper(int curr,vector<int>&nums, vector<int>&temp, vector<vector<int>>&ans,bool consider)
     {
-        if(curr>=sz)
+        if(curr>=nums.size())
         {
             ans.push_back(temp);
             return;
         }
         
-        helper(curr+1,sz,nums,temp,ans,false);
+        helper(curr+1,nums,temp,ans,false);
         
         if(curr>0 and nums[curr]==nums[curr-1] and consider==false)
             return;
         
         temp.push_back(nums[curr]);
-        helper(curr+1,sz,nums,temp,ans,true);
+        helper(curr+1,nums,temp,ans,true);
         temp.pop_back();
         
        
@@ -22,11 +22,11 @@ public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         
         sort(nums.begin(),nums.end());
-        int sz=nums.size();
+        
         vector<int>temp;
         vector<vector<int>>ans;
         
-        helper(0,sz,nums,temp,ans,false);
+        helper(0,nums,temp,ans,false);
         return ans;
     }
 };
