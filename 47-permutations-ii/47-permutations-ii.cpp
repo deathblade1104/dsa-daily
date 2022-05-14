bcba2f571ba9eb3801d551fb57daf16ec98001dc
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-15 03:39:20 +0530

    Time: 0 ms (100.00%), Space: 10 MB (34.27%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     void helper(int pos, int&sz, vector<int>&nums, vector<vector<int>>&ans)
     {
-         if (pos ==sz) {
+         if (pos ==sz-1) {
             ans.push_back(nums);
+             return;
         } 
         else 
         {
             for (int i = pos; i < sz; ++i) 
             {
-                
-                if (i > pos && nums[i] == nums[pos]) continue;
+                if (i > pos && nums[i] == nums[pos])
+                    continue;
                 
                 swap(nums[pos], nums[i]);
                 helper(pos + 1,sz,nums,ans);
