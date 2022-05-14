383333fb1b7f12712d884b9ef7f2a40a42b3b739
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-15 02:54:23 +0530

    Time: 36 ms (51.17%), Space: 9 MB (89.86%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void helper(int curr, int&n,int&k,vector<int>&temp,vector<vector<int>>&ans)
+    {
+        if(temp.size()==k)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        
+        for(int i=curr;i<=n;i++)
+        {
+            temp.push_back(i);
+            helper(i+1,n,k,temp,ans);
+            temp.pop_back();
+        }
+        
+        return;
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        
+        helper(1,n,k,temp,ans);
+        return ans;
+    }
+};
