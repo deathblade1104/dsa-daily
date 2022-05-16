53be7d3c5a701972e2f5dc348520684f2dba411d
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-16 06:04:28 +0530

    Time: 96 ms (53.87%), Space: 19.3 MB (83.15%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -7,28 +7,34 @@ public:
         
         int n= grid.size();
         array<int,9>dir= {1,0,-1,0,1,1,-1,-1,1};
-        queue<array<int,3>>q;
-        q.push({1,0,0});
+        queue<array<int,2>>q;
+        q.push({0,0});
         grid[0][0] = 1;
+        int steps=1;
         
         while(q.size()>0)
         {
-            auto p = q.front();
-            q.pop();
-            
-            if(p[1]==n-1 and p[2]==n-1)
-                return p[0];
-            
-            for(int i=1;i<=8;i++)
+            int sz = q.size();
+            while(sz--)
             {
-                int x = p[1] + dir[i-1] , y = p[2] + dir[i];
-                
-                if(x>=0 and y>=0 and x<n and y<n and grid[x][y]==0)
+                auto p = q.front();
+                q.pop();
+
+                if(p[0]==n-1 and p[1]==n-1)
+                return steps;
+
+                for(int i=1;i<=8;i++)
                 {
-                    q.push({p[0]+1,x,y});
-                    grid[x][y]=1;
-                }
-            }   
+                    int x = p[0] + dir[i-1] , y = p[1] + dir[i];
+
+                    if(x>=0 and y>=0 and x<n and y<n and grid[x][y]==0)
+                    {
+                        q.push({x,y});
+                        grid[x][y]=1;
+                    }
+                }   
+            }
+            steps++;
         }
         
         return -1;
