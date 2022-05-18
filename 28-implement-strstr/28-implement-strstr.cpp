6d280781bf0239db49c4dd84b897d2a563c3cede
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-18 16:22:50 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (70.22%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,31 +1,58 @@
 class Solution {
+
 public:
-    int strStr(string haystack, string needle) {
+    int strStr(string text, string pattern) {
         
-        if(haystack==needle)
-            return 0;
-        
-        int sz=needle.size();
-        string temp="";
-        
-        for(int i=0;i<sz;i++)
-            temp+=haystack[i];
-        
-        
-        if(temp==needle)
-            return 0;
-        
-        for(int i=sz;i<haystack.size();i++)
+        int q = 101;
+        int m = pattern.size();
+        int n = text.size();
+        int i, j, hash_p = 0, hash_t = 0, h = 1, d = m + n;
+
+        for (i = 0; i < m - 1; i++)
+            h = (h * d) % q;
+
+
+        // Calculate hash value for pattern and text
+        for (i = 0; i < m; i++) 
         {
-            temp.erase(temp.begin() + 0);
-            temp+=haystack[i];
-            
-            if(temp==needle)
-                return i-sz+1;
+            hash_p = (pattern[i] + d * hash_p) % q;
+            hash_t = (text[i] + d * hash_t) % q;
         }
-        
+
+        // Find the match
+        for (i = 0; i <= n - m; i++) 
+        {
+
+        // Check the hash values of current windows of text and pattern.
+            if (hash_p == hash_t) 
+            {
+
+                for (j = 0; j < m; j++) 
+                {
+                    if (text[i + j] != pattern[j]) 
+                    break;
+                }
+                if (j == m) 
+                return i;
+            }
+
+
+            if (i < n - m) 
+            {
+                hash_t = (d * (hash_t - text[i] * h) + text[i + m]) % q;
+
+                //converting negative value of hash_t to positive.
+                if (hash_t < 0)
+                    hash_t = (hash_t + q);
+            }
+
+
+
+        }
+    
+     
         return -1;
-        
-        
     }
+        
+
 };
