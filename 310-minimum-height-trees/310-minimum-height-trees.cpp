3b90a2b61ad7e9f4d668e08b46dfb7e8b9dfddc6
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-19 20:13:30 +0530

    Time: 288 ms (35.70%), Space: 50 MB (95.02%) - LeetHub

--- 310-minimum-height-trees/310-minimum-height-trees.cpp
+++ 310-minimum-height-trees/310-minimum-height-trees.cpp
@@ -2,44 +2,43 @@ class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
         vector<int>adj[n];
-        vector<int>in(n,0);
+        vector<int>inedges(n,0);
         
-        for(int i=0;i<edges.size();i++)
+        for(auto &v : edges)
         {
-            auto t = edges[i];
+            inedges[v[0]]++;
+            inedges[v[1]]++;
             
-            adj[t[0]].push_back(t[1]);
-            adj[t[1]].push_back(t[0]);
-            
-            in[t[0]]++;
-            in[t[1]]++;
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
         }
+        
         
         vector<int>ans;
         queue<int>q;
         for(int i=0;i<n;i++)
         {
-            if(in[i]==1)
+            if(inedges[i]==1)
                 q.push(i);
         }
         
-        while(q.size()!=0)
+        while(q.size()>0)
         {
             ans.clear();
-            int sz=q.size();
-            for(int i=0;i<sz;i++)
+            int sz = q.size();
+            
+            while(sz--)
             {
-                int curr=q.front();
+                int curr = q.front();
                 q.pop();
                 ans.push_back(curr);
-                in[curr]--;
                 
-                for(int neigh : adj[curr])
+                for(int&neigh : adj[curr])
                 {
-                    in[neigh]--;
-                    if(in[neigh]==1)
+                    inedges[neigh]--;
+                    if(inedges[neigh]==1)
                         q.push(neigh);
-                }                
+                }
             }
         }
         
