2054b04b0df687ef5e9820edb69e0de958047984
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-19 20:55:26 +0530

    Time: 453 ms (56.77%), Space: 130.6 MB (25.98%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,72 +1,53 @@
 class Solution {
-private:
-   struct mycomp{
-        bool operator()(const pair<string,int>&a,const pair<string,int>&b)
+public:
+    struct mycomp
+    {
+        bool operator()(const array<int,2>&a, const array<int,2>&b)
         {
-            return a.second>b.second;
+            return a[1]>b[1];
         }
     };
     
-    string get_key(vector<int>&v)
-    {
-        string ans= to_string(v[0]) + "_" + to_string(v[1]);
-        return ans;
-    }
-    
-    int get_dist(vector<int>&v1, vector<int>&v2)
-    {
-        
-        int z = abs(v1[0]-v2[0]) + abs(v1[1]-v2[1]);        
-        return z;       
-    }
-    
-public:       
     int minCostConnectPoints(vector<vector<int>>& points) {
- 
-        if(points.size()==684)
-            return 4196509;
+       
+        int n = points.size();
+        vector<vector<array<int,2>>>adj(n);
         
-        if(points.size()==992)
-            return 61998010;
-        
-        unordered_set<string>vis;
-        unordered_map<string,vector<pair<string,int>>>adj;
-        
-        for(int i=0;i<points.size();i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=i+1;j<points.size();j++)
+            for(int j=i+1;j<n;j++)
             {
-                if(i!=j)
-                {
-                    string u = get_key(points[i]), v = get_key(points[j]);
-                    int dist = get_dist(points[i],points[j]);
-                    
-                    adj[u].emplace_back(v,dist);
-                    adj[v].emplace_back(u,dist);
-                }
+                int dist = abs(points[i][0] - points[j][0]) + abs(points[j][1] - points[i][1]);
+                adj[j].push_back({i,dist});
+                adj[i].push_back({j,dist});
             }
         }
         
-        priority_queue<pair<string,int>,vector<pair<string,int>>,mycomp>pq;
-        pq.emplace(get_key(points[0]),0);
-        int ans=0;
+        priority_queue<array<int,2>, vector<array<int,2>> , mycomp>pq;
+        pq.push({0,0});
         
-        while(pq.size()>0 and vis.size()<=points.size())
+        vector<bool>vis(n,false);
+        int count =0, cost=0;
+                
+        while(pq.size()>0 and count<n)
         {
-            auto p=pq.top();
+            auto p = pq.top();
             pq.pop();
             
-            if(vis.count(p.first)==0)
+            if(vis[p[0]])
+                continue;
+            
+            vis[p[0]]=true;
+            count++;            
+            cost+=p[1];
+            
+            for(auto &neigh : adj[p[0]])
             {
-                vis.insert(p.first);
-                ans+=p.second;                    
-               
-                for(auto x : adj[p.first])
-                    if(vis.count(x.first)==0)
-                        pq.emplace(x.first,x.second);
+                if(!vis[neigh[0]])
+                    pq.push(neigh);
             }
         }
         
-        return ans;
+        return cost;
     }
 };
