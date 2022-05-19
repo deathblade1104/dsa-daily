709a15f5143d89f45eb64974c640ea1746a0c876
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-19 07:54:14 +0530

    Time: 51 ms (70.26%), Space: 17.5 MB (78.16%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,44 +1,57 @@
 class Solution {
-private:
-    int dim[5]={1,0,-1,0,1};
-    void DFS(int r,int c,int&n,int&m,int prev,vector<vector<int>>&heights,vector<vector<bool>>&vis)
-    {
-        if(r<0 or c<0 or r>=n or c>=m or vis[r][c] or heights[r][c]<prev)
-            return;
-        
-        vis[r][c]=true;
-        
-        for(int i=1;i<5;i++)
-            DFS(r+dim[i-1],c+dim[i],n,m,heights[r][c],heights,vis);
-    }
 public:
+    array<int,5>dir={1,0,-1,0,1};
+    void DFS(int i, int j, int&n, int&m , vector<vector<bool>>&vis, vector<vector<int>>&heights)
+    {
+        vis[i][j]=true;
+        
+        for(int k=1;k<=4;k++)
+        {
+            int x = i + dir[k-1] , y = j + dir[k];
+            
+            if(x<0 or y<0 or x>=n or y>=m or vis[x][y] or heights[x][y]<heights[i][j])
+                continue;
+            
+            DFS(x,y,n,m,vis,heights);
+        }
+        
+        return;
+    }
+    
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        
+        int n  = heights.size(), m = heights[0].size();
+        
+        vector<vector<bool>>pacific(n,vector<bool>(m,false)),atlantic(n,vector<bool>(m,false));
+         
         vector<vector<int>>ans;
-        int n=heights.size(),m=heights[0].size();
         if(n==0 or m==0)
             return ans;
-        
-        vector<vector<bool>>pac(n,vector<bool>(m,false));
-        vector<vector<bool>>atl(n,vector<bool>(m,false));
         
         
         for(int i=0;i<m;i++)
         {
-            DFS(0,i,n,m,INT_MIN,heights,pac);
-            DFS(n-1,i,n,m,INT_MIN,heights,atl);
+            DFS(0,i,n,m,pacific,heights);
+            DFS(n-1,i,n,m,atlantic,heights);
         }
         
         for(int i=0;i<n;i++)
         {
-            DFS(i,0,n,m,INT_MIN,heights,pac);
-            DFS(i,m-1,n,m,INT_MIN,heights,atl);
+            DFS(i,0,n,m,pacific,heights);
+            DFS(i,m-1,n,m,atlantic,heights);
         }
         
         
+        
         for(int i=0;i<n;i++)
+        {
             for(int j=0;j<m;j++)
-                if(pac[i][j] and atl[i][j])
+            {
+                if(pacific[i][j] and atlantic[i][j])
                     ans.push_back({i,j});
+            }
+        }
+        
         return ans;
     }
 };
