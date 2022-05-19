e259a9788789c0995a8f073cf941cd6bfe9e6694
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-19 06:48:29 +0530

    Time: 27 ms (52.14%), Space: 23.3 MB (66.61%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,35 +1,40 @@
 class Solution {
-private :
-    void DFS(int r,int c,int&n, int&m,vector<vector<int>>&grid, int&count)
-    {
-        if(r<0 or c<0 or r>=n or c>=m or grid[r][c]==0)
-            return;
-
-        grid[r][c]=0;
-        count++;
-        
-        DFS(r+1,c,n,m,grid,count);
-        DFS(r-1,c,n,m,grid,count);
-        DFS(r,c+1,n,m,grid,count);
-        DFS(r,c-1,n,m,grid,count);
-    }
 public:
+    int DFS(int i, int j, int&n, int &m , array<int,5>&dir, vector<vector<int>>&grid)
+    {        
+        if(i<0 or j<0 or i>=n or j>=m or grid[i][j]==0)
+            return 0;
+        
+        grid[i][j]=0;
+        
+        int neigh =0;
+        
+        for(int k=1;k<=4;k++)
+        {
+            int x = dir[k-1] + i, y = dir[k] + j;
+            
+            neigh += DFS(x,y,n,m,dir,grid);
+        }
+        
+        return 1 + neigh;
+        
+    }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int ans=0,n=grid.size(),m=grid[0].size();
+        array<int,5>dir = {1,0,-1,0,1};
+        int n = grid.size(), m = grid[0].size();
+        int ans =0;
+        
         
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(grid[i][j]==1)
-                {
-                    int count =0;
-                    DFS(i,j,n,m,grid,count);
-                    ans=max(ans,count);
-                }
+                    ans = max(ans,DFS(i,j,n,m,dir,grid));
             }
         }
         
         return ans;
+        
     }
 };
