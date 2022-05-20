e12c23b6cecda2fd8c06eb630dc0e115dcd539a8
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-20 07:01:20 +0530

    Time: 3 ms (80.74%), Space: 8 MB (13.17%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-       
-        int m=grid.size(), n=grid[0].size();
-       
-        if(grid[m-1][n-1]==1 or grid[0][0]==1)
+    
+    int helper(int i,int j, int&n,int&m, vector<vector<int>>&grid, vector<vector<int>>&dp)
+    {
+        if(i>=n or j>=m)
             return 0;
         
-       vector<vector<long long>>dp(m,vector<long long>(n,0));
+        if(grid[i][j]==1)
+            return dp[i][j]=0;
         
-        dp[m-1][n-1]=1;        
-        for(int i=m-1;i>=0;i--)
-        {
-            for(int j=n-1;j>=0;j--)
-            {
-                if(i==m-1 and j==n-1)
-                    continue;
+        if(i==n-1 and j==m-1)
+            return dp[i][j]=1;
                 
-                else if(grid[i][j]==1)
-                    dp[i][j]=0;
-                
-                else
-                {
-                    if(i==m-1)
-                        dp[i][j]= dp[i][j+1];
-
-                        
-                    if(j==n-1)
-                        dp[i][j] = dp[i+1][j];
-                        
-                    if(i!=m-1 and j!=n-1)
-                        dp[i][j] = dp[i+1][j] + dp[i][j+1];
-                }
-            }
-        }        
-        return dp[0][0];
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        int down = helper(i+1,j,n,m,grid,dp);
+            
+        int right = helper(i,j+1,n,m,grid,dp);
+            
+        return dp[i][j]= down + right;        
+    }
+    
+    
+    int uniquePathsWithObstacles(vector<vector<int>>&grid) {
+        
+        
+        int n= grid.size(), m= grid[0].size();
+        if(grid[0][0]==1 or  grid[n-1][m-1]==1)
+            return 0;
+        
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return helper(0,0,n,m,grid,dp);
+        
+        
     }
 };
