da975d485d54bef03015dffa17f48556a8d7132f
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-05-23 12:49:00 +0530

    Time: 38 ms (62.88%), Space: 11.6 MB (14.33%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,23 +1,121 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
+    long long sumSubarrayMaxs(vector<int>& arr) {
         
-        long long ans=0;
+        int n =arr.size();
         
-        for(int i=0;i<nums.size();i++)
+        stack<int>st;
+        vector<int>nse(n),pse(n);
+        
+        for(int i=0;i<n;i++)
         {
-            int m1 = nums[i], m2= nums[i];
-            
-            for(int j=i;j<nums.size();j++)
-            {
-                m1 = min(m1,nums[j]);
-                m2 = max(m2, nums[j]);
-                
-                ans+= m2 - m1;
-            }
+            pse[i]= i+1;
+            nse[i] = n - i;
         }
         
+        st.push(0);
+        
+        for(int i=1;i<n;i++)
+        {
+            while(st.size()>0 and arr[st.top()]<arr[i])
+                st.pop();
+            
+            if(st.size()!=0)
+                pse[i]=i - st.top();
+            
+            st.push(i);
+        }
+        
+        while(st.size()!=0)
+            st.pop();
+        
+        st.push(n-1);
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            while(st.size()>0 and arr[st.top()]<=arr[i])
+               st.pop();
+            
+            if(st.size()!=0)
+                nse[i]=  st.top() - i;
+            
+            st.push(i);
+        }
+        
+       long long ans= 0;
+        for(int i=0;i<n;i++)
+        {
+            long long curr = arr[i];
+            curr*=pse[i];
+            curr*=nse[i];
+        
+            ans = ans + curr;        
+        }
+        
+        
+        return ans;   
+    }
+    
+    
+    long long sumSubarrayMins(vector<int>& arr) {
+        
+        int n =arr.size();
+        
+        stack<int>st;
+        vector<int>nse(n),pse(n);
+        
+        for(int i=0;i<n;i++)
+        {
+            pse[i]= i+1;
+            nse[i] = n - i;
+        }
+        
+        st.push(0);
+        
+        for(int i=1;i<n;i++)
+        {
+            while(st.size()>0 and arr[st.top()]>arr[i])
+                st.pop();
+            
+            if(st.size()!=0)
+                pse[i]=i - st.top();
+            
+            st.push(i);
+        }
+        
+        while(st.size()!=0)
+            st.pop();
+        
+        st.push(n-1);
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            while(st.size()>0 and arr[st.top()]>=arr[i])
+               st.pop();
+            
+            if(st.size()!=0)
+                nse[i]=  st.top() - i;
+            
+            st.push(i);
+        }
+        
+       long long ans= 0;
+        for(int i=0;i<n;i++)
+        {
+            long long curr = arr[i];
+            curr*=pse[i];
+            curr*=nse[i];
+        
+            ans = ans + curr;        
+        }
+        
+        
         return ans;
+        
+    }
+    long long subArrayRanges(vector<int>& nums) {
+        
+        return sumSubarrayMaxs(nums) - sumSubarrayMins(nums);
         
     }
 };
