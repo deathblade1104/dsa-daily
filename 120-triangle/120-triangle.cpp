346de43dc62cbd035d69c66acb6e16081bc6f585
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-06-13 21:36:57 +0530

    Time: 20 ms (5.45%), Space: 8.8 MB (49.02%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int dp[201][201];
-    int get_ans(int r, int c, vector<vector<int>>&triangle)
+    int helper(int curr, int row, vector<vector<int>>&triangle)
     {
-        if(r==triangle.size())
+        if(row>=triangle.size())
             return 0;
         
-        if(dp[r][c]!=-1)
-            return dp[r][c];
+        if(dp[row][curr]!=-1)
+            return dp[row][curr];
         
-        int call1 = get_ans(r+1,c,triangle);
-        int call2 = 1000001;
+        int op1 = triangle[row][curr],op2 =triangle[row][curr];
         
-        if(c<triangle[r].size())
-            call2 = get_ans(r+1,c+1,triangle);
+        op1 += helper(curr,row+1,triangle);
+        op2 += helper(curr+1,row+1,triangle);
         
-        return dp[r][c] = triangle[r][c] +  min(call1,call2);
+        return dp[row][curr] = min(op1,op2);
+            
     }
-    
     int minimumTotal(vector<vector<int>>& triangle) {
-        memset(dp,-1,201*201*sizeof(int));
+        memset(dp, -1, 201*201*sizeof(int));
+        return helper(0,0,triangle);
+
         
-       return get_ans(0,0,triangle);
     }
 };
