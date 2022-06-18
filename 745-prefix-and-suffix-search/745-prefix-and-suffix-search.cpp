4ddf088985ae4b0871043c8d461939b59e30a27f
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-06-18 21:27:53 +0530

    Time: 1079 ms (17.54%), Space: 154.1 MB (59.33%) - LeetHub

---
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -0,0 +1,37 @@
+class WordFilter {
+public:
+    unordered_map<string,int>mp;
+    WordFilter(vector<string>& words) {
+        for(int i=0;i<words.size();i++)
+        {
+            string curr = words[i], rev = curr;
+            
+            for(int j=1;j<=curr.size();j++)
+            {
+                string p = curr.substr(0,j);
+                
+                for(int k=curr.size()-1;k>=0;k--)
+                {
+                    string s = p + "|" + curr.substr(k);             
+                    mp[s] = i;
+                }
+            }
+        }
+    }
+    
+    int f(string prefix, string suffix) {
+        
+        string w = prefix + "|" + suffix;
+        
+        if(mp.count(w)==0)
+            return -1;
+        
+        return mp[w];
+    }
+};
+
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * WordFilter* obj = new WordFilter(words);
+ * int param_1 = obj->f(prefix,suffix);
+ */
