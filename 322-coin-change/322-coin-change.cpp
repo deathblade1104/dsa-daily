de15dfff5ffbaf370cfac26e3c0d77b00215a06c
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-07-04 23:45:40 +0530

    Time: 219 ms (16.71%), Space: 14.6 MB (40.76%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
-    int helper(int curr,vector<int>&coins,vector<int>&dp)
+    int coinChange(vector<int>& nums, int sum) {
+    
+    int n  = nums.size();
+    vector<int>dp(sum+1,sum+1);
+    sort(nums.begin(),nums.end());
+    dp[0]=0;
+
+    for(int i=1;i<=sum;i++)
     {
-        if(curr==0)
-            return 0;
-        
-        if(dp[curr]!=-1)
-            return dp[curr];
-        
-        int ans=INT_MAX/2;
-        for(int i=0;i<coins.size();i++)
+        for(int j=0;j<n;j++)
         {
-            int temp=INT_MAX/2;
-            if(coins[i]<=curr)
-               temp = 1 + helper(curr-coins[i],coins,dp);
-            
-            
-            ans=min(ans,temp);
-            
-            if(coins[i]>curr)
+            if(nums[j]>i)
                 break;
+
+            dp[i] = min(dp[i], 1 + dp[i - nums[j]]);
         }
-        
-        return dp[curr]=ans;
+        // cout<<i<<" "<<dp[i]<<endl;
     }
+
     
-    int coinChange(vector<int>& coins, int amount) {
-    
-        int n = coins.size();
-        vector<int>dp(amount+1,-1);
-        
-        sort(coins.begin(),coins.end());
-        
-        int ans =  helper(amount,coins,dp);
-        
-        return ans >= INT_MAX/2 ? -1 : ans;
-        
+    int ans = dp[sum];
+    if(ans  == sum+1)
+        ans = -1;
+
+    return ans;
         
     }
 };
