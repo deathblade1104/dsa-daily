87d1094176b4e495deb5185f891372426fd0a1d1
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-07-29 23:05:18 +0530

    Time: 85 ms (92.06%), Space: 43.1 MB (77.52%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    
+    if(t.length !== s.length)
+        return false;
+    
+    let sMap = {};
+    let tMap = {};
+    
+    
+    for(let i=0;i<s.length;i++)
+        {
+            if(s[i] in sMap)
+                sMap[s[i]]++;
+            
+            else
+                sMap[s[i]]=1;
+            
+            if(t[i] in tMap)
+                tMap[t[i]]++;
+            
+            else
+                tMap[t[i]]=1;
+        }
+    
+    for(let ele in sMap)
+        {
+            if(sMap[ele] !== tMap[ele])
+                return false;
+        }
+    
+    return true;
+};
