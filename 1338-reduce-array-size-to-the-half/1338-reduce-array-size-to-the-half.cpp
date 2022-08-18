1d8d7b9df22416acea60146bf17e4a21b4eff48e
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-08-19 01:44:32 +0530

    Time: 420 ms (30.09%), Space: 81.2 MB (23.55%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    struct mycomp{
+        bool operator()(const array<int,2>&a, const array<int,2>&b){
+            return a[1]<b[1];
+        }
+    };
+    
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int,int>mp;
+        
+        for(int&i : arr)
+            mp[i]++;
+            
+        priority_queue<array<int,2>,vector<array<int,2>>,mycomp>pq;
+        
+        for(auto it : mp)
+            pq.push({it.first,it.second});
+        
+        
+        int required = arr.size()/2;
+        while(required>0 and pq.size()>0){
+            auto p = pq.top();
+            pq.pop();
+            
+            
+            required-= p[1];
+        }
+        
+        return mp.size() - pq.size();
+        
+    }
+};
