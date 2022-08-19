abb6f5309596ba2c29e40d44a84ed1a0d224f666
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-08-19 19:55:59 +0530

    Time: 81 ms (99.06%), Space: 54.5 MB (88.31%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        int cnt[2001] ={0};
+        
+        for(int&i: nums)
+            cnt[i+1000]++;
+        
+        
+        for(int i=0;i<=1998;i++){
+            
+           while (cnt[i]) {
+                int c = 0;
+                while (cnt[i+c] <= cnt[i+c+1]) {
+                    cnt[i+c]--;
+                    c++;
+                }
+            
+                cnt[i+c]--;
+                c++;
+               
+                if (c < 3) return false;
+            }
+        }
+        if(cnt[1999] or cnt[2000])
+            return false;
+        
+        return true;
+        
+        
+    }
+};
