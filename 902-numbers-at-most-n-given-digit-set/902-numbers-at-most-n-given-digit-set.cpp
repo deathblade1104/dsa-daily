39aad9eceb496c5400713fe40e1661d2acfa1c51
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-09-10 02:51:43 +0530

    Time: 5 ms (24.72%), Space: 8.1 MB (68.59%) - LeetHub

---
+++ 902-numbers-at-most-n-given-digit-set/902-numbers-at-most-n-given-digit-set.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    int dp[10][2][2];
+    unordered_set<int>st;
+    int low = 9, high = 0;
+    
+    int helper(int pos,bool tight,bool leadingZeroes,string&s){
+        
+        if(pos>=s.size())
+            return leadingZeroes;
+            
+        if(dp[pos][tight][leadingZeroes]!=-1)
+            return dp[pos][tight][leadingZeroes];
+        
+        
+        int res = 0;
+        int ub = tight ? s[pos]-'0' : high;
+        int lb = leadingZeroes ? low : 0;
+        
+        for(int i=lb;i<=ub;i++){
+            if(st.count(i)){
+                // cout<<pos<<" _ "<<tight<<" _ "<<leadingZeroes<<" ## "<<i<<endl;
+                res+=helper(pos+1,tight&&(i==ub),leadingZeroes||(i>0),s); 
+            }
+        }
+        
+        return dp[pos][tight][leadingZeroes] = res;
+    }
+    
+    int atMostNGivenDigitSet(vector<string>& digits, int n) {
+        memset(dp,-1,sizeof dp);
+        string s= to_string(n);
+        
+        st.insert(0);
+        for(string&str : digits){
+            int curr = stoi(str);
+            st.insert(curr);
+            low = min(low,curr);
+            high = max(high,curr);
+        }
+        
+        return helper(0,true,false,s);
+        
+    }
+};
