0d399573ac40cbfbe28bebd06020b07cfef8023f
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-09-11 04:32:24 +0530

    Time: 25 ms (38.46%), Space: 11.8 MB (60.56%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     int dp[1001][2][101];
-	
-    int dp_fun(int k,vector<int>& prices,int idx,int b){
-	// if k==0 we simply return 0 as all the k transactions are done 
-        if(k==0){
-            return 0;
-        }
+    int earnedProfit(vector<int>& prices,int &sz,int curr,int can_buy,int transaction)
+    {
+        if(curr>=sz || transaction<=0)
+        return 0;
+            
+        if(dp[curr][can_buy][transaction]!=-1)
+            return dp[curr][can_buy][transaction];
+       
+       int buy=0,sell=0;
+            
+        if(can_buy)
+        buy = max(-prices[curr] + earnedProfit(prices,sz,curr+1,0,transaction),
+                    earnedProfit(prices,sz,curr+1,1,transaction));
+
+        else
+        sell = max(prices[curr] + earnedProfit(prices,sz,curr+1,1,transaction-1),
+                   earnedProfit(prices,sz,curr+1,0,transaction));
+
         
-		// if we reached the end of the array then we return 0;
-		
-        if(idx==prices.size()){
-            return 0;
-        }
-        
-		//  Returning the already solved problem(overlapping subproblem)
-		
-        if(dp[idx][b][k] != -1){
-            return dp[idx][b][k];
-        }
-        
-		// if buying is possible and when selling is possible
-        if(b==1){
-            return dp[idx][b][k] = max((-prices[idx]+dp_fun(k,prices,idx+1,0)), dp_fun(k,prices,idx+1,1));
-        }
-        else{
-            return dp[idx][b][k] = max((prices[idx]+dp_fun(k-1,prices,idx+1,1)), dp_fun(k,prices,idx+1,0));
-        }
+        return dp[curr][can_buy][transaction]=max(buy,sell);
     }
+
     int maxProfit(int k, vector<int>& prices) {
+        int sz=prices.size();
         
-        memset(dp,-1,sizeof(dp));
-        return dp_fun(k,prices,0,1);
+        memset(dp,-1,sizeof dp);
+        
+        return earnedProfit(prices,sz,0,1,k);
+        
     }
 };
