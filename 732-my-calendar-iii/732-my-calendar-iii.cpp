36882963e9d025a9d1736e49930448b958ef0446
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-09-13 05:10:58 +0530

    Time: 1515 ms (5.21%), Space: 309.3 MB (5.21%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.cpp
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -1,22 +1,38 @@
 class MyCalendarThree {
 public:
-    map<int,int>mp;
-    MyCalendarThree() {
+    unordered_map<int,int>mp;
+    unordered_map<int,int>lazy;
+    
+    void add(int left, int right,int start=0 ,int end=1e9+1,  int currIdx=0){
         
+        if(start>end or start>right or end<left)
+            return;
+        
+        if(start>=left and end<=right){
+            lazy[currIdx]++;
+            mp[currIdx]++;
+            return;
+        }
+    
+        int mid = start + ((end  - start)/2);
+        
+        add(left,right,start,mid,currIdx*2 +1);
+        add(left, right,mid+1,end,currIdx*2 +2);
+        
+        mp[currIdx] = lazy[currIdx] +  max(mp[currIdx*2 +1] , mp[currIdx*2 + 2]);
+        
+        return;
+        
+    }
+    MyCalendarThree() {
+        mp[0]=0;
     }
     
     int book(int start, int end) {
-        mp[start]++;
-        mp[end]--;
+         add(start,end-1);
         
-        int sum =0, ans = sum;
+        return mp[0];
         
-        for(auto&it : mp){
-            sum+=it.second;
-            ans = max(ans,sum);
-        }
-        
-        return ans;
     }
 };
 
