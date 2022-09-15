1001a53872174444e9f552df79ffe067fe5aebaf
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-09-16 04:50:20 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (5.76%) - LeetHub

--- 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
+    int dp[10][2][1025];
     string s;
-    int dp[10][2][2][1025];
-    int helper(int pos,bool tight,int st,int mask){
+    
+    int helper(int pos,bool tight,int mask){
         
         if(pos == s.size())
             return 1;
         
-        if(dp[pos][tight][st][mask]!=-1)
-            return dp[pos][tight][st][mask];
+        if(dp[pos][tight][mask]!=-1)
+            return dp[pos][tight][mask];
         
         int upperBound  = tight ? s[pos]-'0' : 9,ans =0;
     
-        if(!st){
+        if(mask==0){
+            
             for(int i=0;i<=upperBound;i++){
                 
                 if(i==0)
-                    ans+=helper(pos+1,tight&(i==upperBound),0,0);
+                    ans+=helper(pos+1,tight&(i==upperBound),0);
                 
                 else
-                    ans+=helper(pos+1,tight&(i==upperBound),1,mask|(1<<i));
+                    ans+=helper(pos+1,tight&(i==upperBound),mask|(1<<i));
             }
         }
         else {
+            
             for(int i=0;i<=upperBound;i++){
                 
                 if(mask&(1<<i))
                     continue;
                 
-                ans+=helper(pos+1,tight&(i==upperBound),1,mask|(1<<i));
+                ans+=helper(pos+1,tight&(i==upperBound),mask|(1<<i));
             }
         }
         
-        return dp[pos][tight][st][mask] = ans;
+        return dp[pos][tight][mask] = ans;
     }
     
     int countNumbersWithUniqueDigits(int n) {
@@ -40,6 +43,6 @@ public:
         s = to_string(temp);
         memset(dp,-1,sizeof dp);
         
-        return helper(0,1,0,0);
+        return helper(0,1,0);
     }
 };
