00d40dcd5faeeb35a06da337a705e39b8a228cf2
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-09-20 21:35:13 +0530

    Time: 429 ms (61.64%), Space: 16.4 MB (66.37%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     
-    int get_ans(int i,int j, int n, int m, vector<int>&s1, vector<int>&s2,vector<vector<int>>&dp,int &res)
+    int dp[1001][1001];
+    int get_ans(int i,int j, int n, int m, vector<int>&s1, vector<int>&s2,int &res)
     {
         if((i>=n) or (j>=m)) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
@@ -9,10 +10,10 @@ public:
         int ans=0;
         
         if(s1[i]==s2[j])
-        ans = 1 + get_ans(i+1,j+1,n,m,s1,s2,dp,res);
+        ans = 1 + get_ans(i+1,j+1,n,m,s1,s2,res);
         
-        int x = get_ans(i+1,j,n,m,s1,s2,dp,res);
-        int y = get_ans(i,j+1,n,m,s1,s2,dp,res);
+        int x = get_ans(i+1,j,n,m,s1,s2,res);
+        int y = get_ans(i,j+1,n,m,s1,s2,res);
         
         res=max(res,ans);
       
@@ -21,11 +22,8 @@ public:
     
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         
-        int n=nums1.size(), m = nums2.size();
-        
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        int res=-1;
-        int x = get_ans(0,0,n,m,nums1,nums2,dp,res);
+        memset(dp,-1,sizeof dp);
+        int n=nums1.size(), m = nums2.size(),res=-1,x = get_ans(0,0,n,m,nums1,nums2,res);
         return res;
     }
 };
