adf49c7183d7f641224502ba52770398ae842b75
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-10-21 10:36:30 +0530

    Time: 276 ms (10.07%), Space: 51.5 MB (52.93%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_set<int>st;
+        
+        for(int i: nums){
+            
+            if(st.count(i)==1)
+                return true;
+            
+            st.insert(i);
+        }
+        
+        return false;
+    }
+};
