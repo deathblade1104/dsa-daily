bc1f54cc18adfe980946688b1c5e738950ca556e
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-10-24 15:45:34 +0530

    Time: 196 ms (46.51%), Space: 100 MB (25.38%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool checker[26];
-    bool check_unique(string s){
+    bool checkUnique(string s){
         memset(checker,false,sizeof checker);
         
         for(char ch : s){
@@ -16,8 +16,6 @@ public:
     }
     
     bool isPossible(unordered_map<char,int>&mp,string s){
-        
-        
         for(char ch : s){
             if(mp.count(ch))
                 return false;
@@ -25,21 +23,20 @@ public:
         
         return true;
     }
-    void helper(int curr,vector<string>&arr,unordered_map<char,int>&mp,int&ans){
+    
+    void helper(int curr,vector<string>&arr,vector<bool>hasUnique,unordered_map<char,int>&mp,int&ans){
         
         if(curr == arr.size()){
             ans = max(ans,(int)mp.size());
             return;
         }
         
-        
-        
-        if(check_unique(arr[curr]) and isPossible(mp,arr[curr])){
+        if(hasUnique[curr] and isPossible(mp,arr[curr])){
             
             for(char&ch : arr[curr])
             mp[ch]++;
         
-            helper(curr+1,arr,mp,ans);
+            helper(curr+1,arr,hasUnique,mp,ans);
 
             for(char&ch : arr[curr]){
 
@@ -51,14 +48,18 @@ public:
             }
         }
         
-        helper(curr+1,arr,mp,ans);
+        helper(curr+1,arr,hasUnique,mp,ans);
         
     }
     int maxLength(vector<string>& arr) {
-        int ans = 0;
+        int ans = 0,n= arr.size();
         unordered_map<char,int>mp;
+        vector<bool>hasUnique(n);
         
-        helper(0,arr,mp,ans);
+        for(int i=0;i<n;i++)
+            hasUnique[i] = checkUnique(arr[i]);
+        
+        helper(0,arr,hasUnique,mp,ans);
         
         return ans;
         
