bf2472c4c3390c1d66090e9e8e2f673204402748
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-10-24 19:34:51 +0530

    Time: 511 ms (30.22%), Space: 69.1 MB (48.69%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int arr[100001];
+    int pref[20003];
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
         
-        memset(arr,0, sizeof arr);
-        
-        for(auto&curr : bookings){
-            arr[curr[0]]+=curr[2];
-            arr[curr[1]+1]-=curr[2];
+        memset(pref,0,sizeof pref);
+        for(auto&v : bookings){
+            pref[v[0]]+=v[2];
+            pref[v[1]+1]-=v[2];
         }
         
         vector<int>ans;
-        
         for(int i=1;i<=n;i++){
-            
-            arr[i]+=arr[i-1];
-            ans.push_back(arr[i]);
+            pref[i]+=pref[i-1];
+            ans.push_back(pref[i]);
         }    
         
         return ans;
+        
     }
 };
