52c4aafb60ffcc92fc2cf541996fce495adad1a2
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-10-26 03:45:23 +0530

    Time: 911 ms (80.13%), Space: 14.6 MB (68.16%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -8,8 +8,8 @@ public:
         if(dp[r][prev]!=-1)
             return dp[r][prev];
         
-        int ans = INT_MAX/2;
-        for(int i=0;i<m and ans!=0;i++){
+        int ans = helper(r+1,n,m,mat,target,prev+mat[r][0]);
+        for(int i=1;i<m and ans!=0;i++){
             ans = min(ans,helper(r+1,n,m,mat,target,prev+mat[r][i]));
         }
         
@@ -19,6 +19,9 @@ public:
         
         memset(dp,-1,sizeof dp);
         int n = mat.size(),m= mat[0].size(),curr=0,ans=INT_MAX;
+        
+        for(int i=0;i<n;i++)
+            sort(mat[i].begin(),mat[i].end());
         
         return helper(0,n,m,mat,target,0);
         
