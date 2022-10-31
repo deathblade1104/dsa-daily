4c60257dd3eecda349b391e7f075667c27003cc4
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-10-31 05:58:33 +0530

    Time: 35 ms (38.25%), Space: 17.5 MB (15.16%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -5,37 +5,35 @@ public:
         int m = matrix.size(),n = matrix[0].size();
         
         for(int c1=0;c1<n;c1++){
-            int r = 0,c=c1;
-            
-            unordered_set<int>st;
+            int r = 0,c=c1,prev = matrix[r][c];
+            r++,c++;
             
             while(r<m and c<n){
                 
-                st.insert(matrix[r][c]);
+                int curr = matrix[r][c];
                 
-                if(st.size()>=2)
+                if(curr!=prev)
                     return false;
                 
                 r++;
                 c++;
             }
-            
         }
         
         for(int r1=1;r1<m;r1++){
-            int r = r1,c=0;
+            int r = r1,c=0,prev = matrix[r][c];
+            r++,c++;
             
-            unordered_set<int>st;
             while(r<m and c<n){
                 
-                st.insert(matrix[r][c]);
+                int curr = matrix[r][c];
                 
-                if(st.size()>=2)
+                if(curr!=prev)
                     return false;
                 
                 r++;
                 c++;
-            }   
+            }
         }
         
         return true;
