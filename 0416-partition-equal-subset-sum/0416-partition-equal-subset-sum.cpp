06c9bce68a637f7d74bb66d1b0ea96c33c5bcfb6
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-11-02 15:08:21 +0530

    Time: 299 ms (69.64%), Space: 27.2 MB (46.47%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -2,27 +2,30 @@ class Solution {
 public:
     int dp[201][20001];
     
-    bool get_ans(int curr, int val, int target, vector<int>&nums)
+    bool get_ans(int curr, int target, vector<int>&nums)
     {
-        if(target==val) return true;
+        if(target==0) return true;
         
         if(curr>=nums.size())
             return false;
         
-        if(dp[curr][val]!=-1)
-            return dp[curr][val];
+        if(dp[curr][target]!=-1)
+            return dp[curr][target];
         
-        bool op1 = get_ans(curr+1, val+nums[curr],target, nums);
+        bool op1 = false;
+        
+        if(nums[curr]<=target)
+        op1 =get_ans(curr+1, target - nums[curr], nums);
         
         if(op1)
-            return dp[curr][val] = true;
+            return dp[curr][target] = true;
             
-        bool op2 = get_ans(curr+1, val,target, nums);
+        bool op2 = get_ans(curr+1, target, nums);
         
-        return dp[curr][val] = op1 | op2;
+        return dp[curr][target] = op1 | op2;
     }
     bool canPartition(vector<int>& nums) {
-        memset(dp,-1,201*20001*sizeof(int));
+        memset(dp,-1,sizeof dp);
         int sum=0;
         
         for(int i : nums)
@@ -31,8 +34,8 @@ public:
         if(sum%2!=0)
             return false;
         
-        else
-            return get_ans(0,0,sum/2,nums);
+        sort(nums.begin(),nums.end());
+        return get_ans(0,sum/2,nums);
         
     }
 };
