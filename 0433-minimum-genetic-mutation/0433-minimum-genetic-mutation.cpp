201f5d7be00cfff0af37e61a49061aa08b6f5c35
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-11-02 23:48:26 +0530

    Time: 3 ms (45.67%), Space: 6.7 MB (15.87%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int minMutation(string start, string end, vector<string>& bank) {
+        vector<char>validChars={'A','C','G','T'};
+        unordered_set<string>st(bank.begin(),bank.end());
+        
+        queue<string>q;
+        q.push(start);
+        int steps = 0;
+        
+        while(q.size()){
+            
+            int sz = q.size();
+            
+            while(sz --){
+                
+                string s = q.front();
+                q.pop();
+                
+                if(s == end)
+                    return steps++;
+                
+                for(int i=0;i<s.size();i++){
+                    
+                    string left = s.substr(0,i);
+                    string right = s.substr(i+1);
+                    
+                    for(char&ch : validChars){
+                        
+                        if(ch != s[i]){
+                            string temp  = left;
+                            temp+=ch;
+                            temp+=right;
+                            
+                            if(st.count(temp)){
+                                st.erase(temp);
+                                q.push(temp);
+                            }
+                        }
+                    }
+                }
+            }
+            steps++;
+        }
+        
+        return -1;
+    }
+};
