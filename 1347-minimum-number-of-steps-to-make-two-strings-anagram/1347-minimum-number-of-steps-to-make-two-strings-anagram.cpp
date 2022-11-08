331a5299d71641ca449b71d808198fd129991134
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-11-08 16:16:34 +0530

    Time: 83 ms (84.60%), Space: 14 MB (99.65%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     
-    int arr1[26],arr2[26];
+    int arr[26];
     int minSteps(string&s, string&t) {
         
         
-        memset(arr1,0,sizeof arr1);
-        memset(arr2,0,sizeof arr2);
-        
+        memset(arr,0,sizeof arr);
         
         for(char&ch : s)
-            arr1[ch - 'a']++;
+            arr[ch - 'a']++;
         
         for(char&ch : t)
-            arr2[ch - 'a']++;
+            arr[ch - 'a']--;
         
         
         int ans = 0;
         
         for(int i=0;i<26;i++)
-            ans+=(abs(arr1[i] - arr2[i]));
+            ans+=(abs(arr[i]));
         
         return ans/2;
     }
