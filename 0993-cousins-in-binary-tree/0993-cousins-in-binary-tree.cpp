0188324c5859c8bc7ed937a3a4e006d61443ed49
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-11-10 01:38:35 +0530

    Time: 12 ms (12.85%), Space: 11.1 MB (48.33%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
@@ -1,51 +1,51 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    
-    
-    void getLevelAndGrandParent(TreeNode *root,int par,int&lvl,int&val,pair<int,int>&ans){
-        
-        if(!root)
-            return;
-        
-        if(root->val == val){
-            ans.first = par;
-            ans.second = lvl;
+class Solution
+{
+    public:
+        bool isCousins(TreeNode *root, int x, int y)
+        {
+            queue<pair<TreeNode*, int>> q;
+            pair<int,int>px,py;
+            q.push(make_pair(root,-1));
+
+            int lvl = 1;
             
-            return;
+            while (!q.empty())
+            {
+                int size = q.size();
+                while(size--)
+                {
+                    pair<TreeNode*,int> currnode = q.front();
+                    q.pop();
+                    TreeNode *f = currnode.first;
+                    int s = currnode.second;
+                    
+                    if (f->val == x) px.first = s,px.second = lvl;
+                    if (f->val == y) py.first = s,py.second = lvl;
+                    
+                    if (f->left != NULL) 
+                        q.push({ f->left,f->val});
+                    
+                    if (f->right != NULL) 
+                        q.push({ f->right,f->val});
+                }
+                
+                lvl++;
+            }
+            
+            if(px.first!=py.first and px.second == py.second)
+                return true;
+            
+            return false;
         }
-        lvl++;
-        
-        getLevelAndGrandParent(root->left,root->val,lvl,val,ans);
-        getLevelAndGrandParent(root->right,root->val,lvl,val,ans);
-        
-        lvl --;
-        
-        return;
-    }
-    bool isCousins(TreeNode* root, int x, int y) {
-        
-        
-        pair<int,int>p1,p2;
-        int lvl = 1;
-        getLevelAndGrandParent(root,-1,lvl,x,p1);
-        getLevelAndGrandParent(root,-2,lvl,y,p2);
-        
-        
-        if(p1.first != p2.first and p1.second == p2.second)
-            return true;
-        
-        
-        return false;
-    }
 };
