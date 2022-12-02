146ebf3cc4c4fdd036a9fbd89bdd07f2fa74910d
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-12-03 03:00:20 +0530

    Time: 238 ms (53.12%), Space: 20.4 MB (83.09%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    int c1[26],c2[26];
+    int w1[26],w2[26];
     bool closeStrings(string word1, string word2) {
         
         if(word1.size() != word2.size())
             return false;
         
-        vector<int>w1(26,0),w2(26,0);
+        memset(w1,0,sizeof w1);
+        memset(w2,0,sizeof w2);
 
         for(char c:word1)
             w1[c-'a']++;
@@ -22,8 +23,14 @@ public:
             else return false;
         }
         
-        sort(begin(w1),end(w1));
-        sort(begin(w2),end(w2));
-        return w1==w2;
+        sort(w1,w1 + 26);
+        sort(w2,w2 + 26);
+        
+        for(int i=0;i<26;i++){
+            
+            if(w1[i]!=w2[i])
+                return false;
+        }
+        return true;
     }
 };
