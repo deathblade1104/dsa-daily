13e5ea4d3a39a57ddc0e103b1babaf34d0a95f11
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-12-04 00:11:52 +0530

    Time: 6 ms (99.32%), Space: 8.4 MB (67.07%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
-    struct mycomp
-    {
-        bool operator()(const pair<int,int>&a, const pair<int,int>&b)
-        {
-            return a.second<b.second;
-        }
-    };
-    
+    int counter[128];
     string frequencySort(string s) {
-        int mp[128];
-        memset(mp,0,sizeof(mp));
         
-        for(char ch : s)
-            mp[ch]++;
+        memset(counter,0,sizeof counter);
         
-        priority_queue<pair<int,int>,vector<pair<int,int>>,mycomp>pq;
-        
-        for(int i=0;i<128;i++)
-        {
-            if(mp[i]>0)
-                pq.push({i,mp[i]});
+        for(char&ch : s){
+            counter[ch]++;
         }
         
-        s="";
         
-        while(pq.size()>0)
-        {            
-            auto p = pq.top();
-            pq.pop();
-            
-            while(p.second--)
-            s+=(char)(p.first);
+        set<pair<int,char>>st;
+        
+        for(int i=0;i<128;i++){
+            if(counter[i]){
+                st.insert({counter[i],(char)i});
+            }
         }
         
-        return s;
+        string ans="";
+        for(auto&it : st){
+            int i=0;
+            while(i!=it.first){
+                i++;
+                ans+=it.second;
+            }
+        }
         
+        reverse(ans.begin(),ans.end());
+        return ans;
         
-            
     }
 };
