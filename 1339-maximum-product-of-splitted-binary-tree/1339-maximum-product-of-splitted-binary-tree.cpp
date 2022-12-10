b22819386aafe9f0f6b080c60da44d7d7f2adb8a
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-12-10 13:51:20 +0530

    Time: 301 ms (47.68%), Space: 77.2 MB (89.61%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -11,33 +11,38 @@
  */
 class Solution {
 public:
-    const long long int mod = 1e9 + 7;
+    const int mod = 1e9 + 07;
+    int treeSum;
+    long long ans;
     
-    long long int getSum(TreeNode *root){
-        
-        if(!root)
-            return 0LL;
-        
-        return (root->val)*1LL + getSum(root->left) + getSum(root->right);
-    }
-    
-    long long int dfsHelper(TreeNode *root, long long int&treeSum, long long int&ans){
+    int getTreeSum(TreeNode *root){
         
         if(!root)
             return 0;
         
-        long long int l = dfsHelper(root->left,treeSum,ans),
-        r = dfsHelper(root->right,treeSum,ans),
-        currSum = (root->val*1LL) + l + r,
-        currAns = (treeSum - currSum) * currSum * 1LL;
+        return getTreeSum(root->left) + getTreeSum(root->right) + root->val;
+    }
+    
+    int getAns(TreeNode *root){
         
-        ans = max(ans,currAns);
+        if(!root)
+            return 0;
+        
+        
+        int currSum = root->val + getAns(root->left) + getAns(root->right), remSum = treeSum - currSum;
+        ans = max(ans,(currSum * 1LL * remSum));
         
         return currSum;
-        
     }
+    
+    
     int maxProduct(TreeNode* root) {
-        long long int treeSum = getSum(root),ans=0LL,temp= dfsHelper(root,treeSum,ans);
+        
+        ans = 0LL;
+        treeSum = getTreeSum(root);
+        
+        int temp = getAns(root);
+        
         return (int)(ans%mod);
     }
 };
