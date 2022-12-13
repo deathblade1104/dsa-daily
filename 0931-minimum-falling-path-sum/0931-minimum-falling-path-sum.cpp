206e8c29d1b76700cf76246fbb6604e8cfdbc394
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-12-13 08:14:58 +0530

    Time: 23 ms (71.93%), Space: 10 MB (69.80%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,60 +1,33 @@
-class Solution
-{
-    public:
-    int solve(int i, int j, vector<vector < int>> &matrix, int m, int n, int dp[102][102])
-    {
-        if (j < 0 || j >= n)
-        {
-            return INT_MAX/2;
-        }
-
-        if (i == m - 1)
-        return dp[i][j] = matrix[i][j];
+class Solution {
+public:
+    int dp[101][101];
+    
+    int helper(int i, int j, int&n, vector<vector<int>>&matrix){
         
-        int currkeyrow = i;
-        int currkeycol = j;
-
-        //         string currkey=to_string(currkeyrow) + '-' + to_string(currkeycol);
-
-        if (dp[currkeyrow][currkeycol] != -1)
-        {
-            return dp[currkeyrow][currkeycol];
-        }
-
-        //3 choices
-
-        //down
-        int down = matrix[i][j] + solve(i + 1, j, matrix, m, n, dp);
-
-        //left down
-        int left = matrix[i][j] + solve(i + 1, j - 1, matrix, m, n, dp);
-
-        //right down
-
-        int right = matrix[i][j] + solve(i + 1, j + 1, matrix, m, n, dp);
-
-        int mini = min(down, left);
-
-        dp[currkeyrow][currkeycol] = min(mini, right);
-
-        return min(mini, right);
+        if(j<0 or j>=n)
+            return 10001;
+        
+        if(i == n-1)
+            return dp[i][j] = matrix[i][j];
+        
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        return dp[i][j] = matrix[i][j] + min({
+            helper(i+1,j-1,n,matrix),
+            helper(i+1,j,n,matrix),
+            helper(i+1,j+1,n,matrix)
+        });
     }
     
-    int minFallingPathSum(vector<vector < int>> &matrix)
-    {
-
-        int m = matrix.size();
-        int n = matrix[0].size();
-
-        int dp[102][102];
-        memset(dp, -1, sizeof(dp));
-
-        int ans = INT_MAX;
-        for (int j = 0; j < n; j++)
-        {
-            ans = min(ans, solve(0, j, matrix, m, n, dp));
-        }
-
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        memset(dp,-1,sizeof dp); 
+        
+        int n = matrix.size(),ans = INT_MAX;
+        
+        for(int i=0;i<n;i++)
+            ans= min(ans,helper(0,i,n,matrix));
+        
         return ans;
     }
 };
