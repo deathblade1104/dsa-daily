382b9f05927ceb1fe6788921e5f14200970e05ba
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-12-14 21:34:38 +0530

    Time: 36 ms (96.46%), Space: 28.2 MB (66.43%) - LeetHub

---
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    int mp[60];
+    
+    
+    int numPairsDivisibleBy60(vector<int>& time) {
+        
+        memset(mp,0,sizeof mp);
+        int ans = 0;
+        
+        for(int&i : time){
+            
+            if(i%60!=0)
+            ans+=mp[60 - i%60];
+            
+            else
+            ans+=mp[0];
+            
+            mp[i%60]++;
+        }
+        
+        return ans;
+        
+    }
+};
