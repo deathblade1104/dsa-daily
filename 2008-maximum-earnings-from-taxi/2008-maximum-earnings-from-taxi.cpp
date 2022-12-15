d51bfb507e0d8baaeb32707ad5e0420aa17b8023
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-12-15 22:38:37 +0530

    Time: 596 ms (91.98%), Space: 100.3 MB (80.83%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    long long maxTaxiEarnings(int n, vector<vector<int>>&rides) {
-        
-        for(int i=0;i<rides.size();i++){
-            swap(rides[i][0],rides[i][1]);
-            rides[i][2]+=rides[i][0] - rides[i][1];
+    
+    long long maxTaxiEarnings(int n, vector<vector<int>>&pairs) {
+        for(int i=0;i<pairs.size();i++){
+            swap(pairs[i][0],pairs[i][1]);
+            pairs[i][2]+=pairs[i][0] - pairs[i][1];
         }
         
-        sort(rides.begin(),rides.end());
+        sort(pairs.begin(),pairs.end());
         
         map<int,long long>dp;
         dp[0] = 0LL;
         
-        for (auto& job : rides) {
-            long long cur = 0LL +  prev(dp.upper_bound(job[1]))->second + job[2];
+        for (auto& job : pairs) {
+            long long cur = 0LL  + job[2];
+            cur+= prev(dp.upper_bound(job[1]))->second;
+            
             if (cur > dp.rbegin()->second)
                 dp[job[0]] = cur;
         }
+        
         return dp.rbegin()->second;
         
 
