8a37d0de496263daa91eed17e927ce8c4951d023
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-12-15 22:31:23 +0530

    Time: 596 ms (91.98%), Space: 100.4 MB (79.49%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
@@ -1,64 +1,25 @@
 class Solution {
 public:
-    
-    struct mycomp{
+    long long maxTaxiEarnings(int n, vector<vector<int>>&rides) {
         
-        bool operator()(const vector<int>&a, const vector<int>&b){
-            
-            if(a[1]!=b[1])
-                return a[1]>b[1];
-            
-            else{
-                
-                if(a[0]!=b[0])
-                    return a[0]>b[0];
-                
-                else
-                    return a[2]>=b[2];
-            }
-        }
-    };
-    
-    long long maxTaxiEarnings(int n, vector<vector<int>>&pairs) {
-//         map<int,long long>mp;
-//         mp[n] = 0LL;
-//         long long ans = 0;
-        
-//         sort(rides.begin(),rides.end(),mycomp());
-        
-//         for(auto&v : rides){
-            
-//             int start = v[0], end= v[1], tip = v[2];
-            
-//             auto it = mp.lower_bound(end);
-//             long long next = it->second;
-            
-//             mp[start] = max(mp[start], 0LL + end - start + tip + next);
-            
-
-//             cout<<start<<" ---> "<<mp[start]<<" with previous trip ending at : "<<it->first<<" of charge => "<<next<<endl;
-            
-//             ans = max(ans,mp[start]);
-//         }
-        
-        for(int i=0;i<pairs.size();i++){
-            swap(pairs[i][0],pairs[i][1]);
-            pairs[i][2]+=pairs[i][0] - pairs[i][1];
+        for(int i=0;i<rides.size();i++){
+            swap(rides[i][0],rides[i][1]);
+            rides[i][2]+=rides[i][0] - rides[i][1];
         }
         
-        sort(pairs.begin(),pairs.end());
+        sort(rides.begin(),rides.end());
         
         map<int,long long>dp;
         dp[0] = 0LL;
         
-        for (auto& job : pairs) {
+        for (auto& job : rides) {
             long long cur = 0LL +  prev(dp.upper_bound(job[1]))->second + job[2];
             if (cur > dp.rbegin()->second)
                 dp[job[0]] = cur;
         }
         return dp.rbegin()->second;
         
-        //return ans;
+
      
         
     }
