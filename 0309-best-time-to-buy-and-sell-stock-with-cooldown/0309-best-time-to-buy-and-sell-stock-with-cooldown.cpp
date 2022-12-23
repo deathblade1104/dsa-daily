5211d4a36ce3214f61024bda7ed642ba5f1a498e
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2022-12-23 22:32:18 +0530

    Time: 11 ms (39.16%), Space: 12.4 MB (9.65%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     
-    int dp[5001][2][1001];
-    int helper(int curr,bool canBuy,int prev, vector<int>&prices){
+    int dp[5001][2];
+    int helper(int curr,bool canBuy, vector<int>&prices){
         if(curr>=prices.size())
             return 0;
         
-        
-        if(dp[curr][canBuy][prev]!=-1){
-            return dp[curr][canBuy][prev];
+        if(dp[curr][canBuy]!=-1){
+            return dp[curr][canBuy];
         }
         
-        int doNothing = helper(curr+1,canBuy,prev,prices);
+        int doNothing = helper(curr+1,canBuy,prices);
         
         int sold=INT_MIN,buy=INT_MIN;
         
-        
         if(canBuy){
             
-            buy = -prices[curr] + helper(curr+1,false,prices[curr],prices);
+            buy = -prices[curr] + helper(curr+1,false,prices);
         }
         
-        if(!canBuy and prices[curr]>prev){
-            
-            sold = prices[curr] + helper(curr+2,true,0,prices);
+        else{
+            sold = prices[curr] + helper(curr+2,true,prices);
         }
         
         
-        return dp[curr][canBuy][prev] = max({doNothing,sold,buy});
+        return dp[curr][canBuy]= max({doNothing,sold,buy});
     }
     int maxProfit(vector<int>& prices) {
         
         memset(dp,-1,sizeof dp);
-        return helper(0,1,0,prices);
+        return helper(0,1,prices);
         
     }
 };
