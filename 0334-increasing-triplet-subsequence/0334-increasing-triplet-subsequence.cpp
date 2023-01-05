59101ca207c23b2f7a2a9d74cfab549b51d76618
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-01-05 17:56:18 +0530

    Time: 214 ms (51.90%), Space: 115.1 MB (8.70%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        vector<int>ans;
-        ans.push_back(nums[0]);
-        for(int i=1;i<nums.size();i++){
-            int it = lower_bound(ans.begin(),ans.end(),nums[i]) - ans.begin();
+        int n = nums.size();
+        
+        vector<int>suffix(n,-1);
+        suffix[n-1] = nums.back();
+    
+        for(int i=n-2;i>=0;i--)
+        suffix[i] = max(nums[i],suffix[i+1]);
+        
+        set<int>st;
+        st.insert(nums[0]);
+        
+        for(int i=1;i<n-1;i++){
+            st.insert(nums[i]);
             
-            if(it == ans.size())
-                ans.push_back(nums[i]);
+            auto it = st.find(nums[i]);
             
-            else
-                ans[it] = nums[i];
+            if(it == st.begin())
+                continue;
             
-            if(ans.size()>=3)
+            int op1 = *--it, op2=suffix[i];
+            
+            if(op1<nums[i] and nums[i]<op2)
+            {
+                // cout<<op1<<" "<<nums[i]<<" "<<op2<<endl;
                 return true;
+            }
         }
         
         return false;
