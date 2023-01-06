8d75063c4177b43c31084dd140c403d0142430e2
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-01-06 17:32:29 +0530

    Time: 102 ms (79.40%), Space: 10.3 MB (79.67%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.cpp
@@ -9,21 +9,14 @@ public:
         for(int i=0;i<n;i++){
             
             int currChar = s[i] - 'a',curr=0;
-            // start = currChar - k + 26,end = currChar + k;
-            // cout<<s[i]<<" : "<<endl;
-            // start%=26;
-            // end%=26;
-            // cout<<start<<" __ "<<end<<endl;
+           
             for(int j=0;j<26;j++){
                 if(abs(j - currChar)<=k)
-                curr = max(curr,1 + count[j]);
-                
-                //cout<<(char)(j+'a')<<" ----> "<<1 + count[j]<<" :: "<<curr<<endl;
+                    curr = max(curr,1 + count[j]);
             }
             
             count[currChar] = max(count[currChar],curr);
             ans = max(ans,count[currChar]);
-            //cout<<"currAns ==>"<<ans<<endl;
         }
         
         return ans;
