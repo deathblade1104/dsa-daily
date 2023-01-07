a7b0c546eba6fd44327846fcb5b769dcf242b8c4
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-01-07 18:33:06 +0530

    Time: 31 ms (58.65%), Space: 18.7 MB (27.94%) - LeetHub

---
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+    int dp1[10001],dp2[10001];
+    
+    void getLIS(vector<int>&nums,int&n){
+        
+        memset(dp1,0,sizeof dp1);
+        
+        for(int i=0;i<n;i++){
+            int curr = 1;
+            int prev = max(0,i-1);
+            
+            if(nums[prev]<nums[i])
+                curr = 1 + dp1[prev];
+            
+            dp1[i] = curr;
+        }
+        return;
+    }
+    
+    void getLDS(vector<int>&nums,int&n){
+        
+        memset(dp2,0,sizeof dp1);
+        
+        for(int i=n-1;i>=0;i--){
+            int curr = 1;
+            int prev = min(n-1,i+1);
+            
+            if(nums[prev]<nums[i])
+                curr = 1 + dp2[prev];
+            
+            dp2[i] = curr;
+        }
+        return;
+    }
+    
+    int longestMountain(vector<int>& arr) {
+        
+        int n = arr.size(),ans = 0;
+        getLIS(arr,n);
+        getLDS(arr,n);
+        
+        for(int i=0;i<n;i++)
+            if(dp1[i]>1 and dp2[i]>1)
+                ans = max(ans,dp1[i] + dp2[i] - 1);
+        
+        return ans;
+        
+        
+    }
+};
