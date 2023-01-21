534140088cd1d0123d9f13e97469e4924d4d43a9
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-01-21 14:48:00 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (89.32%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    
+    bool isValid(string&s){
+        
+        if(s.size()>3 or !s.size())
+            return false;
+        
+        if(s[0] == '0' and s.size()>1)
+            return false;
+        
+        if(stoi(s)>255)
+            return false;
+        
+        return true;
+    }
+    
+    
+    string stringMaker(string&s1,string&s2,string&s3,string&s4){
+        
+        return s1+"."+s2+"."+s3+"."+s4;
+    }
+    
+    vector<string> restoreIpAddresses(string s) {
+        
+        vector<string>ans;
+        
+        for(int i=0;i<3;i++){
+            string first = s.substr(0,i+1);
+            
+            if(!isValid(first))
+                continue;
+            
+            //cout<<i<<" i --> "<<first<<endl;
+            for(int j=i+1;j-i<=3 and j<s.size();j++){
+                string second = s.substr(i+1,j-i);
+                
+                if(!isValid(second))
+                    continue;
+                
+                //cout<<j<<" j --> "<<second<<endl;
+                
+                for(int k=j+1;k-j<=3 and k<s.size();k++){
+                    string third = s.substr(j+1,k-j);
+                    
+                    if(!isValid(third))
+                        continue;
+                    
+                    //cout<<k<<" k --> "<<third<<endl;
+                    
+                    for(int l=k+1;l-k<=3 and l<s.size();l++){
+                    
+                        string fourth = s.substr(k+1,l-k);
+                        
+                        if(!isValid(fourth))
+                            continue;
+                        
+                        if(l==s.size()-1)
+                        ans.push_back(stringMaker(first,second,third,fourth));
+                        
+                        //cout<<l<<" l --> "<<fourth<<endl;
+                    }
+                }
+            }
+        }
+        
+        return ans;
+        
+        
+        
+    }
+};
