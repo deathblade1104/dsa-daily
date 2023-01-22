89311ffc8a99bae69f2bc1a922ebd19af6d9fb2c
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-01-22 16:17:23 +0530

    Time: 87 ms (99.03%), Space: 49.1 MB (99.21%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     
-    int palinDP[17][17];
-    
-    int isPalinHelper(int i,int j, string &s){
+    bool isPalin(int i,int j, string&s){
+
+        if(i == j)
+            return true;
         
-        if(i==j)
-            return palinDP[i][j] = true;
-        if(i == j-1)
-            return palinDP[i][j] = (s[i]==s[j])? true : false;
+        while(i<=j){
+            
+            if(s[i]!=s[j])
+                return false;
+            
+            i++;
+            j--;
+        }
         
-        if(palinDP[i][j]!=-1)
-            return palinDP[i][j];
-        
-        bool ans = false;
-        
-        if(s[i] == s[j])
-            ans = true & isPalinHelper(i+1,j-1,s);
-        
-        bool x = isPalinHelper(i+1,j,s) , y = isPalinHelper(i,j-1,s);
-        
-        return palinDP[i][j] = ans;
+        return true;
     }
     
-    void helper(int curr,string&s,vector<string>&temp,vector<vector<string>>&ans){
+    void helper(int curr,string&s, vector<string>&temp,vector<vector<string>>&ans){
         
         
         if(curr>=s.size()){
@@ -32,8 +27,7 @@ public:
         }
         
         for(int i=curr;i<s.size();i++){
-
-            if(palinDP[curr][i]){
+            if(isPalin(curr,i,s)){
                 temp.push_back(s.substr(curr,i-curr+1));
                 helper(i+1,s,temp,ans);
                 temp.pop_back();
@@ -41,14 +35,8 @@ public:
         }
         
         return;
-        
     }
-    
     vector<vector<string>> partition(string s) {
-        
-        memset(palinDP,-1,sizeof palinDP);
-        
-        int x = isPalinHelper(0,(int)s.size()-1,s);
         
         vector<vector<string>>ans;
         vector<string>temp;
