b23372e636e58e25f3bec01403cf2964170ebf62
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-01-23 12:12:09 +0530

    Time: 221 ms (57.54%), Space: 60.6 MB (95.56%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        
+        int out[n+1],in[n+1];
+        memset(out,0,sizeof out);
+        memset(in,0,sizeof in);
+        
+        for(auto&v : trust){
+            out[v[0]]++;
+            in[v[1]]++;
+        }
+        
+        for(int i=1;i<=n;i++){
+            
+            if(out[i] ==0 and in[i]==n-1)
+                return i;
+        }
+        
+        return -1;
+    }
+};
