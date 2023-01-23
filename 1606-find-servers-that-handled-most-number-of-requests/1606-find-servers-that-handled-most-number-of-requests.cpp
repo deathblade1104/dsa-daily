4a77d96470536ddd815c7bb3bf020a4a249248db
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-01-23 22:57:29 +0530

    Time: 539 ms (55.19%), Space: 122.1 MB (30.66%) - LeetHub

---
+++ 1606-find-servers-that-handled-most-number-of-requests/1606-find-servers-that-handled-most-number-of-requests.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    int count[(int)(1e5) + 1];
+    
+    void insert(int x,int arr,int load,set<int>&st,priority_queue<array<long long,2>,vector<array<long long,2>>,greater<array<long long,2>>>&pq){
+        st.erase(x);
+        count[x]++;
+        
+        pq.push({arr+load,x});
+        return;
+    }
+    
+    vector<int>getMax(int k){
+        
+        int res=-1;
+        vector<int>ans;
+        
+        for(int i=0;i<k;i++){
+            
+            if(count[i]>res){
+                ans.clear();
+                res = count[i];
+            }
+            
+            if(count[i] == res){
+                ans.push_back(i);
+            }
+        }
+        
+        return ans;
+    }
+    vector<int> busiestServers(int k, vector<int>& arrival, vector<int>& load) {
+        memset(count,0,sizeof count);
+        
+        set<int>st;
+        for(int i=0;i<k;i++){
+            st.insert(i);
+        }
+        
+        priority_queue<array<long long,2>,vector<array<long long,2>>,greater<array<long long,2>>>pq;
+        
+        for(int i=0;i<arrival.size() and i<k;i++)
+            insert(i,arrival[i],load[i],st,pq);
+            
+        
+        for(int i=k;i<arrival.size();i++){
+            
+            while(pq.size() and pq.top()[0]<=arrival[i]){
+                st.insert(pq.top()[1]);
+                pq.pop();
+            }
+            
+            if(st.size() == 0)
+                continue;
+            
+            auto server = st.lower_bound(i%k);
+            
+            if(server == st.end())
+                server = st.begin();
+
+            insert(*server,arrival[i],load[i],st,pq);
+            
+        }
+        
+        return getMax(k);
+    }
+};
