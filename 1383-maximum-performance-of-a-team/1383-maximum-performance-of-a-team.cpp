7e0418e222bbf26800e37a998adaabfc0fa96f8a
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-01-24 16:54:19 +0530

    Time: 107 ms (54.47%), Space: 36.7 MB (69.11%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,34 +1,37 @@
+typedef array<int,2> arr;
+
 class Solution {
 public:
     int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
         
-        vector<array<int,2>>v;
+        vector<arr>v;
         
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++){
             v.push_back({efficiency[i],speed[i]});
+        }
         
-        sort(v.begin(),v.end(),greater<array<int,2>>());
+        sort(v.rbegin(),v.rend());
+        
+        long long ans = 0, currSum =0;
         
         priority_queue<int,vector<int>,greater<int>>pq;
+        const int mod = 1e9 + 7;
         
-        long long ans=0, curr=0,speedSum=0, mod = 1e9 + 7;
-        
-        for(auto&p : v){
-        
-            pq.push(p[1]);
-            speedSum+=p[1];
+        for(auto&itr : v){
+            
+            currSum+=(itr[1]*1LL);
+            pq.push(itr[1]);
             
             while(pq.size()>k){
-                speedSum-=pq.top();
+                currSum-=pq.top();
                 pq.pop();
             }
             
-            curr = (speedSum* p[0]);
             
-            ans = max(ans,curr);
+            ans= max(ans,(1LL * itr[0] *currSum));
+            
         }
         
-        
-        return ans%mod;
+        return (int)(ans%mod);
     }
 };
