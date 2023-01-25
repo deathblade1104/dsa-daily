a3d53a9cafe82a6e894fe4f5d91f6ce9efbd0929
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-01-26 01:45:51 +0530

    Time: 373 ms (52.57%), Space: 131.7 MB (36.86%) - LeetHub

---
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    
+    vector<int>BFShelper(int n, vector<int>adj[],int src){
+        
+        vector<int>dist(n,INT_MAX);
+        
+        dist[src] = 0;
+
+        queue<int>q;
+        q.push(src);
+        
+        while(q.size()){
+            
+            int sz = q.size();
+            while(sz --){
+                
+                int curr = q.front();
+                q.pop();
+                
+                for(int&neigh : adj[curr]){
+                    
+                    if(dist[neigh]==INT_MAX){
+                        dist[neigh] = dist[curr]+1;
+                        q.push(neigh);
+                    }   
+                }
+            }
+        }
+        
+        return dist;
+    }
+    
+    
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        int n = edges.size();
+        vector<int>adj[n];
+        
+        for(int i=0;i<n;i++){
+            
+            if(edges[i]!=-1){
+                adj[i].push_back(edges[i]);
+            }
+        }
+        
+        vector<int>dist1 = BFShelper(n,adj,node1);
+        vector<int>dist2 = BFShelper(n,adj,node2);
+        
+        int ans = -1,res = INT_MAX;
+        
+        for(int i=0;i<n;i++){
+            
+            if(dist1[i]!=INT_MAX and dist2[i]!=INT_MAX){
+                
+                int currRes = max(dist1[i],dist2[i]);
+                
+                if(currRes<res){
+                    res = currRes;
+                    ans = i;
+                }
+            }
+            
+            //cout<<i<<" -- "<<dist1[i]<<" -- "<<dist2[i]<<endl;
+        }
+        
+        return ans; 
+    }
+};
