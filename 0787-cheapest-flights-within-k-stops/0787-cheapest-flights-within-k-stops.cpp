311ebb293a085fc06f598324a5b1f0c9994213ee
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-01-27 13:59:29 +0530

    Time: 24 ms (83.02%), Space: 13 MB (84.96%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,32 +1,51 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int>dist(n,INT_MAX);
-        dist[src] =0;
         
-        for(int i=0;i<=k;i++)
-        {
-            vector<int>temp = dist;
-            bool flag = false;
+        vector<array<int,2>>adj[n];
+        
+        for(auto&f : flights){
             
-            for(auto&x : flights)
-	        {
-	            int source= x[0], dest= x[1], weight = x[2];
-	            // cout<<x[0]<<"_"<<x[1]<<"_"<<x[2]<<endl;
-	            if(dist[source] != INT_MAX and temp[dest] > dist[source] + weight)
-	            {
-	                flag = true;
-	                temp[dest] = dist[source] + weight;
-	            }
-            }
-            
-            dist=temp;
-                        
-            if(!flag)
-                break;
+            adj[f[0]].push_back({f[1],f[2]});
         }
         
-        return dist[dst] == INT_MAX ?  - 1 : dist[dst];
+        queue<array<int,2>>q;
+        int dist[n];
         
+        for(int i=0;i<n;i++){
+            dist[i] = INT_MAX;
+        }
+        dist[src] = 0;
+        
+        q.push({src,0});
+        int steps = 0;
+        
+        while(steps<=k and q.size()){
+            
+            int sz = q.size();
+            
+            while(sz --){
+                
+                array<int,2>arr= q.front();
+                q.pop();
+                
+                //cout<<arr[0]<<" ";
+                for(auto&v : adj[arr[0]]){
+                    
+                    int neigh = v[0], neighDist = v[1] + arr[1];
+                    //cout<<neigh<<" , ";
+                    if(dist[neigh]>neighDist){
+                        dist[neigh] = neighDist;
+                        q.push({neigh,neighDist});
+                    }
+                }
+                //cout<<endl;
+            }
+            
+            steps++;
+        }
+        
+        
+        return dist[dst] == INT_MAX? -1 : dist[dst];
     }
 };
