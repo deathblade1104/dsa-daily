2e2619790bcd60f6c569564160ace81aa08a00be
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-01-27 14:39:42 +0530

    Time: 54 ms (52.43%), Space: 11.6 MB (85.95%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    map<array<int,3>,int>dp;
+    int dp[501][501][2];
     
     int helper(int i,int j,bool prev,vector<int>&nums1,vector<int>&nums2){
         
         if(i>=nums1.size() or j>=nums2.size())
             return prev == false ? INT_MIN/2 : 0;
         
-        array<int,3>curr = {i,j,prev};
-        
-        if(dp.count(curr))
-            return dp[curr];
+        if(dp[i][j][prev]!=INT_MIN)
+            return dp[i][j][prev];
         
         int select =(nums1[i] * nums2[j]) + helper(i+1,j+1,prev|true,nums1,nums2);
         
         int dontSelect = max(helper(i,j+1,prev|false,nums1,nums2), helper(i+1,j,prev|false,nums1,nums2));
         
-        return dp[curr] = max(select,dontSelect);
+        return dp[i][j][prev] = max(select,dontSelect);
     }
     
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         
-        dp.clear();
+        for(int i=0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+                dp[i][j][0] = dp[i][j][1] = INT_MIN;
+            }
+        }
         return helper(0,0,0,nums1,nums2);
     }
 };
