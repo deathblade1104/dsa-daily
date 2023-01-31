78250b6b864b29077e82f788383d9dfc19ad3144
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-01-31 12:15:33 +0530

    Time: 403 ms (31.81%), Space: 210.4 MB (5.52%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     
-    int dp[1001][1001];
-    int helper(int curr, int prevAge,vector<array<int,2>>&v){
+    
+    int helper(int curr, int prevAge,int&n,vector<array<int,2>>&v,vector<vector<int>>&dp){
        
-        if(curr>=v.size())
+        if(curr>=n)
             return 0;
         
         if(dp[curr][prevAge]!=-1)
@@ -13,26 +13,29 @@ public:
         int consider=0,dontConsider = 0;
         
         if(v[curr][1]>=prevAge)
-            consider = v[curr][0] + helper(curr+1,v[curr][1],v);
+            consider = v[curr][0] + helper(curr+1,v[curr][1],n,v,dp);
         
-        dontConsider = helper(curr+1,prevAge,v);
+        dontConsider = helper(curr+1,prevAge,n,v,dp);
         
         return dp[curr][prevAge] = max(consider,dontConsider);
     }
     
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
-        memset(dp,-1,sizeof dp);
         
         vector<array<int,2>>v;
+        int n = scores.size(),ageLimit =0;
         
-        for(int i=0;i<scores.size();i++){
+        for(int i=0;i<n;i++){
         
             v.push_back({scores[i],ages[i]});
+            ageLimit = max(ageLimit,ages[i]);
         }
-        
+    
         sort(v.begin(),v.end());
         
-        return helper(0,0,v);
+        vector<vector<int>>dp(n,vector<int>(ageLimit+1,-1));
+        
+        return helper(0,0,n,v,dp);
         
         
     }
