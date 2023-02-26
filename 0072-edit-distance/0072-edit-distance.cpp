2eeee9d82f3805aafa3d37677b2993ba86c4283c
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-02-27 01:30:48 +0530

    Time: 104 ms (9.30%), Space: 7.7 MB (76.35%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int dp[501][501];
+    int n1, n2;
     int helper(int i,int j,string&s1,string&s2){
         
-         if(i==s1.size())
-            return s2.size()-j;
+         if(i==n1)
+            return n2-j;
         
-        if(j==s2.size())
-            return s1.size()-i;
+        if(j==n2)
+            return n1-i;
         
         if(dp[i][j]!=-1)
             return dp[i][j];
@@ -18,7 +19,8 @@ public:
         else return dp[i][j] = 1 + min({helper(i,j+1,s1,s2), helper(i+1,j,s1,s2), helper(i+1,j+1,s1,s2)});
     }
     int minDistance(string word1, string word2) {
-        
+        n1 = word1.size();
+        n2 = word2.size();
         memset(dp,-1,sizeof dp);
         return helper(0,0,word1,word2);
     }
