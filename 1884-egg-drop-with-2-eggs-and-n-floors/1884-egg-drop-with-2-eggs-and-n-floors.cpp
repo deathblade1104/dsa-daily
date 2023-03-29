f55f8af1364275b5ed2bec29daeb0b8896e616e4
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-03-29 19:42:34 +0530

    Time: 8 ms (56.31%), Space: 6.1 MB (44.36%) - LeetHub

---
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int dp[10001][3];
+    int get_ans(int floor, int eggs)
+    {
+        if(floor==0 and eggs>0) return dp[floor][eggs]=0;
+        else if (floor==1 and eggs>0) return dp[floor][eggs]=1;
+        else if(eggs==0) return dp[floor][eggs]=INT_MAX/2;    
+        else if(eggs==1 and floor>0) return dp[floor][eggs]=floor;
+        
+        if(dp[floor][eggs]!=-1) return dp[floor][eggs];
+        
+        int res =INT_MAX,low=1,high=floor;
+        
+        while(low<=high)
+        {
+            int mid = (low+high)/2,left = get_ans(mid-1,eggs-1),right = get_ans(floor-mid,eggs),temp =  1 + max(left,right);
+            
+            if(left<right)
+                low=mid+1;
+            
+            else
+                high=mid-1;
+            
+            res=min(res,temp);
+        }
+        
+        return dp[floor][eggs]=res;           
+    }
+    int twoEggDrop(int n) {
+        memset(dp,-1,sizeof dp);
+        
+        return get_ans(n,2); 
+        
+    }
+};
