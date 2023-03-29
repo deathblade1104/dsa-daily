c0512c90864d42aa4c99c9a8fa92f45003d550bf
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-03-29 13:25:52 +0530

    Time: 30 ms (57.04%), Space: 9.2 MB (59.87%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int dp[501][502];
+    int helper(int curr,int t, vector<int>&satisfaction){
+        
+        if(curr>=satisfaction.size())
+            return 0;
+        
+        
+        if(dp[curr][t]!=-1)
+            return dp[curr][t];
+        
+        int op1 = satisfaction[curr]*t + helper(curr+1,t+1,satisfaction);
+        int op2 = helper(curr+1,t,satisfaction);
+        
+        return dp[curr][t] = max(op1,op2);
+    }
+    int maxSatisfaction(vector<int>& satisfaction) {
+        
+        sort(satisfaction.begin(),satisfaction.end());
+        memset(dp,-1,sizeof dp);
+        
+        return helper(0,1,satisfaction);
+        
+        
+    }
+};
