40713a63b4bb6f94e042bfaff83521b8ad04803b
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-04-10 12:15:48 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (56.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        
+        for(char ch : s)
+        {
+            if(ch=='(' or ch=='[' or ch=='{')
+                st.push(ch);
+            
+            else
+            {
+                if(st.size()==0)
+                    return false;
+                
+                else if(ch == ']' and st.top()=='[')
+                    st.pop();
+                
+                else if(ch == '}' and st.top()=='{')
+                    st.pop();
+                
+                else if(ch == ')' and st.top()=='(')
+                    st.pop();
+                
+                else return false;
+            }
+        }
+        
+        return st.size()==0;
+    }
+};
