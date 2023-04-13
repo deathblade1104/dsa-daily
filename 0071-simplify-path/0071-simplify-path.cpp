e42a6618beff32fb5810e26a468f006d4a5c1512
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-04-14 00:26:08 +0530

    Time: 4 ms (78.98%), Space: 9.7 MB (57.58%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        char x='/';
+        path+=x;
+        vector<string>st;
+        string temp="";
+        for(char  c : path)
+        {
+            if(c==x)
+            {
+                string a=temp;
+                temp="";
+                
+                if(a == "" or a== ".")
+                    continue;
+                
+                else if (a == "..")
+                {
+                    if(st.size()>0)
+                        st.pop_back();
+                }
+                
+                else
+                {
+                    st.push_back(a);
+
+                }
+                
+            }
+            
+            else
+                temp+=c;
+        }
+        
+        string ans="";
+        
+        if(st.size()!=0)
+        for(string a : st)
+            ans+="/"+a;
+        
+        if(ans.size()==0)
+            return "/";
+        
+        return ans;
+        
+        
+    }
+};
