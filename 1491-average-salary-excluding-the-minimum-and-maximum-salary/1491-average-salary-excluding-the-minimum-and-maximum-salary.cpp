8e5120593d6df30682e426227b48062ff449d4e3
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-05-01 08:52:00 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (84.06%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+     
+        int n = salary.size();
+        double d = 0.0;
+        int m1 = 900,m2 = 1000001;
+        for(int&i : salary){
+            
+            d+=(1.0 * i);
+            m1 = max(m1,i);
+            m2 = min(m2,i);
+        }
+        
+        d-=(1.0 * m1);
+        d-=(1.0 * m2);
+        
+        return (d/(1.0 * (n - 2)));
+    }
+};
