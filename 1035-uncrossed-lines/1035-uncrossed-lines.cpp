b8e94ba03f01254e9b12d2ef63e4d6efbbdbf588
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-05-11 19:42:39 +0530

    Time: 23 ms (51.51%), Space: 10.7 MB (80.86%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -4,20 +4,18 @@ public:
     int dp[501][501];
     int helper(int maxYet,int currIdx, vector<int>&nums1,vector<int>&nums2){
         
-        if(currIdx>=nums1.size())
+        if(maxYet>=nums2.size() || currIdx>=nums1.size())
             return 0;
         
         if(dp[maxYet][currIdx]!=-1)
              return dp[maxYet][currIdx];
         
-        int ans = helper(maxYet,currIdx+1,nums1,nums2);
-        for(int i=maxYet;i<nums2.size();i++){
-            if(nums1[currIdx] == nums2[i]){
-                //cout<<"Matching :"<<nums1[currIdx]<<" with : "<<nums2[i]<<endl;
-                ans = max(ans, 1 + helper(i+1,currIdx+1,nums1,nums2));
-            }
+        int ans=0;
+        if(nums1[currIdx] == nums2[maxYet]){
+            ans = max(ans,1 + helper(maxYet+1,currIdx+1,nums1,nums2));
+        }else{
+            ans = max(helper(maxYet+1,currIdx,nums1,nums2), helper(maxYet,currIdx+1,nums1,nums2));
         }
-        
         return dp[maxYet][currIdx] = ans;
     }
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
