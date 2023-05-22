b80ce77a5a07dd72e0de13f3f5c98ac9d3265663
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-05-22 09:29:14 +0530

    Time: 66 ms (39.16%), Space: 19.7 MB (74.73%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.cpp
+++ 0934-shortest-bridge/0934-shortest-bridge.cpp
@@ -1,36 +1,95 @@
 class Solution {
-public:
-   int dir[5] = {0, 1, 0, -1, 0};
-void paint(vector<vector<int>>& A, int i, int j, vector<pair<int, int>> &q) {
-    if (min(i, j) >= 0 && max(i, j) < A.size() && A[i][j] == 1) {
-        A[i][j] = 2;
-        q.push_back({i, j});
-        for (int d = 0; d < 4; ++d)
-            paint(A, i + dir[d], j + dir[d + 1], q);
+    public:
+    int dim[5]={1,0,-1,0,1};
+    
+    void DFS(int r,int c,int &n,int&m,int&no,vector<vector<int>>&grid)
+    {
+        if(r<0 or c<0 or r>=n or c>=m or grid[r][c]!=1)
+            return;
+        
+        grid[r][c]=no;
+        
+        for(int i=1;i<=4;i++)
+            DFS(r+dim[i-1],c+dim[i],n,m,no,grid);
+        
+        return;        
     }
-}
-int shortestBridge(vector<vector<int>>& A) {
-    vector<pair<int, int>> q;
-    for (int i = 0; q.size() == 0 && i < A.size(); ++i)
-        for (int j = 0; q.size() == 0 && j < A[0].size(); ++j)
-            paint(A, i, j, q);
-    while (!q.empty()) {
-        vector<pair<int, int>> q1;
-        for (auto [i, j] : q) {
-            for (int d = 0; d < 4; ++d) {
-                int x = i + dir[d], y = j + dir[d + 1];
-                if (min(x, y) >= 0 && max(x, y) < A.size()) {
-                    if (A[x][y] == 1)
-                        return A[i][j] - 2;
-                    if (A[x][y] == 0) {
-                        A[x][y] = A[i][j] + 1;
-                        q1.push_back({x, y});
-                    }
+    
+    
+    int shortestBridge(vector<vector<int>>& grid) {
+        
+        int n=grid.size(),m=grid[0].size();
+        int island_no=2;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    DFS(i,j,n,m,island_no,grid);
+                    island_no++;
                 }
             }
         }
-        swap(q, q1);
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=0;j<m;j++)
+//                 cout<<grid[i][j]<<" ";
+            
+//             cout<<endl;
+//         }
+        
+        
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==2)
+                    q.push({i,j});
+            }
+        }
+        
+        int steps=0;
+        
+        while(q.size()>0)
+        {
+            int sz=q.size();
+            for(int i=0;i<sz;i++)
+            {
+                auto p=q.front();
+                q.pop();
+                
+                int r=p.first,c=p.second;
+                
+                // cout<<r<<"_"<<c<<"--->"<<grid[r][c]<<endl;
+                
+                if(grid[r][c]==1)
+                    continue;
+                
+                else if(grid[r][c]==3)
+                    return steps-1;
+                
+                grid[r][c]=1;
+                
+                for(int j=1;j<=4;j++)
+                {
+                    int x=r+dim[j-1],y=c+dim[j];
+                    
+                    if(x<0 or y<0 or x>=n or y>=n or grid[x][y]==1)
+                        continue;
+                    
+                    else
+                        q.push({x,y});
+                }
+            }
+            
+            steps++;           
+        }
+        
+        return -1;      
+        
     }
-    return 0;
-}
 };
