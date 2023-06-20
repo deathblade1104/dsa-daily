f9c8567d22d9195ad686e117a17e412c3e70c567
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-06-20 23:21:43 +0530

    Time: 258 ms (58.32%), Space: 130.7 MB (52.46%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
-    //long long pref[100002];
+    long long pref[100002];
     vector<int> getAverages(vector<int>& nums, int k) {
         
-         int n = nums.size();
-        int windowSize = 2 * k + 1;
-        vector<int> ans(n, -1);
-
+        if(k==0)
+            return nums;
+        
+        int n = nums.size(),windowSize = 2 * k + 1;
+        
+        vector<int>ans(n,-1);
+       
         if (n < windowSize) {
             return ans;
         }
-
-        vector<long long> prefixSum(n + 1);
-        for (int i = 0; i < n; ++i) {
-            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        
+        memset(pref,0LL,sizeof pref);
+        
+        pref[0] = nums[0];
+        for(int i=1;i<n;i++){
+            pref[i]+=pref[i-1]+nums[i];    
         }
-
-        for (int i = k; i + k < n; ++i) {
-            ans[i] = (prefixSum[i + k + 1] - prefixSum[i - k]) / windowSize;
+        
+        for(int i=k;i<n-k;i++){
+            
+            int lb = i-k-1,ub = i+k;
+            long long sum = pref[ub];
+            
+            long long sub = 0;
+            if(lb>=0)
+                sub=pref[lb];
+            
+            sum -= sub;
+            //cout<<i<<" _ "<<ub<<" _ "<<lb<<" --> "<<pref[ub]<<" "<<sub<<" "<<sum<<endl;
+            ans[i] =(int) (sum/windowSize);
         }
-
+        
         return ans;
         
     }
