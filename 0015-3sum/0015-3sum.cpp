6fd210476c47462e7e51d8c29a64226c22fafccf
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-07-01 16:01:38 +0530

    Time: 202 ms (58.70%), Space: 23.9 MB (98.69%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,27 +6,18 @@ public:
         
         int n = nums.size();
         
-         for(int i=0;i<nums.size();i++)
+         for(int i=0;i<n;i++)
         {
-            int j=i+1,k=nums.size()-1;            
+            int j=i+1,k=n-1;            
             
             while(j<k)
             {                
                 if(nums[i] + nums[j] + nums[k] <0){
                     j++;
-                    
-//                     int l = nums[j];
-                    
-//                     while(j<nums.size() and j<k and nums[j]==l)
-//                         j++;
                 }
                 
                 else if(nums[i] + nums[j] + nums[k] > 0){
                     k--;
-                    
-                    // int  r= nums[k];
-                    //  while(k>=0 and k>j and nums[k]==r)
-                    //     k--;
                 }
                 
                 else
@@ -35,7 +26,7 @@ public:
                     
                     int l = nums[j], r= nums[k];
                     
-                    while(j<nums.size() and j<k and nums[j]==l)
+                    while(j<n and j<k and nums[j]==l)
                         j++;
                     
                     while(k>=0 and k>j and nums[k]==r)
@@ -43,9 +34,8 @@ public:
                     
                 }
             }
-            
-            int x = nums[i];
-            while(i+1<nums.size() and nums[i+1]==nums[i])
+
+            while(i+1<n and nums[i+1]==nums[i])
                 i++;
         }
         
