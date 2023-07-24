a2099543d78b5a57290e7d47fb2775a64d3a1e46
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-07-25 03:11:24 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (92.19%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double helper(double&x, int n)
+    {        
+        if(n==0)
+            return 1.0;
+        
+        if(n==1)
+            return x;
+        
+        double val = helper(x,n/2);
+        val*=val;
+        
+        return (n%2==0)? val: x*val;
+    }
+    double myPow(double x, int n) {
+                
+        double val = helper(x,abs(n));
+        
+        if(n<0)
+            return (1.0/val);
+        
+        return val;
+    }
+};
