3be4bca765901f309be160ada20c5a48c1685bfb
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-08-05 04:18:47 +0530

    Time: 12 ms (55.42%), Space: 10.7 MB (53.43%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -16,7 +16,7 @@ public:
             { 
                 string str = s.substr(i);
                 if(get_ans(str,mp))
-                    return mp[str] = mp[s]=mp[temp]=true;
+                    return mp[str] =mp[temp]= true;
                 else
                     mp[str] = false;
             }               
