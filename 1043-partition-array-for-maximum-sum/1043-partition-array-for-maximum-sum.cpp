051e0ee8716240300a77606a4f6440a67ddabdef
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-08-15 04:38:17 +0530

    Time: 13 ms (85.35%), Space: 9.6 MB (5.03%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,15 +1,5 @@
 class Solution {
 public:
-    int getMax(vector<int>&arr,int i,int j){
-        
-        int ans = arr[i];
-        for(int iter= i+1;iter<=j;iter++){
-            ans = max(ans,arr[iter]);
-        }
-        
-        return ans;
-    }
-    
     
     int dp[501][501]={};
     int K,N;
@@ -25,10 +15,9 @@ public:
             return dp[i][j];
         
         int mini = 0;
-        
+        int maxEle = arr[i];
         for(int k=i;k<i+K && k<N;k++){
-            
-            int maxEle  = getMax(arr,i,k);
+            maxEle = max(arr[k],maxEle);
             int curr = maxEle*(k-i+1) + helper(k+1,j,arr);
             mini = max(mini,curr);
         }
