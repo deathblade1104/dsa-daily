44d0595b8cc7386548a1875894842b6c15b91642
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-08-16 04:50:53 +0530

    Time: 1222 ms (68.47%), Space: 14.4 MB (80.28%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
-    int dp[71][5001];
-    int helper(int r,int&n,int&m,vector<vector<int>>&mat,int&target,int prev){
-       
-        if(r>=n)
-            return abs(target - prev);
-        if(dp[r][prev]!=-1)
-            return dp[r][prev];
+    int dp[71][4901];
+    
+    int n,m,tar;
+    
+    int helper(int r,int currSum,vector<vector<int>>&mat){
         
-        int ans = helper(r+1,n,m,mat,target,prev+mat[r][0]);
-        for(int i=1;i<m and ans!=0;i++){
-            ans = min(ans,helper(r+1,n,m,mat,target,prev+mat[r][i]));
+        if(r>=n)
+            return abs(currSum - tar);
+        
+        if(dp[r][currSum]!=-1)
+            return dp[r][currSum];
+        
+        
+        int ans = INT_MAX;
+        
+        for(int i=0;i<m;i++){
+            ans = min(ans,helper(r+1,currSum+mat[r][i],mat));
         }
         
-        return dp[r][prev] = ans;
+        return dp[r][currSum] = ans;
+        
     }
     int minimizeTheDifference(vector<vector<int>>& mat, int target) {
-        
         memset(dp,-1,sizeof dp);
-        int n = mat.size(),m= mat[0].size(),curr=0,ans=INT_MAX;
+        n = mat.size();
+        m = mat[0].size();
+        tar = target;
         
-        for(int i=0;i<n;i++)
-            sort(mat[i].begin(),mat[i].end());
-        
-        return helper(0,n,m,mat,target,0);
-        
+        return helper(0,0,mat);
+    
     }
 };
