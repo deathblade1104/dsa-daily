2b19814db87ff574cfc1ce95df143dc60a89e064
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-08-21 05:26:18 +0530

    Time: 208 ms (97.48%), Space: 8.2 MB (94.34%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,31 +1,40 @@
 class Solution {
 public:
-        vector<vector<int>> dp;
+    int dp[303][303];
     
-    int maxCoins(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> list = {1};
-        for (int i = 0; i < n; i++) list.push_back(nums[i]);
-        list.push_back(1);
+    int helper(int i, int j, vector<int>& list) {
+        if (i > j) 
+            return 0;
         
-        dp.assign(n + 2, vector<int>(n + 2, -1));
+        if (dp[i][j] != -1) 
+            return dp[i][j];
         
-        return helper_max_coins(1, n, list);
-    }
-    
-    int helper_max_coins(int i, int j, vector<int>& list) {
-        if (i > j) return 0;
-        
-        if (dp[i][j] != -1) return dp[i][j];
-        
-        int maxi = INT_MIN;
+        int maxi = 0;
         
         for (int ind = i; ind <= j; ind++) {
             int costs = list[i - 1] * list[ind] * list[j + 1] 
-                          + helper_max_coins(i, ind - 1, list)   // left partition
-                          + helper_max_coins(ind + 1, j, list);  // right partition
+                          + helper(i, ind - 1, list)   // left partition
+                          + helper(ind + 1, j, list);  // right partition
+            
             maxi = max(maxi, costs);
         }
+        
         return dp[i][j] = maxi;
     }
+    
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> list = {1};
+        for (int i = 0; i < n; i++) 
+            list.push_back(nums[i]);
+        
+        list.push_back(1);
+        
+        memset(dp,-1,sizeof dp);
+        
+        return helper(1, n, list);
+    }
+    
+    
 };
