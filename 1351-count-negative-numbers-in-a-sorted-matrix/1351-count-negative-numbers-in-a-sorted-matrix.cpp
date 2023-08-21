4b1a388a77c8c4cfab568ca4d4b37e4ab610f89e
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-08-22 04:01:43 +0530

    Time: 16 ms (46.85%), Space: 10.3 MB (60.85%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-    int countNegatives(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
+    
+    int getNegativeNumbers(vector<int>&arr,int&sz){
         
-        int i=m-1;
-        int j=0;
-		// to start from the grid[m-1][0] position
-        int count=0;
+        if(arr.back()>=0)
+            return 0;
         
-        while(i>=0 && i<m && j>=0 && j<n){
-            if(grid[i][j] < 0){
-                count = count + (n-j); //when the first occurrence of a number in a row is -ve, all following numbers will be -ve. So, add their count.
-                i=i-1; //then move to the above row
+        int low = 0, high = sz - 1,ans = sz;
+        
+        while(low<=high){
+            int mid = low + ((high - low)/2);
+            
+            if(arr[mid]>=0){
+                low = mid + 1;
             }
             else{
-                j=j+1; //else move to the right (i.e. to the next column)
+                ans = mid;
+                high = mid - 1;
             }
-                
-        }       
+        }
         
-        return count;
+        return sz - ans;
+    }
+    
+    int countNegatives(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size(),ans = 0;
+        
+        
+        for(int i=0;i<n;i++){
+            ans+=getNegativeNumbers(grid[i],m);
+        }
+        
+        return ans;
+        
         
     }
 };
