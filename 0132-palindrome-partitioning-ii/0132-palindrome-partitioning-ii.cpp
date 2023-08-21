f32e19ee8ad15c78d0db4c1f0d3eb7b25202ab60
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-08-21 05:31:22 +0530

    Time: 1280 ms (61.94%), Space: 6.6 MB (77.36%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     int dp[2001];
-    int palinDP[2001][2001];
-    
-    int isPalinHelper(int i,int j, string &s){
+    bool isPalindrome(int i, int j,string&s){
         
-        if(i==j)
-            return palinDP[i][j] = true;
-        if(i == j-1)
-            return palinDP[i][j] = (s[i]==s[j])? true : false;
-        
-        if(palinDP[i][j]!=-1)
-            return palinDP[i][j];
-        
-        bool ans = false;
-        
-        if(s[i] == s[j])
-            ans = true & isPalinHelper(i+1,j-1,s);
-        
-        bool x = isPalinHelper(i+1,j,s) , y = isPalinHelper(i,j-1,s);
-        
-        return palinDP[i][j] = ans;
+        while(i<j){
+            if(s[i]!=s[j])
+                return false;
+            
+            i++;
+            j--;
+        }
+        return true;
     }
     
     int helper(int curr,string&s){
@@ -34,7 +24,7 @@ public:
         int ans = INT_MAX;
         
         for(int i=curr;i<s.size();i++){
-            if(palinDP[curr][i]){
+            if(isPalindrome(curr,i,s)){
                 int t = 1 + helper(i+1,s);
                 ans = min(ans,t);
             }
@@ -43,9 +33,8 @@ public:
         return dp[curr] = ans;
     }
     int minCut(string s) {
-        memset(palinDP,-1,sizeof palinDP);
+        
         memset(dp,-1,sizeof dp);
-        int x = isPalinHelper(0,(int)s.size()-1,s);
         return helper(0,s);
         
     }
