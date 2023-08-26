b273e855cb6a7edab5e6fd10628c22cb86fb696c
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-08-26 22:55:30 +0530

    Time: 3 ms (78.55%), Space: 6.3 MB (90.75%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int dp[101][101];
+    int m,n,o;
     
     bool helper(int i,int j,string&s1,string&s2,string&s3){
         int k = i + j;
         
-        if(k>=s3.size() and i>=s1.size() and j>=s2.size())
+        if(k>=o and i>=m and j>=n)
             return true;
         
         if(dp[i][j]!=-1)
@@ -13,10 +14,10 @@ public:
         
         bool op1= false,op2 = false;
         
-        if(i<s1.size() and s1[i]==s3[k])
+        if(i<m and s1[i]==s3[k])
             op1 = helper(i+1,j,s1,s2,s3);
         
-         if(j<s2.size() and s2[j]==s3[k])
+         if(j<n and s2[j]==s3[k])
             op2= helper(i,j+1,s1,s2,s3);
         
         return dp[i][j] = false | op1 | op2;
@@ -24,6 +25,10 @@ public:
     
     bool isInterleave(string s1, string s2, string s3) {
         memset(dp,-1,sizeof dp);
+        o= s3.size(),m = s1.size(),n = s2.size();
+        
+        if(o!=m + n)
+            return false;
         
         return helper(0,0,s1,s2,s3);
         
