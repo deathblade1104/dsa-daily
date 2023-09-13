1e73a645e8361266713080f4b0392252acaee499
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-09-13 19:12:59 +0530

    Time: 14 ms (83.40%), Space: 18 MB (29.76%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int candy(vector<int>& arr) {
+        
+        int n = arr.size();
+        vector<int>ans(n,1);
+        
+        for(int i=1;i<n;i++){
+            
+            if(arr[i]>arr[i-1])
+                ans[i] = 1 + ans[i-1];
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            
+            if(arr[i]>arr[i+1]){
+                ans[i] = max(ans[i],1+ans[i+1]);
+            }
+        }
+        
+        
+        return accumulate(ans.begin(),ans.end(),0);
+        
+        
+    }
+};
