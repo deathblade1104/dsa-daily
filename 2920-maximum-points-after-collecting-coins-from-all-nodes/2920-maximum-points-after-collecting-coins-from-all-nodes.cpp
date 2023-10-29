d4a6b03f30eda481b0df6a84171edc8ca8e728aa
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-10-29 09:42:05 +0530

    Time: 839 ms (55.56%), Space: 204.7 MB (66.67%) - LeetHub

---
+++ 2920-maximum-points-after-collecting-coins-from-all-nodes/2920-maximum-points-after-collecting-coins-from-all-nodes.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    vector<vector<int>>adj;
+    int dp[100001][32];
+    int K;
+
+    int helper(int curr,int par,int flag,vector<int>&coins){
+        
+        if(flag>=32)
+            return 0;
+        
+        if(dp[curr][flag]!=-1)
+            return dp[curr][flag];
+        
+        int currCoins = coins[curr]>>flag;
+        int op1 = currCoins - K;
+        int op2 = floor(currCoins/2);
+        
+        for(int&neigh : adj[curr]){
+            if(neigh!=par){
+                op2+=helper(neigh,curr,flag+1,coins);
+                op1+=helper(neigh,curr,flag,coins);
+            }
+        }
+        
+        return dp[curr][flag] =  max(op1,op2);
+    }
+    
+    int maximumPoints(vector<vector<int>>& edges, vector<int>& coins, int k) {
+        int n = edges.size() +1 ;
+        adj.assign(n+1,vector<int>());
+        memset(dp,-1,sizeof dp);
+        for(auto&v : edges){
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
+        }
+        K = k;
+        return helper(0,-1,0,coins);
+    }
+};
