e1326c207453c92a50283aadacb52407018ce51f
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-10-29 12:36:01 +0530

    Time: 0 ms (100.00%), Space: 7 MB (31.24%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    typedef vector<int> vi;
+    vector<vi>ans;
+    
+    void helper(int curr,int n, int k,vi&temp){
+        
+        if(curr>9){
+            if(n== 0 and k==0){
+                ans.push_back(temp);
+            }
+            return;
+        }
+        
+        if(curr<=n and k>0){
+            temp.push_back(curr);
+            helper(curr+1,n-curr,k-1,temp);
+            temp.pop_back();
+        }
+        
+        helper(curr+1,n,k,temp);
+        return;   
+    }
+    
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vi temp;
+        ans.clear();
+        
+        if(n>45)
+            return ans;
+        
+        helper(1,n,k,temp);
+        return ans;
+    }
+};
