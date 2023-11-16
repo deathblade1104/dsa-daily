5d5494c0119ecdf7004d070ee9a09b676f5b3a8e
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-11-16 23:57:48 +0530

    Time: 19 ms (61.59%), Space: 9.2 MB (94.12%) - LeetHub

--- 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.cpp
+++ 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     int n,finalState;
-    int dp[15][16385];
-    int helper(int curr, int mask,vector<int>& nums1, vector<int>& nums2){
+    int dp[16385];
+    
+    int getSetBits(int n){
+        
+        int count = 0;
+        
+        while(n){
+            count++;
+            n = (n & (n-1));
+        }
+        
+        return count;
+        
+    }
+    
+    int helper(int mask,vector<int>& nums1, vector<int>& nums2){
+        int curr  = getSetBits(mask);
         
         if(mask>=finalState or curr>=n)
             return 0;
         
-        if(dp[curr][mask]!=-1)
-             return dp[curr][mask];
+        if(dp[mask]!=-1)
+             return dp[mask];
         
         int ans = 1e9;
 
@@ -18,16 +33,16 @@ public:
                 continue;
             
             int currSum = nums1[curr] ^ nums2[i];
-            currSum+=helper(curr+1,mask|(1<<i),nums1,nums2);
+            currSum+=helper(mask|(1<<i),nums1,nums2);
             ans = min(ans,currSum);
         }
-        return dp[curr][mask]  = ans;
+        return dp[mask]  = ans;
     }
     int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
         n = nums2.size();
         finalState = (1<<n) - 1;
         memset(dp,-1,sizeof dp);
         
-        return helper(0,0,nums1,nums2);
+        return helper(0,nums1,nums2);
     }
 };
