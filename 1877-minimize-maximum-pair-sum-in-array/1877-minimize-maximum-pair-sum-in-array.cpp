7d6dc286dc5a0e5915d64bef93d650921713ca6a
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-11-17 05:44:19 +0530

    Time: 202 ms (11.73%), Space: 96.7 MB (34.31%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(),nums.end());
         int n = nums.size();
         int ans = nums[0] + nums.back();
-        for(int i=0;i<n/2;i++){
+        for(int i=1;i<n/2;i++){
             
             int curr = nums[i] + nums[n-i-1];
             ans = max(ans,curr);
