be24f7eb5e5137420d9ba55531abe0b8b10e7b41
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-11-20 16:11:22 +0530

    Time: 164 ms (73.08%), Space: 102.4 MB (58.71%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
-    static const int maxN = 1e5+1;
-    int arr1[maxN],arr2[maxN],arr3[maxN];
     
-    void helper(string&s,char ch,int&i,int arr[maxN],int&currIndex){
+    void helper(string&s,char ch,int&i,int&sum,int&currIndex){
         
         int curr=0;
         for(char&c :s){
@@ -15,18 +13,18 @@ public:
             i=currIndex;
         
         
-        arr[currIndex] = curr;
+        sum+=curr;
         
         return;
         
     }
     
-    int getAns(int i,int arr[maxN],vector<int>&travel){
+    int getAns(int i,int&sum,vector<int>&travel){
         
         if(i<0)
             return 0;
         
-        return travel[i] + arr[i];
+        return travel[i] + sum;
     }
     
     int garbageCollection(vector<string>& v, vector<int>& travel) {
@@ -38,29 +36,23 @@ public:
         travel.push_back(0);
         reverse(travel.begin(),travel.end());
 
-        memset(arr1,0,sizeof arr1);
-        memset(arr2,0,sizeof arr2);
-        memset(arr3,0,sizeof arr3);
         
-        int i1=-1,i2=-1,i3=-1;
+        int sum1=0,sum2=0,sum3=0,i1=-1,i2=-1,i3=-1;
         
         for(int i=0;i<n;i++){
             
-            helper(v[i],'G',i1,arr1,i);
-            helper(v[i],'P',i2,arr2,i);
-            helper(v[i],'M',i3,arr3,i);
+            helper(v[i],'G',i1,sum1,i);
+            helper(v[i],'P',i2,sum2,i);
+            helper(v[i],'M',i3,sum3,i);
             
             if(i>0){
-                arr1[i]+=arr1[i-1];
-                arr2[i]+=arr2[i-1];
-                arr3[i]+=arr3[i-1];
                 travel[i]+=travel[i-1];
             }
         }
         
-        int op1 = getAns(i1,arr1,travel);
-        int op2 = getAns(i2,arr2,travel);
-        int op3 = getAns(i3,arr3,travel);
+        int op1 = getAns(i1,sum1,travel);
+        int op2 = getAns(i2,sum2,travel);
+        int op3 = getAns(i3,sum3,travel);
         
         return op1+op2+op3;
         
