640bc7ce7b1b7031573e8849762b8c0d2a6524b0
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2023-11-30 06:30:15 +0530

    Time: 347 ms (35.40%), Space: 14.8 MB (39.75%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,73 +1,55 @@
 class Solution {
 public:
-    int n,avg;
-    vector<int>nums;
-    static const int maxN=16,finalState = (1<<maxN) -1;
-    int dp[finalState][maxN+1];
-    
-    int getSum(int mask){
-    
-        int sum = 0;
-        
-        for(int i=0;i<n;i++){
-            if(mask & (1<<i))
-                sum+=nums[i];
-        }
-        
-        return sum;
-    }
-    
-bool helper(int mask, int parts) {
-    int curr = __builtin_popcount(mask);
+    int n, avg;
+    vector<int> nums;
+    static const int maxN = 16, finalState = (1 << maxN) - 1;
+    int dp[finalState][maxN + 1];
 
-    if (curr >= n)
-        return parts == 0;
+    bool helper(int mask, int parts, int currSum) {
+        int curr = __builtin_popcount(mask);
 
-    if (parts < 0)
-        return false;
+        if (curr >= n)
+            return parts == 0;
 
-    int &ans = dp[mask][parts];
-
-    if (ans != -1)
-        return ans;
-
-    ans = 0;
-
-    int currSum = getSum(mask)%avg;
-
-    for (int i = 0; i < n; i++) {
-        if (mask & (1 << i))
-            continue;
-
-        int newMask = mask | (1 << i);
-        int curP = (currSum + nums[i]== avg) ? 1 : 0;
-        ans = ans || helper(newMask, parts - curP);
-       
-    }
-
-    return ans;
-}
-
-    
-    bool canPartitionKSubsets(vector<int>& arr, int k) {
-        n=arr.size();
-        nums = arr;
-        
-        int sum=0,maxi=0;
-        
-        for(int&i : arr){
-            sum+=i;
-            maxi= max(maxi,i);
-        }
-        
-        avg = sum/k;
-        
-        if(n<k or (sum%k) or maxi>avg)
+        if (parts < 0)
             return false;
-        
-        
-        memset(dp,-1,sizeof dp);
-        return helper(0,k);
-        
+
+        int &ans = dp[mask][parts];
+
+        if (ans != -1)
+            return ans;
+
+        ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (mask & (1 << i))
+                continue;
+
+            int newMask = mask | (1 << i);
+            int curP = (currSum + nums[i] == avg) ? 1 : 0;
+            ans = ans || helper(newMask, parts - curP, (currSum%avg + nums[i]%avg)%avg);
+        }
+
+        return ans;
+    }
+
+    bool canPartitionKSubsets(vector<int> &arr, int k) {
+        n = arr.size();
+        nums = arr;
+
+        int sum = 0, maxi = 0;
+
+        for (int &i : arr) {
+            sum += i;
+            maxi = max(maxi, i);
+        }
+
+        avg = sum / k;
+
+        if (n < k || (sum % k) || maxi > avg)
+            return false;
+
+        memset(dp, -1, sizeof dp);
+        return helper(0, k, 0);
     }
 };
