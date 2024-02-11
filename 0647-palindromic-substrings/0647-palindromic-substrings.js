0ee67ef9e82ad86d559b7a4ccc37216fb24e7393
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2024-02-11 21:56:33 +0530

    Time: 96 ms (25.40%), Space: 70.5 MB (8.87%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const countSubstrings = function(s) {
+    
+    const strArr = s.split("");
+    const n = strArr.length;
+    const dp = new Array(n).fill(null).map(() => new Array(n).fill(-1));
+
+    let count = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            if (helper(i, j,strArr,dp)) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+        
+}
+
+
+const helper = (i, j, strArr,dp) => {
+        if (i > j) {
+            return false;
+        }
+        if (i === j) {
+            return true;
+        }
+        if (dp[i][j] !== -1) {
+            return dp[i][j];
+        }
+        if (i + 1 === j) {
+            dp[i][j] = strArr[i] === strArr[j];
+        } else {
+            dp[i][j] = (strArr[i] === strArr[j]) && helper(i + 1, j - 1,strArr,dp);
+        }
+        return dp[i][j];
+    };
