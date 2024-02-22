5f77b01448b6431e7405d772eb229bd7e7e02c40
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2024-02-22 08:52:19 +0530

    Time: 3 ms (71.04%), Space: 50 MB (98.28%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,38 @@
+class Solution {
+    
+    private int[] inEdge;
+    private int[] outEdge;
+    
+    
+    Solution(){
+        inEdge  = new int[1002];
+        outEdge = new int[1002];
+    }
+    
+    public int findJudge(int n, int[][] trust) {
+     
+        for(int i=1;i<=n;i++){
+            inEdge[i] = outEdge[i] = 0;
+        }
+        
+        
+        
+        for(int i=0;i<trust.length;i++){
+            int a = trust[i][0], b = trust[i][1];
+            
+            inEdge[b]++;
+            outEdge[a]++;
+        }
+        
+        
+        for(int i=1;i<=n;i++){
+            if(outEdge[i] == 0 && inEdge[i] == n-1)
+                return i;
+        }
+        
+        return -1;
+        
+        
+        
+    }
+}
