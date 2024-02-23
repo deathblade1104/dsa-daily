4e02b17416bb4d33a1d3184d49cefbc82d9902d2
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2024-02-23 09:39:08 +0530

    Time: 8 ms (44.68%), Space: 43.8 MB (96.78%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,50 +1,43 @@
-import java.util.*;
-
-
 class Solution {
-    private List<int[]>[] adj;
-    private  int[] dist;
-    
+    ArrayList<ArrayList<int[]>> adj;
+    ArrayList<Integer> dist;
+    Queue<int[]> q;
+
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        adj = new ArrayList[n];
+        adj = new ArrayList<>(n);
+        dist = new ArrayList<>(n);
         
-        // Initialize adjacency list
+        // Initialize dist with Integer.MAX_VALUE
         for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<>();
+            dist.add(Integer.MAX_VALUE);
         }
         
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
         
         for (int[] flight : flights) {
-            adj[flight[0]].add(new int[] {flight[1], flight[2]});
+            adj.get(flight[0]).add(new int[] { flight[1], flight[2] });
         }
         
-        // Initialize distance array
-        dist = new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[src] = 0;
-        
-        // Perform BFS
-        Queue<int[]> q = new LinkedList<>();
+        q = new ArrayDeque<>();
         q.offer(new int[]{src, 0});
-        int steps = 0;
-        
-        while (steps <= k && !q.isEmpty()) {
+        int steps =0;
+        while (steps<=k && !q.isEmpty()) {
             int sz = q.size();
             while (sz-- > 0) {
-                int[] arr = q.poll();
-                int node = arr[0], cost = arr[1];
-                
-                for (int[] v : adj[node]) {
-                    int neigh = v[0], neighDist = v[1] + cost;
-                    if (dist[neigh] > neighDist) {
-                        dist[neigh] = neighDist;
-                        q.offer(new int[]{neigh, neighDist});
+                int[] curr = q.poll();
+                for (int[] neigh : adj.get(curr[0])) {
+                    int neighNode = neigh[0], neighDist = neigh[1] + curr[1];
+                    if (dist.get(neighNode) > neighDist) {
+                        dist.set(neighNode, neighDist);
+                        q.offer(new int[]{neighNode, neighDist});
                     }
                 }
             }
             steps++;
         }
         
-        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+        return dist.get(dst) == Integer.MAX_VALUE ? -1 : dist.get(dst);   
     }
 }
