14668f5da2c7709c6351e79a14cc3e76b5779419
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2024-02-29 03:36:20 +0530

    Time: 16 ms (98.09%), Space: 55.6 MB (90.08%) - LeetHub

---
+++ 2560-house-robber-iv/2560-house-robber-iv.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minCapability(int[] nums, int k) {
+        
+        int low = Integer.MAX_VALUE, high = Integer.MIN_VALUE;
+        
+        for(int i : nums){
+            low = Math.min(i,low);
+            high = Math.max(i,high);
+        }
+        
+        int ans = -1,n = nums.length;
+        while(low<=high){
+            
+            int mid = low + ((high - low)/2);
+            
+            int taken= 0;
+            for(int i=0;i<n;i++){
+
+                if(nums[i]<=mid){
+                    taken++;
+                    i++;
+                }
+                
+                if(taken>=k)
+                    break;
+            }
+            
+            if(taken>=k){
+                ans = mid;
+                high = mid - 1;
+            }
+            
+            else low = mid + 1;    
+        }
+        
+        return ans;
+    }
+}
