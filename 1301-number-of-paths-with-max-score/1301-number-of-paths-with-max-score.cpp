2fcb4bc8242738901d9a48777011eeb2e7871f23
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2024-03-03 04:49:51 +0530

    Time: 4 ms (94.39%), Space: 10.1 MB (64.80%) - LeetHub

--- 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
@@ -1,57 +1,59 @@
 class Solution {
 private:
-    static int dp[101][101];
-    static int dp2[101][101][1774];
+    static int dp[101][101][2];
     static const int mod = 1e9 + 7;
     int n;
 
 public:
-    int helper(int r, int c, vector<string>& board) {
+    array<int,2> helper(int r, int c, vector<string>& board) {
         if(r == 0 && c == 0)
-            return 0;
+            return {0,1};
         
         if (r < 0 || c < 0 || board[r][c] == 'X')
-            return -1775;
+            return {-1775,0};
 
-        if (dp[r][c] != -1)
-            return dp[r][c];
+        if (dp[r][c][0] != -1)
+            return {dp[r][c][0],dp[r][c][1]};
 
-        int ans = isdigit(board[r][c]) ? board[r][c] -'0' : 0;
-        int currAns = helper(r - 1, c, board);
-        currAns = max(currAns, helper(r, c - 1, board));
-        currAns = max(currAns, helper(r - 1, c - 1, board));
-
-        return dp[r][c] = ans + currAns;
+        int currAns = isdigit(board[r][c]) ? board[r][c] -'0' : 0;
+        array<int,2> op1 = helper(r - 1, c, board);
+        array<int,2> op2 = helper(r, c - 1, board);
+        array<int,2> op3= helper(r - 1, c - 1, board);
+        
+        int maxi = max({op1[0],op2[0],op3[0]});
+        int ways = 0;
+        
+        if(maxi == op1[0])
+            ways=op1[1];
+        
+        if(maxi == op2[0])
+            ways=(ways%mod + op2[1]%mod)%mod;
+        
+        if(maxi == op3[0])
+            ways=(ways%mod + op3[1]%mod)%mod;
+        
+        dp[r][c][0] = maxi + currAns;
+        dp[r][c][1] = ways;
+        
+        return {maxi + currAns,ways};
     }
     
-    int helper2(int r, int c,int currSum,vector<string>&board){
-        
-        if(r==0 and c==0 and currSum==0)
-            return 1;
-        
-        if(r < 0 || c < 0 || board[r][c] == 'X' || currSum<0)
-            return 0;
-        
-        if(dp2[r][c][currSum]!=-1)
-            return dp2[r][c][currSum];
-        
-        int currVal = isdigit(board[r][c]) ? board[r][c] -'0' : 0;
-        int ans = (helper2(r-1,c,currSum-currVal,board)%mod + helper2(r-1,c-1,currSum-currVal,board)%mod + helper2(r,c-1,currSum-currVal,board)%mod)%mod;
-        
-        return dp2[r][c][currSum] = ans;
-    }
+
 
     vector<int> pathsWithMaxScore(vector<string>& board) {
         n = board.size();
-        memset(dp, -1, sizeof dp);
-        memset(dp2,-1,sizeof dp2);
-
-        int ans = max(0,helper(n - 1, n - 1, board));
-        int ans2 = ans>=0 ? helper2(n-1,n-1,ans,board) : 0;
-
-        return {ans, ans2};
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j][0] = -1;
+                dp[i][j][1] = 0;
+            }
+        }
+        
+        array<int,2> arr = helper(n-1,n-1,board);
+        
+        return {max(arr[0],0), arr[1]};
     }
 };
 
-int Solution::dp[101][101];
-int Solution::dp2[101][101][1774];
+int Solution::dp[101][101][2];
