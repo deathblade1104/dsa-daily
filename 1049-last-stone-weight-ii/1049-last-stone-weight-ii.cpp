c5d29c849328225de9cd8fa73c8936fa1a536d5f
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2024-03-02 18:24:57 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (75.06%) - LeetHub

---
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -0,0 +1,49 @@
+class Solution {
+private: 
+    static int sum,n,target;
+    static array<array<int,1501>,1501>dp;
+    
+    int helper(int curr,int currSum,vector<int>&nums){
+        
+        if(currSum>=target || curr>=n)
+            return abs(currSum - (sum - currSum));
+        
+        if(dp[curr][currSum]!=-1)
+            return dp[curr][currSum];
+        
+        
+        int ans = helper(curr+1,currSum+nums[curr],nums);
+        ans = min(ans,helper(curr+1,currSum,nums));
+        
+        return dp[curr][currSum] = ans;
+    }
+    
+    
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        
+        n = stones.size();
+        sum = 0;
+        
+        for(int&i : stones){
+            sum+=i;
+        }
+        
+        target = ceil(sum/2);
+        //cout<<n<<" "<<target<<endl;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=target;j++){
+                dp[i][j] = -1;
+            }
+        }
+        
+        
+        return helper(0,0,stones);        
+    }
+};
+
+int Solution::sum;
+int Solution::n;
+int Solution::target;
+array<array<int, 1501>, 1501> Solution::dp;
