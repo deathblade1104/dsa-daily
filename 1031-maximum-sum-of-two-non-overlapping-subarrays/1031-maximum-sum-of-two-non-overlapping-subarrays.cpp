c30412a3cfe490d707803631564f773920761d03
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2024-03-04 08:22:50 +0530

    Time: 6 ms (56.42%), Space: 10.9 MB (52.64%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -1,56 +1,75 @@
 class Solution {
 private : 
-    int pref[1001],n;
+    vector<int>start,end;
 public:
-    
-    int getSubArrSum(int l, int r){
-        
-        if(l<0 or r>=n)
-            return 0;
-        
-        int currSum = pref[r];
-        
-        if(l)
-            currSum -= pref[l-1];
-        
-        return currSum;
-    }
-    
-    
-    int maxSumTwoNoOverlap(vector<int>& nums, int f, int s){
-        memset(pref,0,sizeof pref);
-        n = nums.size();
-        
-        for(int i=0;i<n;i++){
-            
-            pref[i]+=nums[i];
-            
-            if(i)
-                pref[i]+=pref[i-1];
-        }
-        
+    int maxSumTwoNoOverlap(vector<int>& nums, int f, int s) {
         
         if(f>s)
             swap(f,s);
         
+        int n = nums.size();
+        start.assign(n,0);
+        end.assign(n,0);
         
-        int ans = 0;
+        int currSum = 0;
         
-        for(int i=0;i<n;i++){
-            int secondSum = getSubArrSum(i,i+s-1);
-            int firstSum;
+        for(int i=0;i<f;i++){
+            currSum+=nums[i];
+        }
+        
+        end[f-1] = currSum;
+        for(int i=f;i<n;i++){
+            currSum-=nums[i-f];
+            currSum+=nums[i];
+            end[i] = max(end[i-1],currSum);
+        }
+        
+        
+        
+        currSum=0;
+        reverse(nums.begin(),nums.end());
+         for(int i=0;i<f;i++){
+            currSum+=nums[i];
+        }
+        
+        start[f-1] = currSum;
+        for(int i=f;i<n;i++){
+            currSum-=nums[i-f];
+            currSum+=nums[i];
+            start[i] = max(start[i-1],currSum);
+        }
+        
+        reverse(start.begin(),start.end());
+        reverse(nums.begin(),nums.end());
+        
+
+        currSum = 0;
+        // for(int i=0;i<n;i++){
+        //     cout<<i<<" "<<start[i]<<" "<<end[i]<<endl;
+        // }
+        
+        for(int i=0;i<s;i++){
+            currSum+=nums[i];
+        }
+        
+        int ans = currSum + start[s];
+        
+        for(int i=s;i<n;i++){
+            currSum-=nums[i-s];
+            currSum+=nums[i];
             
-            for(int j=i+s;j<n;j++){
-                firstSum = getSubArrSum(j,j+f-1);
-                ans = max(ans, secondSum + firstSum);
-            }
+            int startSum = i+1<n ? start[i+1] :0;
+            int endSum = i-s-1>=0? end[i-s-1] :0;
             
-            for(int j=0;j+f-1<i and j<n;j++){
-                firstSum = getSubArrSum(j,j+f-1);
-                ans = max(ans, secondSum + firstSum);
-            }
+            ans  = max(ans,currSum + max(startSum,endSum));
+            
+            
         }
         
         return ans;
+        
+        
+        
+        
     }
 };
