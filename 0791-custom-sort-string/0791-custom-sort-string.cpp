edd1d647066e076d4f376d76781cd5f51b948f50
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2024-03-11 13:44:12 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (35.04%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    static int charOrderMap[26]; // Make charOrderMap static
-    
+
     struct myComp {
-        bool operator()(const char& ch1, const char& ch2) {
+        const vector<int>& orderMap;
+
+        myComp(const vector<int>& map) : orderMap(map) {}
+
+        bool operator()(const char& ch1, const char& ch2) const {
             int idx1 = ch1 - 'a';
             int idx2 = ch2 - 'a';
-            return charOrderMap[idx1] < charOrderMap[idx2];
+            return orderMap[idx1] < orderMap[idx2];
         }
     };
 
     string customSortString(string order, string s) {
-        
-        fill_n(charOrderMap, 26, 0);
-        
+       
+        vector<int> charOrderMap(26,0);
         
         int currOrder = 0;
         for (char& ch : order) {
             charOrderMap[ch - 'a'] = ++currOrder;
         }
-
-       
-        sort(s.begin(), s.end(), myComp());
+        
+        sort(s.begin(), s.end(), myComp(charOrderMap));
 
         return s;
     }
 };
-
-int Solution::charOrderMap[26] = {};
