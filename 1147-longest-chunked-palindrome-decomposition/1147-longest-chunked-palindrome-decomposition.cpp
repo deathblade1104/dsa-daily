cb493601f55d66c371c352373f462dbdebc4d2dd
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2024-03-11 19:05:26 +0530

    Time: 6 ms (38.78%), Space: 12.2 MB (24.08%) - LeetHub

--- 1147-longest-chunked-palindrome-decomposition/1147-longest-chunked-palindrome-decomposition.cpp
+++ 1147-longest-chunked-palindrome-decomposition/1147-longest-chunked-palindrome-decomposition.cpp
@@ -1,25 +1,96 @@
+class RollingHash {
+private:
+    long long int mod, p;
+    int n;
+    string str;
+    vector<long long int> pows, invPows, prefixHash;
+
+    long long int binaryExp(long long int x, long long int n, long long int m) {
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        long long int half = binaryExp(x, n / 2, m);
+        long long int ans = ((half % m) * (half % m)) % m;
+        if (n & 1) ans = ((ans % m) * (x % m)) % m;
+        return ans;
+    }
+    
+    void populatePowers() {
+        long long int currPow = 1LL;
+        for (int i = 0; i < n; i++) {
+            pows[i] = currPow;
+            currPow = ((currPow) % mod * (p) % mod) % mod;
+        }
+        invPows[n - 1] = binaryExp(pows[n - 1], mod - 2, mod);
+        for (int i = n - 2; i >= 0; i--) {
+            invPows[i] = (invPows[i + 1] * p) % mod;
+        }
+    }
+
+    void calculatePrefixHashes() {
+        long long int hashSoFar = 0;
+        for (int i = 0; i < n; i++) {
+            hashSoFar = (hashSoFar + (str[i] - 'a' + 1) * pows[i]) % mod;
+            prefixHash[i] = hashSoFar;
+        }
+    }
+
+public:
+    RollingHash(string s, long long int m) {
+        str = s;
+        n = str.size();
+        mod = m;
+        p = 31;
+        pows.resize(n);
+        invPows.resize(n);
+        prefixHash.resize(n);
+        populatePowers();
+        calculatePrefixHashes();
+    }
+
+
+
+    long long int substringHash(int l, int r) {
+        long long int val1 = prefixHash[r];
+        long long int val2 = l > 0 ? prefixHash[l - 1] : 0;
+        return ((val1 - val2 + mod) * invPows[l]) % mod;
+    }
+};
+
+
 class Solution {
 public:
     int longestDecomposition(string text) {
         
-        if(text == "")
-            return 0;
+        RollingHash hash1 = RollingHash(text,(long)(1e9 + 7));
+        RollingHash hash2 = RollingHash(text,(long)(1e9 + 5));
+        int s=0,e=text.size()-1;
         
-        int ans = 1, n = text.size();
-
-        string temp1 ="", temp2 = "";
-
-        for(int i=0;i<n;i++){
-            temp1+=text[i];
-            temp2 = text.substr(n-i-1);
-
-            if(temp1 == temp2 and i<n-1){
-                string nxtText = text.substr(i+1,n - i- i -2);
-                return 2 + longestDecomposition(nxtText);
-            }
+        int first_found=0,last_found=text.size()-1;
+        
+        int cnt=0;
+        
+        while(s<e){
+            string a=text.substr(first_found,s-first_found+1);
+            string b=text.substr(e,last_found-e+1);
+            
+            long long int preH1 = hash1.substringHash(first_found,s),preH2 = hash2.substringHash(first_found,s);
+            long long int suffH1 = hash1.substringHash(e,last_found),suffH2 = hash2.substringHash(e,last_found);
+            
+            s++;
+            e--;
+            
+            if(preH1 == suffH1 and preH2 == suffH2){
+                cnt+=2;
+                first_found=s;
+                last_found=e;
+            }   
         }
-
-        return ans;
         
+        if(first_found<=last_found){
+            cnt++;
+        }
+        
+        return cnt;
+
     }
 };
