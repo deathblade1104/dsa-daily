d621de904c7edf4d07cfbb0a90b90dff7b2235b7
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2024-05-12 02:54:29 +0530

    Time: 51 ms (15.41%), Space: 33.1 MB (8.96%) - LeetHub

---
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double mincostToHireWorkers(vector<int>& q, vector<int>& w, int K) {
+        vector<vector<double>> workers;
+        for (int i = 0; i < q.size(); ++i)
+            workers.push_back({(double)(w[i]) / q[i], (double)q[i]});
+        sort(workers.begin(), workers.end());
+        double res = 1.0 * LLONG_MAX, qsum = 0;
+        priority_queue<int> pq;
+        for (auto worker: workers) {
+            qsum += worker[1], pq.push(worker[1]);
+            if (pq.size() > K) qsum -= pq.top(), pq.pop();
+            if (pq.size() == K) res = min(res, qsum * worker[0]);
+        }
+        return res;
+        
+    }
+};
