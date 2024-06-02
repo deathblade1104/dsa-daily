5e453d511dfb1e7486116488c34ac28635fe31ec
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2024-06-03 03:52:59 +0530

    Time: 0 ms (100.00%), Space: 45.3 MB (95.94%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length,i=0, j=n-1;
+        
+        while(i<j){
+            
+            char ch = s[i];
+            s[i] = s[j];
+            s[j] = ch;
+            
+            i++;
+            j--;
+        }
+        
+        return;
+    }
+}
