1216109048b4555729ccb17cfa2465b3063c23ec
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2024-06-21 23:15:30 +0530

    Time: 4 ms (48.61%), Space: 47.4 MB (33.68%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxSatisfied(int[] customer, int[] grumpy, int m) {
+        
+        int n = grumpy.length,sum = 0;
+        int[] preSumUnsatisfied = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+        
+            preSumUnsatisfied[i] = 0;
+            
+            if (grumpy[i] == 1) {
+                preSumUnsatisfied[i] += customer[i];
+            } else {
+                sum += customer[i];
+            }
+            
+            if(i == 0)
+                continue;
+            
+            preSumUnsatisfied[i] += preSumUnsatisfied[i - 1];
+            
+        }
+        
+        int ans = sum;
+        
+        for (int i = m - 1; i < n; i++) {
+            int r = i, l = i - m + 1;
+            
+            int unsatisfied = preSumUnsatisfied[r];
+            if (l > 0) {
+                unsatisfied -= preSumUnsatisfied[l - 1];     
+            }
+            
+            ans = Math.max(ans, sum + unsatisfied);
+        }
+        
+        return ans;
+    }
+}
