607313bd4a6d03168759815219b5782e11fd43d3
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2024-07-31 03:28:00 +0530

    Time: 162 ms (12.68%), Space: 32.2 MB (47.42%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,45 +1,43 @@
 class Solution {
 public:
-    int f(int i, string &s, bool bSeen, vector<vector<int>>& dp) {
-        // Base case: If we reach the end of the string, no further deletions are needed
-        if (i == s.size()) {
+    static const int sz = 1e5 + 1;
+    int dp[sz][2];
+    int n;
+    int f(int i, string &s, bool bSeen) {
+    
+        if (i>= n)
             return 0;
-        }
-        // If already computed, return the stored result
+        
+       
         if (dp[i][bSeen] != -1) {
             return dp[i][bSeen];
         }
 
         int take = 1e9, notTake = 1e9;
 
-        // If the current character is 'a'
         if (s[i] == 'a') {
             if (bSeen) {
-                // We have already seen a 'b', so we must delete this 'a'
-                notTake = 1 + f(i + 1, s, bSeen, dp);
-            } else {
-                // We have not seen a 'b', so we can keep this 'a'
-                take = f(i + 1, s, bSeen, dp);
+                notTake = 1 + f(i + 1, s, bSeen);
+            } else {           
+                take = f(i + 1, s, bSeen);
             }
         } else {
-            // If the current character is 'b'
+    
             if (bSeen) {
-                // We have already seen a 'b', so we can keep this 'b'
-                take = f(i + 1, s, bSeen, dp);
+                take = f(i + 1, s, bSeen);
             } else {
-                // We have not seen a 'b', so we can either keep this 'b' or delete it
-                take = f(i + 1, s, true, dp);  // Mark b as seen
-                notTake = 1 + f(i + 1, s, bSeen, dp);  // Delete the 'b'
+                take = f(i + 1, s, true);  
+                notTake = 1 + f(i + 1, s, bSeen); 
             }
         }
 
-        // Store the result in dp array and return the minimum deletions
+        
         return dp[i][bSeen] = min(take, notTake);
     }
 
     int minimumDeletions(string s) {
-        int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));  // Initialize 2D dp array with -1
-        return f(0, s, false, dp);  // Start the recursive function from the first character with bSeen as false initially
+        n = s.size();
+        memset(dp,-1,sizeof dp);
+        return f(0, s, false);
     }
 };
