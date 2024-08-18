56692ef357b8cd3da4789879e31c8712f2bfca06
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2024-08-19 00:45:27 +0530

    Time: 68 ms (33.23%), Space: 31.9 MB (22.25%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+       if(n==1)return 1;
+        set<long long>st={2,3,5};
+        int count =2;
+        long long ans =-1;
+                
+        while(count <=n)
+        {
+            auto it = st.begin();
+            ans=*it;
+            st.erase(it);
+            
+            count++;
+            
+            st.insert(ans*2);
+            st.insert(ans*3);
+            st.insert(ans*5);
+        }
+        
+        return  ans; 
+    }
+};
