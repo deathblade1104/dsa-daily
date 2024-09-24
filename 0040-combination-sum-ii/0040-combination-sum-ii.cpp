a7635b422c4af0f760eac584df308ecf7c3345cf
Shahbaz Hasan Raja <shahbazhraja@gmail.com>
2024-09-24 08:32:40 +0530

    Time: 4 ms (56.70%), Space: 13 MB (42.25%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,41 +1,38 @@
 class Solution {
-public:
-    vector<vector<int>>ans;
-    int N;
-    void helper(int currIdx, int currSum,vector<int>&nums,vector<int>&temp){
-        
-        if(currSum == 0){
+private:
+    vector<vector<int>> ans;
+    int n;
+    
+    void helper(int curr, int currSum, vector<int>& nums, vector<int>& temp) {
+        if (currSum == 0) {
             ans.push_back(temp);
             return;
         }
         
-        if(currIdx>=N)
+        if (curr >= n || nums[curr] > currSum)
             return;
         
-        if(nums[currIdx]<=currSum){
-            temp.push_back(nums[currIdx]);
-            helper(currIdx+1,currSum-nums[currIdx],nums,temp);
+        for (int i = curr; i < n; i++) {
+            
+            if (nums[i] > currSum)
+                break;
+            
+            if (i > curr && nums[i] == nums[i - 1])
+                continue;
+            
+            temp.push_back(nums[i]);
+            helper(i + 1, currSum - nums[i], nums, temp);
             temp.pop_back();
         }
-        
-        for(int j=currIdx+1;j<N;j++){
-            if(nums[j]<=currSum and nums[j]!=nums[currIdx]){
-                helper(j,currSum,nums,temp);
-                break;
-            }
-        }
-        
-        return;
     }
-    
+
+public:
     vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());  // Sort to handle duplicates easily
+        n = nums.size();
         ans.clear();
-        vector<int>temp;
-        sort(nums.begin(),nums.end());
-        N = nums.size();
-        
-        helper(0,target,nums,temp);
+        vector<int> temp;
+        helper(0, target, nums, temp);
         return ans;
-        
     }
 };
