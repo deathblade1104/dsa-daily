17d49f63f4be154e8699c3823d5212256a004e8d
Shahbaz H. <shahbazhraja@gmail.com>
2025-11-07 02:51:22 +0530

    Time: 90 ms (66.94%) | Memory: 260.7 MB (9.51%) - LeetSync

--- nov-2025-and-beyond/3863-power-grid-maintenance/power-grid-maintenance.java
+++ nov-2025-and-beyond/3863-power-grid-maintenance/power-grid-maintenance.java
@@ -1,23 +1,13 @@
 class Solution {
 
-    private static class Util {
-        static TreeSet<Integer> mergeTwoSets(TreeSet<Integer> a, TreeSet<Integer> b) {
-            if (a.size() > b.size()) {
-                TreeSet<Integer> tmp = a;
-                a = b;
-                b = tmp;
-            }
-            b.addAll(a);
-            return b;
-        }
-    }
-
     private static class DisjointSet {
         int[] par, rank;
         boolean[] isOn;
         TreeSet<Integer>[] st;
+        int n;
 
         DisjointSet(int n) {
+            this.n = n;
             par = new int[n + 1];
             rank = new int[n + 1];
             isOn = new boolean[n + 1];
@@ -27,8 +17,6 @@ class Solution {
                 par[i] = i;
                 rank[i] = 0;
                 isOn[i] = true;
-                st[i] = new TreeSet<>();
-                st[i].add(i);
             }
         }
 
@@ -44,38 +32,39 @@ class Solution {
             if (rank[p1] >= rank[p2]) {
                 par[p2] = p1;
                 if (rank[p1] == rank[p2]) rank[p1]++;
-                st[p1] = Util.mergeTwoSets(st[p1], st[p2]);
-                st[p2] = null;
             } else {
                 par[p1] = p2;
-                st[p2] = Util.mergeTwoSets(st[p1], st[p2]);
-                st[p1] = null;
+            }
+        }
+
+        void build() {
+            for (int i = 1; i <= n; i++) {
+                int p = findPar(i);
+                if (st[p] == null)
+                    st[p] = new TreeSet<>();
+                st[p].add(i);
             }
         }
 
         int findSmallestInComponent(int u) {
-
-            if(isOn[u])
+            if (isOn[u])
                 return u;
-            
+
             int p = findPar(u);
             TreeSet<Integer> curr = st[p];
-            
-            if (curr == null || curr.isEmpty()) 
+            if (curr == null || curr.isEmpty())
                 return -1;
 
             return curr.first();
         }
 
         void remove(int u) {
-            
-            if (!isOn[u]) 
+            if (!isOn[u])
                 return;
 
             isOn[u] = false;
             TreeSet<Integer> curr = st[findPar(u)];
-
-            if (curr != null) 
+            if (curr != null)
                 curr.remove(u);
         }
     }
@@ -85,15 +74,18 @@ class Solution {
         for (int[] edge : edges)
             dsu.merge(edge[0], edge[1]);
 
+        dsu.build();
+
         List<Integer> ansList = new ArrayList<>();
         for (int[] q : queries) {
             if (q[0] == 1)
                 ansList.add(dsu.findSmallestInComponent(q[1]));
-            else dsu.remove(q[1]);
+            else
+                dsu.remove(q[1]);
         }
 
         int[] res = new int[ansList.size()];
-        for (int i = 0; i < res.length; i++) 
+        for (int i = 0; i < res.length; i++)
             res[i] = ansList.get(i);
 
         return res;
